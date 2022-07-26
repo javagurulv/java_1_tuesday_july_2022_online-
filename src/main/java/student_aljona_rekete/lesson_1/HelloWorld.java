8a279bc2b4cc29c85f5d5a702e8802693af74344
Aljenka <aljenka_@inbox.lv>
2022-07-26 21:46:19 +0300

    Lesson 1 homework !

---
+++ src/main/java/student_aljona_rekete/lesson_1/HelloWorld.java
@@ -0,0 +1,10 @@
+package student_aljona_rekete.lesson_1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+    }
+}
