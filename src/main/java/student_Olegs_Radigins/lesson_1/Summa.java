66afccfe8466db3aba08bef6680eb102cb95d5ac
olegsradigins <oradigins@gmail.com>
2022-07-26 21:15:21 +0300

    ;)

---
+++ src/main/java/student_Olegs_Radigins/lesson_1/Summa.java
@@ -0,0 +1,10 @@
+package student_Olegs_Radigins.lesson_1;
+
+public class Summa {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        int sum = a + b;
+        System.out.println("Summa = " + sum);
+    }
+}
