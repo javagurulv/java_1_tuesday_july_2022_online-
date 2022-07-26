ee476461a33d41a13d2d6649a54b65c7feb50a34
Ivan Mostepanov <neomess13@gmail.com>
2022-07-26 22:49:17 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_ivan_mostepanov/lesson_1/Homework_11.java
@@ -0,0 +1,10 @@
+package student_ivan_mostepanov.lesson_1;
+
+public class Homework_11 {
+    public static void main(String[] args) {
+
+    System.out.println(74+36);
+    System.out.println("Result=" + 50 / 3 );
+
+}
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_1/Homework_12.java
@@ -0,0 +1,11 @@
+package student_ivan_mostepanov.lesson_1;
+
+public class Homework_12 {
+    public static void main(String[] args) {
+
+    System.out.println(50*3);
+
+
+}
+
+}
