445e4b9801ff4fcfb10cb6bcf3eea4dba31a81b8
chees <n.alex@inbox.lv>
2022-07-26 21:56:01 +0300

    Lesson 1 homework 1

--- src/main/java/student_alexS/lesson_1/HelloWorld.java
+++ src/main/java/student_alexS/lesson_1/HelloWorld.java
@@ -2,9 +2,9 @@ package student_alexS.lesson_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
         System.out.println("Hello!");
-
+        System.out.println("Hello!");
+        System.out.println("Hello!");
 }
 
 }
