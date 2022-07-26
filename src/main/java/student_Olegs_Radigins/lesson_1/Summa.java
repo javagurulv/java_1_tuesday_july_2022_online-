c3f98e38d553d1bef2d40b03fa3fe2faf423ce8c
olegsradigins <oradigins@gmail.com>
2022-07-26 21:35:50 +0300

    ;)

--- src/main/java/student_Olegs_Radigins/lesson_1/Summa.java
+++ src/main/java/student_Olegs_Radigins/lesson_1/Summa.java
@@ -4,7 +4,8 @@ public class Summa {
     public static void main(String[] args) {
         int a = 74;
         int b = 36;
+        System.out.println("Summa = " + (a + b)); // Первый вариант
         int sum = a + b;
-        System.out.println("Summa = " + sum);
+        System.out.println("Summa = " + sum); // Второй вариант
     }
 }
