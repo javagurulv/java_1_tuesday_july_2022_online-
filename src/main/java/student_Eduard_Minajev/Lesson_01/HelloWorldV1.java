88fa26c7915f478188ece23210135c13563da9a2
EdwardMnv <Billionare225>
2022-07-26 22:18:04 +0300

    Lesson1 Classwork

---
+++ src/main/java/student_Eduard_Minajev/Lesson_01/HelloWorldV1.java
@@ -0,0 +1,10 @@
+package student_Eduard_Minajev.Lesson_01;
+
+public class HelloWorldV1 {
+  public static void main(String[] args){
+      System.out.println("HelloWorldV1");
+      System.out.println(50/5+16);
+      System.out.println(2*3);
+  }
+
+}
