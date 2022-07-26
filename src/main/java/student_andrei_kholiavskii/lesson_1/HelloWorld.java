8af05940f8f44b57a63d09eba608921476224647
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-07-26 21:49:28 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_andrei_kholiavskii/lesson_1/HelloWorld.java
@@ -0,0 +1,12 @@
+package student_andrei_kholiavskii.lesson_1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println(2 + 2);
+        System.out.println("Result = " +4 / 2 * 6);
+    }
+
+
+}
