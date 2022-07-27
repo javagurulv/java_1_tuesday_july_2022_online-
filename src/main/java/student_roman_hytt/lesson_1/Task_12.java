07bcf0f2ac2d986947a929fc1465ddb0126e369b
Admin <roman.hytt@gmail.com>
2022-07-27 17:09:39 +0300

    r

---
+++ src/main/java/student_roman_hytt/lesson_1/Task_12.java
@@ -0,0 +1,9 @@
+package student_roman_hytt.lesson_1;
+
+public class Task_12 {
+    public static void main ( String [] args) {
+  int x= 120;
+  int y= 130;
+  System.out.println ( x*y); // умножаем х на у
+    }
+}
