1076a6390566a45bf7aadc4ff0e5c221b5218ca3
Admin <roman.hytt@gmail.com>
2022-07-27 16:59:38 +0300

    r

---
+++ src/main/java/student_roman_hytt/lesson_1/Task_11.java
@@ -0,0 +1,10 @@
+package student_roman_hytt.lesson_1;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        int a= 74;
+        int b= 36;
+        System.out.print (b+a);
+
+    }
+}
