ce25916e25ba29d5bbc9c7001fdf1f88ad457d54
lblinnikovs <levs.blinnikovs@gmail.com>
2022-07-27 19:52:46 +0300

    Lesson 2 level 3 junior tasks

---
+++ src/main/java/student_levs_blinnikovs/lesson_2/level_3_junior/TaskElevenCalculateAvg.java
@@ -0,0 +1,45 @@
+package student_levs_blinnikovs.lesson_2.level_3_junior;
+
+/*
+
+Напишите программу, которая запрашивает у пользователя
+три целых числа и выводит их среднее арифметическое
+(складывает все числа и делит на их количество).
+
+Примечение: обратите внимание, что результат может быть дробным числом.
+
+ */
+
+import java.util.Scanner;
+
+public class TaskElevenCalculateAvg {
+
+    static int userInputOne;
+    static int userInputTwo;
+    static int userInputThree;
+
+    static double calculateAvg(int var1, int var2, int var3) {
+        return (double) (var1 + var2 + var3) / 3;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("This program will calculate double average from your three integer input. Please enter 3 values below: ");
+
+        System.out.println("First integer: ");
+        userInputOne = userInput.nextInt();
+
+        System.out.println("Second integer: ");
+        userInputTwo = userInput.nextInt();
+
+        System.out.println("Third integer: ");
+        userInputThree = userInput.nextInt();
+
+        System.out.println("Average result is: ");
+        System.out.println(calculateAvg(userInputOne, userInputTwo, userInputThree));
+
+    }
+
+}
---
+++ src/main/java/student_levs_blinnikovs/lesson_2/level_3_junior/TaskNineMultiplicationTable.java
@@ -0,0 +1,43 @@
+package student_levs_blinnikovs.lesson_2.level_3_junior;
+
+/*
+
+Напишите программу, которая запрашивает у пользователя целое число
+и печатает на консоль таблицу умножения для этого числа до 10.
+
+Test Data:
+Input a number: 8
+
+Expected Output :
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+...
+8 x 10 = 80
+
+ */
+
+import java.util.Scanner;
+
+public class TaskNineMultiplicationTable {
+
+    static int userInputInt;
+
+    static void calculateMultiplication(int inputInt) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(inputInt + " x " + i + " = " + i * inputInt);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("This program will print out multiplication table for desired integer. Please enter below: ");
+        userInputInt = userInput.nextInt();
+
+        System.out.println("Results: ");
+        calculateMultiplication(userInputInt);
+    }
+
+}
