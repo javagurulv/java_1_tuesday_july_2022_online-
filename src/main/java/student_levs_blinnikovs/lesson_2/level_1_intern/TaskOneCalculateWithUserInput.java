9d4d9eb87e1d0719daf89d9c432c625985b64967
lblinnikovs <levs.blinnikovs@gmail.com>
2022-07-27 17:18:43 +0300

    Lesson 2 level 1 intern task 1 - Calc with user input using Scanner.

---
+++ src/main/java/student_levs_blinnikovs/lesson_2/level_1_intern/TaskOneCalculateWithUserInput.java
@@ -0,0 +1,76 @@
+package student_levs_blinnikovs.lesson_2.level_1_intern;
+
+/*
+
+Написать консольную программу, которая запрашивает у пользователя два целых числа и
+выводит на консоль результаты:
+сложения,
+вычитания,
+умножения и
+деления.
+
+1. Создать класс с нужным именем;
+2. Создать описать main метод;
+3. Воспользуйтесь специальным классом Scanner и его методом nextInt() для получения ввода с клавиатуры
+          Scanner scanner = new Scanner(System.in);
+          int firstNumber = scanner.nextInt();
+4. Запишите полученные результаты в переменные;
+5. При помощи System.out.println() выведите на консоль полученные результаты сложения, вычитания, умножения и деления.
+ (каждый результат на новой строке отдельно).
+
+ */
+
+
+import java.util.Scanner;
+
+public class TaskOneCalculateWithUserInput {
+
+    static int userIntOne;
+    static int userIntTwo;
+
+    // Sum method
+    public static int intSum(int var1, int var2) {
+        return var1 + var2;
+    }
+
+    // Subtraction
+    public static int intSubtract(int var1, int var2) {
+        return var1 - var2;
+    }
+
+    // Product
+    public static int intProduct(int var1, int var2) {
+        return var1 * var2;
+    }
+
+    //Divide
+    public static int intDivision(int var1, int var2) {
+        return var1 / var2;
+    }
+
+    public static void main(String[] args) {
+
+        // Creating user input object
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("This program will sum, subtract, multiply and divide your two numbers (integers) of choice.");
+        System.out.println("Please enter first number: ");
+        userIntOne = userInput.nextInt();
+
+        System.out.println("Please enter second number: ");
+        userIntTwo = userInput.nextInt();
+
+        int intSumResult = intSum(userIntOne, userIntTwo);
+        int intSubtractResult = intSubtract(userIntOne, userIntTwo);
+        int intProductResult = intProduct(userIntOne, userIntTwo);
+        int intDivisionResult = intDivision(userIntOne, userIntTwo);
+
+        System.out.println("Results: \n");
+        System.out.println("Sum is: " + intSumResult);
+        System.out.println("Subtraction result is: " + intSubtractResult);
+        System.out.println("Product is: " + intProductResult);
+        System.out.println("Division result is: " + intDivisionResult);
+
+    }
+
+}
