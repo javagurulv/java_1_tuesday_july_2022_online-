fa804ba42e917fe0ac08e04aeff7d84dcf50a41f
Aljenka <aljenka_@inbox.lv>
2022-07-28 23:49:11 +0300

    Added Homework Task 11

---
+++ src/main/java/student_aljona_rekete/lesson_1/Task_11/Divide.java
@@ -0,0 +1,10 @@
+package student_aljona_rekete.lesson_1.Task_11;
+
+public class Divide {
+
+    public static void main( String[] args ) {
+        int a = 50;
+        int b = 3;
+        System.out.println("Divide = " + (a / b));
+    }
+}
---
+++ src/main/java/student_aljona_rekete/lesson_1/Task_11/Sum.java
@@ -0,0 +1,10 @@
+package student_aljona_rekete.lesson_1.Task_11;
+
+public class Sum {
+
+    public static void main( String[] args ) {
+        int a = 74;
+        int b = 36;
+        System.out.println("Sum = " + (a + b));
+    }
+}
