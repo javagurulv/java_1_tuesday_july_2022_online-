fb7daf7bc49feb60f0561ba641c32489e1267685
Aljenka <aljenka_@inbox.lv>
2022-07-28 23:57:29 +0300

    Added Homework Task 12

---
+++ src/main/java/student_aljona_rekete/lesson_1/Task_12/Multiplication.java
@@ -0,0 +1,11 @@
+package student_aljona_rekete.lesson_1.Task_12;
+
+public class Multiplication {
+
+    public static void main( String[] args ) {
+        int a = 50;
+        int b = 3;
+        System.out.println( a * b );
+    }
+
+}
