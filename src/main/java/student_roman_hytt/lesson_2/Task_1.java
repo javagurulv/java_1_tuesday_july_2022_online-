b999345e60b5164f66cd6184ab248b0ea74c5034
Admin <roman.hytt@gmail.com>
2022-07-30 14:16:36 +0300

    new scanner

---
+++ src/main/java/student_roman_hytt/lesson_2/Task_1.java
@@ -0,0 +1,16 @@
+package student_roman_hytt.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main (String [] args) {
+        Scanner scanner= new Scanner (System.in);
+        int firstNumber = scanner.nextInt();
+        int a= 1;
+        int b= 1;
+        int x=a+b;
+        System.out.println( " Result= " + x);
+
+
+    }
+}
