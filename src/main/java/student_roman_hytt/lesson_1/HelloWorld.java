302a92d7d140273bdae9d8f4ef1bf7aabe506120
Admin <roman.hytt@gmail.com>
2022-07-30 14:27:29 +0300

    HelloWorld

--- src/main/java/student_roman_hytt/lesson_1/HelloWorld.java
+++ src/main/java/student_roman_hytt/lesson_1/HelloWorld.java
@@ -1,10 +1,11 @@
 package student_roman_hytt.lesson_1;
 
-public class HelloWorld {  public static void main(String[] args) {
-    System.out.println("Hello!");
-    System.out.println("Hello!");
-    System.out.println("Hello!");
-}
-}
 
+public class HelloWorld {
 
+        public static void main (String []args){
+            System.out.println(" Hello World!!!");
+            System.out.println(" Hello World!!!");
+            System.out.println(" Hello World!!!");
+        }
+    }
