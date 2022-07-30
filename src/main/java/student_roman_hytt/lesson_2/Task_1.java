e6d5690bdb3f0fc6531650fd61c48ef4caa4532e
Admin <roman.hytt@gmail.com>
2022-07-30 20:48:23 +0300

    Task_1.java

--- src/main/java/student_roman_hytt/lesson_2/Task_1.java
+++ src/main/java/student_roman_hytt/lesson_2/Task_1.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class Task_1 {
     public static void main (String [] args) {
+        System.out.println (" Please write number 1 or 2");
+
         Scanner scanner= new Scanner (System.in);
         int firstNumber = scanner.nextInt();
         int a= 1;
-        int b= 1;
+        int b=2;
         int x=a+b;
-        System.out.println( " Result= " + x);
+        if ( firstNumber < 3){
+        System.out.println( " Result= " + x);}
+        if (firstNumber >3) {
+            System.out.println(" ERROR! Please write 1 or 2");
+        }
 
 
     }
