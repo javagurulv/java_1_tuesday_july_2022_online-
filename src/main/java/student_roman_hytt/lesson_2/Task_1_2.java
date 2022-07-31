07e4f85403f4eb7bb132f61303a51733f21befcf
Admin <roman.hytt@gmail.com>
2022-07-31 12:26:37 +0300

    T

---
+++ src/main/java/student_roman_hytt/lesson_2/Task_1_2.java
@@ -0,0 +1,18 @@
+package student_roman_hytt.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_1_2 {
+
+    public static void main (String [] args){
+    Scanner scanner= new Scanner(System.in);
+    int firstNumber = scanner.nextInt();
+      int a= firstNumber;
+      int c=+scanner.nextInt();
+      int y=a+c;
+
+        System.out.println(y);
+
+    }
+
+}
