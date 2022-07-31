e0bf926f7d34e1ba02e47a5f56a83c061408eadf
Admin <roman.hytt@gmail.com>
2022-07-31 12:33:11 +0300

    Size

---
+++ src/main/java/student_roman_hytt/lesson_2/SizeMatters.java
@@ -0,0 +1,10 @@
+package student_roman_hytt.lesson_2;
+
+public class SizeMatters {
+    public static void main (String[] args){
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+
+
+    }
+}
