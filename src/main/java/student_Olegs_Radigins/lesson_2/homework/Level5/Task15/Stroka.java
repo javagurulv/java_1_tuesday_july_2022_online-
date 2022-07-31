9fcf619e5556bbf76b5015f2089e0af700b85c80
olegsradigins <oradigins@gmail.com>
2022-07-31 13:19:31 +0300

    Homework Lesson2 level5 Task 15

--- src/main/java/student_Olegs_Radigins/lesson_2/homework/Level5/Task15/Stroka.java
+++ src/main/java/student_Olegs_Radigins/lesson_2/homework/Level5/Task15/Stroka.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Stroka {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите  число: ");
         int number1 = scanner.nextInt();
