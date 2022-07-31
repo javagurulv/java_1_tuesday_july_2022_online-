21e85aa2f1770db05495fe5dc819d4495decec3f
Admin <roman.hytt@gmail.com>
2022-07-31 12:10:51 +0300

    Task_1_2.java

---
+++ src/main/java/student_roman_hytt/lesson_2/Task_3.java
@@ -0,0 +1,27 @@
+package student_roman_hytt.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double firstDoubleNumber=scanner.nextDouble();
+        double num1,num2,sum;
+        num1=firstDoubleNumber;
+        num2=scanner.nextDouble();
+        sum =num1+num2;
+        System.out.println("Result "+ sum);
+        sum=num1-num2;
+        System.out.println("Result "+ sum);
+        sum=num1*num2;
+        System.out.println("Result "+ sum);
+        sum=num1/num2;
+        System.out.println("Result "+ sum);
+
+
+
+    }
+        }
+
+
+
