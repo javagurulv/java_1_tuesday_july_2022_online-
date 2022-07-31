9cbca1ecdbb807bab36229fe30e26999134f98a0
Yurii <thoryur@gmail.com>
2022-07-31 13:36:56 +0200

    Task 11, 12

---
+++ src/main/java/student_Yurii_Panasiuk/L1/Math.java
@@ -0,0 +1,12 @@
+package student_Yurii_Panasiuk.L1;
+
+public class Math {
+
+    public static void main(String[] args) {
+
+        System.out.println(74+36);
+        System.out.println(50/3);
+        System.out.println(50*3);
+    }
+
+}
