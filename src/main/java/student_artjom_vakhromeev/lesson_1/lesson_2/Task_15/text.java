2198acd4012e3b3536405c2f852953dafd9190e1
Admin <roman.hytt@gmail.com>
2022-08-01 21:22:12 +0300

    Lesson_2

--- src/main/java/student_artjom_vakhromeev/lesson_1/lesson_2/Task_15/text.java
+++
@@ -1,7 +0,0 @@
-package student_artjom_vakhromeev.lesson_1.lesson_2.Task_15;
-
-public class text {
-    public static void main(String[] args) {
-        System.out.print("Hello my name is Artjom");
-    }
-}
