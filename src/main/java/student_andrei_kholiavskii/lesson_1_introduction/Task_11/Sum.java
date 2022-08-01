feeccb7973f1926026543b2ea4cf2c6dfc880948
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-01 20:38:11 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_andrei_kholiavskii/lesson_1_introduction/Task_11/Sum.java
@@ -0,0 +1,9 @@
+package student_andrei_kholiavskii.lesson_1_introduction.Task_11;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+
+    }
+}
