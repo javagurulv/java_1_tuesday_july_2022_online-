c8a122db735889d25f1e8ab58e2a1facf22a9f7e
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-01 13:27:03 +0300

    Level 7 Task 31 - Discounts

---
+++ src/main/java/student_levs_blinnikovs/lesson_3_oop_first_look/level_7_senior/task_31/Product.java
@@ -0,0 +1,64 @@
+package student_levs_blinnikovs.lesson_3_oop_first_look.level_7_senior.task_31;
+
+/*
+Разработать класс - продукт,
+у которого должны быть следующие характеристики:
+
+Свойства:
+- Наименование (String name)
+- Стандартная цена (double regularPrice)
+- Скидка в процентах (double discount)
+
+Методы:
+1. Расчет актуальной стоимости с учетом скидки (double actualPrice()).
+2. Вывод информации о продукте в консоль (void printInformation()).
+3. Название продукта нужно задавать через конструктор.
+4. Стоимость и скидку нужно задавать через соответствующие методы.
+
+Класс с объявлением продукта должен называться "Product".
+Класс с демонстрацией работы должен называться "ProductDemo".
+ */
+
+class Product {
+
+    String name;
+    double regularPrice;
+    double discount;
+
+    Product(String name) {
+        this.name = name;
+    }
+
+    double actualPrice() {
+        return this.regularPrice * (1 - this.discount);
+    }
+
+    void printInformation() {
+        System.out.println("This is " + name + ". It costs " + regularPrice + " without discount and " + this.actualPrice() + " with discount of " + discount + "%.");
+    }
+
+    void setRegularPrice(double regularPrice) {
+        this.regularPrice = regularPrice;
+    }
+
+    void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
+
+class ProductDemo {
+
+    public static void main(String[] args) {
+
+        Product book = new Product("Thinking in Java 4th edition book");
+        book.printInformation();
+
+        book.setRegularPrice(30.00);
+        book.printInformation();
+
+        book.setDiscount(0.5);
+        book.printInformation();
+
+    }
+
+}
