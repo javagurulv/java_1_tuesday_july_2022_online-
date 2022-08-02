9f86cce08396a7407627b85e3b5d732d8f1b098d
Ernest <ernests.aleskevics@gmail.com>
2022-08-02 17:28:14 +0300

    lesson 1 homework 1

--- src/main/java/student_ernest_aleskevics/lesson_1/HelloWorld.java
+++ src/main/java/student_ernest_aleskevics/lesson_1/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!123");
         System.out.println(2 + 2);
-        System.out.println("Result = " + 4/2*6);
+        System.out.println("Result = " + 4/2*4);
     }
 }
