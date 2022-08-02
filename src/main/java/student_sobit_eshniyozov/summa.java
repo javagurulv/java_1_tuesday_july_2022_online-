88017071577fa23265d2a68fd159961202b3c165
sobitjon <seshniyozov@list.ru>
2022-08-02 23:00:59 +0500

    homework 1.2

--- src/main/java/student_sobit_eshniyozov/summa.java
+++ src/main/java/student_sobit_eshniyozov/summa.java
@@ -2,6 +2,6 @@ package student_sobit_eshniyozov;
 
 public class summa {
     public static void main(String[] args) {
-        System.out.println(5+5);
+        System.out.println(5+5-3);
     }
 }
