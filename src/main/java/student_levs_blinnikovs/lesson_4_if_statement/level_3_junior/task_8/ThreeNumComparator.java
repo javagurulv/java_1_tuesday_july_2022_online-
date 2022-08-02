f8c1ea127b8a4b2324a85e59ffbcf053c7de0fe0
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-02 10:59:52 +0300

    Split classes

--- src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_3_junior/task_8/ThreeNumComparator.java
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_3_junior/task_8/ThreeNumComparator.java
@@ -30,19 +30,3 @@ class ThreeNumComparator {
     }
 
 }
-
-class ThreeNumComparatorDemo {
-
-    public static void main(String[] args) {
-
-        ThreeNumComparator test1 = new ThreeNumComparator(1,1,1);
-        ThreeNumComparator test2 = new ThreeNumComparator(1,2,3);
-        ThreeNumComparator test3 = new ThreeNumComparator(1,2,1);
-
-        System.out.println(test1.compareNumbers());
-        System.out.println(test2.compareNumbers());
-        System.out.println(test3.compareNumbers());
-
-    }
-
-}
