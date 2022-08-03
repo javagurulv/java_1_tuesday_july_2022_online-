518448478310c30f020ad701fa78fc1a6ee34054
Ernest <ernests.aleskevics@gmail.com>
2022-08-03 15:25:35 +0300

    lesson 1 task 12

---
+++ src/main/java/student_ernest_aleskevics/lesson_2/homework/task_1/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+package student_ernest_aleskevics.lesson_2.homework.task_1;
+
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Please enter second number: ");
+        int secondNumber = scanner.nextInt();
+
+        int resultSum = firstNumber + secondNumber;
+        int resultSubtract = firstNumber - secondNumber;
+        int resultDivide = firstNumber / secondNumber;
+        int resultMultiply = firstNumber * secondNumber;
+
+        System.out.println("Sum of number :" + resultSum);
+        System.out.println("Subtract of number :" + resultSubtract);
+        System.out.println("Divide of number :" + resultDivide);
+        System.out.println("Multiply of number :" + resultMultiply);
+
+
+    }
+}
