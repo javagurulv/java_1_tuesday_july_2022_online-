a1b2ea3450b3fad6bb8be3ca3924223cffe11e90
Ernest <ernests.aleskevics@gmail.com>
2022-08-03 18:05:28 +0300

    lesson 2 task 7

---
+++ src/main/java/student_ernest_aleskevics/lesson_2/homework/task_7/LoginService.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_2.homework.task_7;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+
+    }
+}
