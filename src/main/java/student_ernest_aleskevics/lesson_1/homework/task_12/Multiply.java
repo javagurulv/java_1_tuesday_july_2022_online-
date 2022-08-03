d0ff0c08dd28c6552303ad12f3c4e69a54fbb6d7
Ernest <ernests.aleskevics@gmail.com>
2022-08-03 14:26:52 +0300

    lesson 1 task 12

---
+++ src/main/java/student_ernest_aleskevics/lesson_1/homework/task_12/Multiply.java
@@ -0,0 +1,24 @@
+package student_ernest_aleskevics.lesson_1.homework.task_12;
+
+public class Multiply {
+    public static void main(String[] args) {
+    /*
+    Напишите Java программу, которая выведет на консоль результат умножения двух чисел.
+    Числа можно придумать любые, их не надо запрашивать у пользователя.
+
+    Тестовые данные:
+    50 * 3
+    Ожидаемый результат:
+    150
+    */
+
+        int numberOne = 50;
+        int numberTwo = 3;
+        int result = numberOne * numberTwo;
+        System.out.println(result);
+
+    }
+
+
+
+}
