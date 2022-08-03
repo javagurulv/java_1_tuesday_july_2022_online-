acc7210d17dd429cc3111acabddf11239862075b
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-03 10:28:02 +0300

    Fix Text2 class

--- src/main/java/student_artjom_vakhromeev/lesson_2/Task_15/Text2.java
+++ src/main/java/student_artjom_vakhromeev/lesson_2/Task_15/Text2.java
@@ -1,6 +1,6 @@
-package student_artjom_vakhromeev.lesson_1.lesson_2.Task_15;
+package student_artjom_vakhromeev.lesson_2.Task_15;
 
-public class text {
+class Text2 {
     public static void main(String[] args) {
         System.out.print("Hello my name is Artjom");
     }
