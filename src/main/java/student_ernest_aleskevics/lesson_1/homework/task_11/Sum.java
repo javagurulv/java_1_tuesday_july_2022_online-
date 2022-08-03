b679eda6aabd8970ef5ff3cd2115c600eff3a684
Ernest <ernests.aleskevics@gmail.com>
2022-08-03 14:20:14 +0300

    lesson 1 task 11

---
+++ src/main/java/student_ernest_aleskevics/lesson_1/homework/task_11/Sum.java
@@ -0,0 +1,33 @@
+package student_ernest_aleskevics.lesson_1.homework.task_11;
+
+public class Sum {
+    public static void main(String[] args) {
+        /*
+        Напишите Java программу, которая выведет на консоль сумму двух чисел.
+        Числа можно придумать любые, их не надо запрашивать у пользователя.
+        Тестовые данные:
+        74 + 36
+        Ожидаемый результат:
+        110
+        */
+
+        int numberOne = 74;
+        int numberTwo = 36;
+        int resultSum = numberOne + numberTwo;
+        System.out.println(resultSum);
+
+        /*
+        Write a Java program to divide two numbers and print on the screen.
+        Test Data :
+        50/3
+        Expected Output :
+        16
+        */
+
+        int numberThree = 50;
+        int numberFour = 3;
+        int resultDivide = numberThree / numberFour;
+        System.out.println(resultDivide);
+
+    }
+}
