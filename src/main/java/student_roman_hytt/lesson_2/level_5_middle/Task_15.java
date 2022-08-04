e3bb8416fc5ff956c8ac5e1eb1a61c51092e0a0f
Admin <roman.hytt@gmail.com>
2022-08-04 20:16:59 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_2/level_5_middle/Task_15.java
@@ -0,0 +1,12 @@
+package student_roman_hytt.lesson_2.level_5_middle;
+
+public class Task_15 {
+
+    public static void main (String[] args){
+       System.out.print("Hello, ");
+       System.out.print("Hello!");
+
+
+
+    }
+}
---
+++ src/main/java/student_roman_hytt/lesson_2/level_6_middle/NoSleepNoFocus.java
@@ -0,0 +1,11 @@
+package student_roman_hytt.lesson_2.level_6_middle;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+
+
+    }
+
+}
