dca7af5ad9eb37f0876e4967a36490e9bcd5fce8
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-04 16:00:27 +0300

    Level 4 Task 12 - Calculator Tests

--- src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_4_junior/task_11_15/CalculatorTest.java
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_4_junior/task_11_15/CalculatorTest.java
@@ -85,4 +85,67 @@ class CalculatorTest {
  */
 
 class CalculatorTest {
+
+    public void sumTest() {
+        int firstNumber = 10;  // подготавливаем тестовые данные
+        int secondNumber = 5;  // подготавливаем тестовые данные
+        int expectedResult = 15;  // подготавливаем тестовые данные
+        Calculator calculator = new Calculator();
+        int realResult = calculator.sum(firstNumber, secondNumber);  // вызываем продакшен код, который хотим протестировать
+        if (realResult == expectedResult) {  // проверяем правильно ли сработал тестируемый код
+            System.out.println("Sum test = OK");
+        } else {
+            System.out.println("Sum test = FAIL");
+        }
+    }
+
+    public void subtractTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 5;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.subtract(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Subtract test = OK");
+        } else {
+            System.out.println("Subtract test = FAIL");
+        }
+    }
+
+    public void divideTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 2;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.divide(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Divide test = OK");
+        } else {
+            System.out.println("Divide test = FAIL");
+        }
+    }
+
+    public void multiplyTest() {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        int expectedResult = 50;
+        Calculator calculator = new Calculator();
+        int realResult = calculator.multiply(firstNumber, secondNumber);
+        if (realResult == expectedResult) {
+            System.out.println("Divide test = OK");
+        } else {
+            System.out.println("Divide test = FAIL");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.sumTest();
+        calculatorTest.subtractTest();
+        calculatorTest.divideTest();
+        calculatorTest.multiplyTest();
+
+    }
+
 }
