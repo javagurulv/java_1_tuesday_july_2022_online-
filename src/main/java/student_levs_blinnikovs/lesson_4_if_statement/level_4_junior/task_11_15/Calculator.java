f94d99fc12745426457a451963d700a555bde16a
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-04 14:51:29 +0300

    Calc stub

---
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_4_junior/task_11_15/Calculator.java
@@ -0,0 +1,21 @@
+package student_levs_blinnikovs.lesson_4_if_statement.level_4_junior.task_11_15;
+
+/*
+Создайте класс Calculator. В этом классе создайте методы
+для сложения, вычитания, деления и умножения двух целых чисел.
+
+Подсказка:
+
+class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    // реализуйте по аналогии оставшиеся методы
+
+}
+ */
+
+class Calculator {
+}
