a60784d9ece044087e0c0bcc48e4c0f68101b545
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-04 12:15:15 +0300

    IntProduct static fix

--- src/main/java/student_levs_blinnikovs/lesson_1_introduction/level_4_junior/task_11/IntProduct.java
+++ src/main/java/student_levs_blinnikovs/lesson_1_introduction/level_4_junior/task_11/IntProduct.java
@@ -15,14 +15,12 @@ package student_levs_blinnikovs.lesson_1_introduction.level_4_junior.task_11;
 import teacher.codereview.CodeReview;
 import teacher.codereview.CodeReviewComment;
 
+import java.util.Scanner;
+
 @CodeReview(approved = false)
 @CodeReviewComment(teacher = "Что это за использование static и свойств класса? "
 		+ "Зачем это нужно? Вы понимаете смысл static?")
 class IntProduct {
-
-    // Input
-    static int var1 = 50;
-    static int var2 = 3;
 
     // Function returns product of 2 integer values
     public static int intProduct(int int1, int int2) {
@@ -30,6 +28,18 @@ class IntProduct {
     }
 
     public static void main(String[] args) {
-        System.out.println("Result of multiplication: " + intProduct(var1, var2));
+
+        System.out.println("System will calculate product of two numbers. ");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int savedInput1 = userInput.nextInt();
+
+        System.out.println("Enter second number and hit Enter to multiply: ");
+        int savedInput2 = userInput.nextInt();
+
+        int multiplicationResult = intProduct(savedInput1, savedInput2);
+
+        System.out.println("Result of multiplication: " + multiplicationResult);
     }
 }
