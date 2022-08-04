3e54a4196fd88a9456488c3a6e7b107e6c16a869
Admin <roman.hytt@gmail.com>
2022-08-04 20:41:30 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_2/level_7_senior/SumTwoNumbers.java
@@ -0,0 +1,23 @@
+package student_roman_hytt.lesson_2.level_7_senior;
+
+public class SumTwoNumbers {
+
+
+        public static void main(String[] args) {
+
+            int numberOne=10;
+            int numberTwo=20;
+
+            System.out.println("Number 1 = " + numberOne);
+            System.out.println("Number 2 = " + numberTwo);
+
+
+            int sum = numberOne + numberTwo;
+
+            System.out.println("Sum = " + sum);
+        }
+
+    }
+
+
+
---
+++ src/main/java/student_roman_hytt/lesson_2/level_7_senior/task_28/BankAccount.java
@@ -0,0 +1,4 @@
+package student_roman_hytt.lesson_2.level_7_senior.task_28;
+
+public class BankAccount {
+}
