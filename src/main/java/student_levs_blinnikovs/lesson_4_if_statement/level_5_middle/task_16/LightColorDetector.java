7ddc09adbcee2755330194f21631a97163aa9e8f
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-05 23:47:56 +0300

    ColorTest

--- src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_5_middle/task_16/LightColorDetector.java
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_5_middle/task_16/LightColorDetector.java
@@ -22,7 +22,7 @@ package student_levs_blinnikovs.lesson_4_if_statement.level_5_middle.task_16;
 class LightColorDetector {
 ​
   public String detect(int wavelength) {
-    //TODO
+    //
   }
 
 }
@@ -32,5 +32,24 @@ class LightColorDetector {
 покрывать все возможные варианты.
  */
 
+import teacher.codereview.CodeReviewComment;
+
 class LightColorDetector {
+    @CodeReviewComment(student = "better initialize String color and assign per each action, and then return in the end, after ifs?")
+    public String detect(int wavelength) {
+        if (wavelength >= 380 && wavelength <= 449) {
+            return "Violet"; // better initialize String color and assign per each action, and then return in the end, after ifs?
+        } else if (wavelength >= 450 && wavelength <= 494) {
+            return "Blue";
+        } else if (wavelength >= 495 && wavelength <= 569) {
+            return "Green";
+        } else if (wavelength >= 570 && wavelength <= 589) {
+            return "Yellow";
+        } else if (wavelength >= 590 && wavelength <= 619) {
+            return "Orange";
+        } else if (wavelength >= 620 && wavelength <= 750) {
+            return "Red";
+        } else return "Invisible Light";
+    }
+
 }
---
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_5_middle/task_16/LightColorDetectorTest.java
@@ -0,0 +1,32 @@
+package student_levs_blinnikovs.lesson_4_if_statement.level_5_middle.task_16;
+
+import teacher.codereview.CodeReviewComment;
+
+class LightColorDetectorTest {
+
+    @CodeReviewComment(student = "Better that null safe equals (Object.equals(realColor, expectedColor) as Idea is suggesting?")
+    public void colorTest(int wavelength, String expectedColor) {
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realColor = lightColorDetector.detect(wavelength);
+        if (realColor.equals(expectedColor)) { // better that null safe equals (Object.equals(realColor, expectedColor) as Idea is suggesting?
+            System.out.println("Color test = OK");
+        } else {
+            System.out.println("Color test = FAIL");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LightColorDetectorTest lightColorDetectorTest = new LightColorDetectorTest();
+
+        lightColorDetectorTest.colorTest(400, "Violet");
+        lightColorDetectorTest.colorTest(494, "Blue");
+        lightColorDetectorTest.colorTest(500, "Green");
+        lightColorDetectorTest.colorTest(570, "Yellow");
+        lightColorDetectorTest.colorTest(619, "Orange");
+        lightColorDetectorTest.colorTest(740, "Red");
+        lightColorDetectorTest.colorTest(0, "Invisible Light");
+
+    }
+
+}
