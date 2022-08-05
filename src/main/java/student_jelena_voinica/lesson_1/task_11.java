df406c26ecd0fc42ff52405a003f4b2a059ce40e
Uguha21 <Agasem2106@icloud.com>
2022-08-05 18:28:05 +0300

    task_12

--- src/main/java/student_jelena_voinica/lesson_1/task_11.java
+++ src/main/java/student_jelena_voinica/lesson_1/task_11.java
@@ -1,13 +1,9 @@
 package student_jelena_voinica.lesson_1;
 
-public class task_11 {
+public class task_11{
     public static void main(String[]args){
-        int num1 = 25;
-        int num2 = 17;
 
-        int sum = num1 + num2;
-
-        System.out.println(sum);
+        System.out.println(1515);
 
 
 
---
+++ src/main/java/student_jelena_voinica/lesson_1/task_12.java
@@ -0,0 +1,8 @@
+package student_jelena_voinica.lesson_1;
+
+public class task_12 {
+
+    public static void main(String[]args){
+        System.out.println(12*5);
+    }
+}
