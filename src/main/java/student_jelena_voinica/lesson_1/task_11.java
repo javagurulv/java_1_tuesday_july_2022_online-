9aa892a9695969f861805d7e20b355248b582544
Uguha21 <Agasem2106@icloud.com>
2022-08-05 17:50:53 +0300

    task_11

---
+++ src/main/java/student_jelena_voinica/lesson_1/task_11.java
@@ -0,0 +1,15 @@
+package student_jelena_voinica.lesson_1;
+
+public class task_11 {
+    public static void main(String[]args){
+        int num1 = 25;
+        int num2 = 17;
+
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+
+
+    }
+}
