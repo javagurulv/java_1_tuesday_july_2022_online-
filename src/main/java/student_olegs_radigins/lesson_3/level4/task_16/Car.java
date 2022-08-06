e758922f9ac9484ac5d2e2e5727352a7828cca4b
olegsradigins <oradigins@gmail.com>
2022-08-06 15:50:30 +0300

    Lesson 3, level 4, Task 16

---
+++ src/main/java/student_olegs_radigins/lesson_3/level4/task_16/Car.java
@@ -0,0 +1,13 @@
+package student_olegs_radigins.lesson_3.level4.task_16;
+
+class Car {
+    String model;
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    String getModel() {
+        return model;
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_3/level4/task_16/CarDemo.java
@@ -0,0 +1,9 @@
+package student_olegs_radigins.lesson_3.level4.task_16;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+}
