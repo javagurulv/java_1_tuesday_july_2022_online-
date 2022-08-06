af8c2bd6dcb9094440aca77ded91ae036ed9475f
olegsradigins <oradigins@gmail.com>
2022-08-06 15:20:07 +0300

    Lesson 3, level 2, Tasks 7-9

---
+++ src/main/java/student_olegs_radigins/lesson_3/level2/Task_8/Name.java
@@ -0,0 +1,11 @@
+package student_olegs_radigins.lesson_3.level2.Task_8;
+
+ class Name {
+     public static void main(String[] args) {
+         String userName = "Oleg";
+         System.out.println("Hi " + userName + "!");
+
+         String greeting = "Hi " + userName + "!";
+         System.out.println(greeting);
+     }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_3/level2/Task_9/UserName.java
@@ -0,0 +1,12 @@
+package student_olegs_radigins.lesson_3.level2.Task_9;
+
+import java.util.Scanner;
+
+public class UserName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String userName = scanner.next();
+        System.out.println("Hello " + userName + "!");
+    }
+}
