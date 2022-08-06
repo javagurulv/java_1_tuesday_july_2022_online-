b227395944e40d097bfa72d26b48745519f84f9a
olegsradigins <oradigins@gmail.com>
2022-08-06 16:04:53 +0300

    Lesson 3, level 4, Task 17

---
+++ src/main/java/student_olegs_radigins/lesson_3/level4/task_17/BankAccount.java
@@ -0,0 +1,25 @@
+package student_olegs_radigins.lesson_3.level4.task_17;
+
+class BankAccount {
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return moneyAmount;
+    }
+}
