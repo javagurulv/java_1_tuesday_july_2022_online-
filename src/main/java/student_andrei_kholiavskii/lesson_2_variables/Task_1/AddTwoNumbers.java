a56703a6ff01591f819ed92a4059997e46c05732
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-06 13:29:30 +0300

    Lesson 1 homework 1

--- src/main/java/student_andrei_kholiavskii/lesson_2_variables/Task_1/AddTwoNumbers.java
+++ src/main/java/student_andrei_kholiavskii/lesson_2_variables/Task_1/AddTwoNumbers.java
@@ -13,12 +13,12 @@ public class AddTwoNumbers {
 
         int secondNumber = scanner.nextInt(); // get second number from user
         int sum = firstNumber + secondNumber; // get sum of numbers
-        int substraction = firstNumber - secondNumber; // get substraction of numbers
+        int subtraction = firstNumber - secondNumber; // get subtraction of numbers
         int multiplication = firstNumber * secondNumber; // get multiplication of numbers
         int division = firstNumber / secondNumber; // get division of numbers
 
         System.out.println("Sum of numbers = " + sum);
-        System.out.println("Substraction of numbers = " + substraction);
+        System.out.println("Substraction of numbers = " + subtraction);
         System.out.println("Multiplication of numbers = " + multiplication);
         System.out.println("Division of numbers = " + division);
     }
