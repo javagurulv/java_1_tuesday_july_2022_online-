31380b3651d94fa2f613d33b112064fba7b11b82
Yurii <thoryur@gmail.com>
2022-08-06 19:28:51 +0200

    Lesson 2, Task 1,2,3,4,5

---
+++ src/main/java/student_Yurii_Panasiuk/L2/Level5/Task18.java
@@ -0,0 +1,12 @@
+package student_Yurii_Panasiuk.L2.Level5;
+
+public class Task18 {
+
+    private static class SizeMatters {
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+    }
+
+}
