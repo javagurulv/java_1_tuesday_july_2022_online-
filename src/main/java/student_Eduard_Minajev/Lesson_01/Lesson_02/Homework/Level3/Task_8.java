7fe29b910722a147d79157588b20dab2850fb6b1
EdwardMnv <Billionare225>
2022-08-06 12:19:05 +0300

    Lesson 2 Level 1 task 2 and level 3 task 8

---
+++ src/main/java/student_Eduard_Minajev/Lesson_01/Lesson_02/Homework/Level3/Task_8.java
@@ -0,0 +1,11 @@
+package student_Eduard_Minajev.Lesson_01.Lesson_02.Homework.Level3;
+import java.util.Scanner;
+public class Task_8 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Please Enter Your Name and Surname!");
+        String userNameSurname = scanner.nextLine();
+        System.out.println("Hello Dear " + userNameSurname);
+
+    }
+}
