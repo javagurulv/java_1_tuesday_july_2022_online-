ebb97a91b2b4845dbe28a58908a42f05e3a7d393
olegsradigins <oradigins@gmail.com>
2022-08-06 15:46:02 +0300

    Lesson 3, level 4, Task 15

---
+++ src/main/java/student_olegs_radigins/lesson_3/level4/task_15/Phone.java
@@ -0,0 +1,13 @@
+package student_olegs_radigins.lesson_3.level4.task_15;
+
+class Phone {
+    String model;
+
+     Phone(String model) {
+        this.model = model;
+    }
+
+    String getModel() {
+        return model;
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_3/level4/task_15/PhoneDemo.java
@@ -0,0 +1,10 @@
+package student_olegs_radigins.lesson_3.level4.task_15;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
