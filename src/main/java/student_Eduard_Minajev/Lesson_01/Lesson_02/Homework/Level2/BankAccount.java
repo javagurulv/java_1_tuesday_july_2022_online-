c2bfc556f41469343eee8464d79f31f862a562a3
EdwardMnv <Billionare225>
2022-08-06 22:25:56 +0300

    Lesson 2 Level 2 tasks

---
+++ src/main/java/student_Eduard_Minajev/Lesson_01/Lesson_02/Homework/Level2/BankAccount.java
@@ -0,0 +1,7 @@
+package student_Eduard_Minajev.Lesson_01.Lesson_02.Homework.Level2;
+
+public class BankAccount {
+    public static void main (String[] args){
+
+    }
+}
---
+++ src/main/java/student_Eduard_Minajev/Lesson_01/Lesson_02/Homework/Level2/LoginService.java
@@ -0,0 +1,7 @@
+package student_Eduard_Minajev.Lesson_01.Lesson_02.Homework.Level2;
+
+public class LoginService {
+    public static void main (String[] args){
+        System.out.println("You can now log-in!");
+    }
+}
