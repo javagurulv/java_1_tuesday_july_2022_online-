baf276eaa36893a3d2aacca863daf1a77a9c77e5
EdwardMnv <Billionare225>
2022-08-06 12:03:27 +0300

    Lesson 2 Level 1 task 1

---
+++ src/main/java/student_Eduard_Minajev/Lesson_01/Lesson_02/Homework/Level_1/Task1.java
@@ -0,0 +1,29 @@
+package student_Eduard_Minajev.Lesson_01.Lesson_02.Homework.Level_1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main (String [] args){
+        Scanner scanner = new Scanner (System.in); //запускаем сканер
+        System.out.println("Please Enter First Whole Number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please Enter Second Whole Number");
+        int secondNumber = scanner.nextInt();
+        System.out.println("Addition");
+        int result = firstNumber + secondNumber;
+        System.out.println(result);
+        System.out.println("Subtraction");
+        int resultsub = firstNumber - secondNumber;
+        System.out.println(resultsub);
+        System.out.println("Multiplying");
+        int resultMul = firstNumber * secondNumber;
+        System.out.println(resultMul);
+        System.out.println("Divide");
+        int resultDiv = firstNumber / secondNumber;
+        System.out.println(resultDiv);
+
+
+
+    }
+
+}
