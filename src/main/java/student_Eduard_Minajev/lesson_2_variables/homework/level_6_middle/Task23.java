d91b248fbaa7c45f2bc8751c8a7815b8583fdf75
EdwardMnv <Billionare225>
2022-08-07 17:57:44 +0300

    Lesson 2 Homework

---
+++ src/main/java/student_Eduard_Minajev/lesson_2_variables/homework/level_6_middle/Task23.java
@@ -0,0 +1,7 @@
+package student_Eduard_Minajev.lesson_2_variables.homework.level_6_middle;
+
+public class Task23 {
+    public static void main(String[] args){
+        System.out.println("Code outside the method will not compile!");
+    }
+}
