1cdb28ba706b9ca90463f2408fc241b018d9a6c2
olegsradigins <oradigins@gmail.com>
2022-08-07 07:30:12 +0300

    Lesson 3, level 5, Task 18

---
+++ src/main/java/student_olegs_radigins/lesson_3/level5/task_18/Dog.java
@@ -0,0 +1,8 @@
+package student_olegs_radigins.lesson_3.level5.task_18;
+
+class Dog {
+    static String dogName = "Sharik";
+    void dogVoice(){
+        System.out.println(dogName + " " + dogName + " " + dogName);
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_3/level5/task_18/DogDemo.java
@@ -0,0 +1,9 @@
+package student_olegs_radigins.lesson_3.level5.task_18;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.dogVoice();
+
+    }
+}
