325b5e9ff951d27a2d1d27df464e8fe579a51d56
sobitjon <seshniyozov@list.ru>
2022-08-08 19:43:46 +0500

    homework TableOfMultiplication

--- src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_3/TableOfMultiplication.java
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_3/TableOfMultiplication.java
@@ -4,4 +4,22 @@ import java.util.Scanner;
 
 public class TableOfMultiplication {
 
+
+    static void TableOfMultiplication(int nomberOne) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(nomberOne + " x " + i + " = " + i * nomberOne);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Naberite sifru:");
+        int userNomberOne;
+        userNomberOne = userInput.nextInt();
+
+        System.out.println("rezultat: ");
+        TableOfMultiplication(userNomberOne);
+    }
 }
