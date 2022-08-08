4c2282ba8379282a93916c0b497ac90d8d00dac2
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-08 21:28:16 +0300

    Lesson 2 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_2_variables/task_24/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package student_andrei_kholiavskii.lesson_2_variables.task_24;
+
+public class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
