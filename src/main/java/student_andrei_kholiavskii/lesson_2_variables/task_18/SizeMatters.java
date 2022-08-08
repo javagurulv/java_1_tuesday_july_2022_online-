fedcec22517c2edf4c3bbe1e2909aadf8093eae6
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-08 20:55:39 +0300

    Lesson 2 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_2_variables/task_18/SizeMatters.java
@@ -0,0 +1,8 @@
+package student_andrei_kholiavskii.lesson_2_variables.task_18;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have errors."); // 1) "System" - should be written with a capital letter;
+        System.out.println("Please, fix me!"); // ";" at the end.
+    }
+}
