6b079a07ca1884e07f6af2696e18dd8fdd631450
EdwardMnv <Billionare225>
2022-08-09 22:07:10 +0300

    Lesson 3 Hope I didn't screw it up...

--- src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/Robot.java
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/Robot.java
@@ -1,4 +1,4 @@
-package student_eduard_minajev.lesson_3_oop_first_look.homework;
+package student_eduard_minajev.lesson_3_oop_first_look.homework.level_1_intern.tasks_1_till_6;
 
  class Robot {
      Robot (){
