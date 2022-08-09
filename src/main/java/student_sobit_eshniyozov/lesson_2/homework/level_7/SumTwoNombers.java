2a821f4cf113b8333b25835b89e037250f949894
sobitjon <seshniyozov@list.ru>
2022-08-09 19:19:39 +0500

    homework Task 28-29

---
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_7/SumTwoNombers.java
@@ -0,0 +1,18 @@
+package student_sobit_eshniyozov.lesson_2.homework.level_7;
+
+public class SumTwoNombers {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
---
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_7/SystemUnit.java
@@ -0,0 +1,4 @@
+package student_sobit_eshniyozov.lesson_2.homework.level_7;
+
+public class SystemUnit {
+}
