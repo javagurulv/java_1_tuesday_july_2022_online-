41b3adffdb67dee086ad3a932e4eb9a51bb200c3
EdwardMnv <Billionare225>
2022-08-09 22:02:36 +0300

    Lesson 3 Hope I didn't screw it up...

---
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/Robot.java
@@ -0,0 +1,13 @@
+package student_eduard_minajev.lesson_3_oop_first_look.homework;
+
+ class Robot {
+     Robot (){
+
+     }
+   public void sayHello (){
+       System.out.println("Hello!");
+
+
+    }
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/RobotApplication.java
@@ -0,0 +1,10 @@
+package student_eduard_minajev.lesson_3_oop_first_look.homework;
+
+public class RobotApplication {
+    public static void main(String[] args){;
+    Robot robo = new Robot();
+    robo.sayHello();
+
+
+    }
+}
