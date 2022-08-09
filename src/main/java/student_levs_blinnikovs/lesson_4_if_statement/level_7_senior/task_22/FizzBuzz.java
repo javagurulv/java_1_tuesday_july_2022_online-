b4236b3e39fada1dc1753d2c92548ead8550b951
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-09 19:09:34 +0300

    FizzBuzz stub

--- src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_7_senior/task_22/FizzBuzz.java
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_7_senior/task_22/FizzBuzz.java
@@ -1,5 +1,41 @@
 package student_levs_blinnikovs.lesson_4_if_statement.level_7_senior.task_22;
 
+/*
+Создайте класс FizzBuzz.
+В классе FizzBuzz создайте метод со следующей сигнатурой:
+
 class FizzBuzz {
+
+    public String detect(int number) {
+        // Реализуйте следующие требования:
+
+
+        // - если переданное число делится на три возвращайте "Fizz"
+        // - если переданное число делится на пять возвращайте "Buzz"
+        // - если переданное число делится на три и на пять возвращайте "FizzBuzz"
+        // - иначе возвращайте само число в виде строки (подсказка: return "" +  number)
+    }
+
+}
+
+Создайте класс FizzBuzzTest и напишите автоматические тесты,
+которые покрывают все возможные сценарии.
+ */
+
+class FizzBuzz {
+
+
+    public String detect(int number) {
+        String result;
+        result = String.valueOf(number);
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+    }
 
 }
---
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_7_senior/task_22/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package student_levs_blinnikovs.lesson_4_if_statement.level_7_senior.task_22;
+
+class FizzBuzzTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("FizzBuzz tests: ");
+
+    }
+
+}
