034a76d20c12039c314ad7729ea7127db3c88a8e
Ivan Mostepanov <neomess13@gmail.com>
2022-08-09 18:50:00 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_ivan_mostepanov/lesson_2/Task_3.java
@@ -0,0 +1,5 @@
+package student_ivan_mostepanov.lesson_2;
+
+public class Task_3 {
+    class Book {}
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_2/Task_4.java
@@ -0,0 +1,8 @@
+package student_ivan_mostepanov.lesson_2;
+
+public class Task_4 {
+
+    class Phone{
+
+}
+}
