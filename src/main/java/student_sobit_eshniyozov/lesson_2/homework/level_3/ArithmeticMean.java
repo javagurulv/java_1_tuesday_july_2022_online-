6014d47124c08178ba2098871cb081b57313c6eb
sobitjon <seshniyozov@list.ru>
2022-08-09 18:22:13 +0500

    homework ArithmeticMean

---
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_3/ArithmeticMean.java
@@ -0,0 +1,25 @@
+package student_sobit_eshniyozov.lesson_2.homework.level_3;
+
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class ArithmeticMean {
+    public static void main(String[] args) {
+        Scanner ScannerOne = new Scanner( System.in );
+        System.out.println("Enter the first number:");
+        double nomberOne = ScannerOne.nextDouble();
+
+        Scanner ScannerTwo = new Scanner( System.in );
+        System.out.println("Enter the second number:");
+        double nomberTwo = ScannerTwo.nextDouble();
+
+        Scanner ScannerThree = new Scanner( System.in );
+        System.out.println("Enter the third number:");
+        double nomberThree = ScannerThree.nextDouble();
+
+        double result = (nomberOne+nomberTwo+nomberThree)/3;
+        System.out.println("Arithmetic mean = " + result);
+
+    }
+}
