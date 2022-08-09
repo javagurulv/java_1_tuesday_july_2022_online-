28298ce74a487693882c38ea1b4d53c2e2a0d534
sobitjon <seshniyozov@list.ru>
2022-08-09 19:20:10 +0500

    homework Task 28-29

--- src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_7/SumTwoNombers.java
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_7/SumTwoNombers.java
@@ -5,14 +5,10 @@ public class SumTwoNombers {
         int numberOne = 10;
         int numberTwo = 20;
 
-
         System.out.println("Number 1 = " + numberOne);
         System.out.println("Number 2 = " + numberTwo);
 
-
         int sum = numberOne + numberTwo;
-
         System.out.println("Sum = " + sum);
     }
-
 }
