4c736ad449b0d05b2877676a36854e1357a00617
Ivan Mostepanov <neomess13@gmail.com>
2022-08-09 23:56:54 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_ivan_mostepanov/lesson_2/Task_7.java
@@ -0,0 +1,12 @@
+package student_ivan_mostepanov.lesson_2;
+
+public class Task_7 {
+
+        public static void main(String[] args) {
+            System.out.println("You can login!");
+        }
+
+    }
+
+
+
---
+++ src/main/java/student_ivan_mostepanov/lesson_2/Task_8.java
@@ -0,0 +1,19 @@
+package student_ivan_mostepanov.lesson_2;
+
+import java.util.Scanner;
+
+    public class Task_8 {
+
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Hello! What is your name?: ");
+            String myName = scanner.nextLine();
+
+
+
+            System.out.println(" Hello " + myName);
+
+        }
+    }
