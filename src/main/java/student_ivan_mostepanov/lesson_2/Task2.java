1de184a9796ec580597eef6e4cd622bf4e2a3e2f
Ivan Mostepanov <neomess13@gmail.com>
2022-08-09 18:33:26 +0300

    Lesson 1 homework 1

---
+++ src/main/java/student_ivan_mostepanov/lesson_2/Task2.java
@@ -0,0 +1,28 @@
+package student_ivan_mostepanov.lesson_2;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+    Scanner scanner =  new Scanner(System.in);
+    System.out.println("Please enter first number: ");
+    double firstDoubleNumber = scanner.nextDouble();
+
+    System.out.println("Please enter second number: ");
+    double secondDoubleNumber = scanner.nextDouble();
+
+
+    double resoultSum = firstDoubleNumber + secondDoubleNumber;
+    double resoultSubtraction = firstDoubleNumber - secondDoubleNumber;
+    double resoultMulti = firstDoubleNumber * secondDoubleNumber;
+    double resoultDivide = firstDoubleNumber / secondDoubleNumber;
+
+    System.out.println(" Sum =  " + resoultSum);
+    System.out.println(" Subtraction =  " + resoultSubtraction);
+    System.out.println(" Multiply =  " + resoultMulti);
+    System.out.println(" Divide = " + resoultDivide);
+
+}
+}
