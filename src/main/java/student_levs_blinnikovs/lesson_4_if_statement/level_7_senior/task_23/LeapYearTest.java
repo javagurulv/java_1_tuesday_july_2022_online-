71244f03ecd22f70c8aa6821038ab3b19e9e32ef
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-09 19:41:42 +0300

    LeapYear stub

---
+++ src/main/java/student_levs_blinnikovs/lesson_4_if_statement/level_7_senior/task_23/LeapYearTest.java
@@ -0,0 +1,13 @@
+package student_levs_blinnikovs.lesson_4_if_statement.level_7_senior.task_23;
+
+class LeapYearTest {
+
+    public static void main(String[] args) {
+
+        LeapYearTest test = new LeapYearTest();
+
+
+
+    }
+
+}
