12d528318b2227568ae6c97d9a237421950ab7a3
sobitjon <seshniyozov@list.ru>
2022-08-09 19:08:53 +0500

    homework Task 24

---
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_6/Task24.java
@@ -0,0 +1,8 @@
+package student_sobit_eshniyozov.lesson_2.homework.level_6;
+
+    class NoSleepNoFocus {
+        public static void main(String[] args) {
+            System.out.println("I wanna coffee");
+            System.out.println("So bad");
+        }
+    }
