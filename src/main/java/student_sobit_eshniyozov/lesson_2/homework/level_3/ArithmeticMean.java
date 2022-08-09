6a4ba49a2b8b0a041a95987adcf9a1177cc0c414
sobitjon <seshniyozov@list.ru>
2022-08-09 18:25:27 +0500

    homework ArithmeticMean

--- src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_3/ArithmeticMean.java
+++ src/main/java/student_sobit_eshniyozov/lesson_2/homework/level_3/ArithmeticMean.java
@@ -6,20 +6,21 @@ import java.lang.Math;
 
 public class ArithmeticMean {
     public static void main(String[] args) {
+
         Scanner ScannerOne = new Scanner( System.in );
         System.out.println("Enter the first number:");
         double nomberOne = ScannerOne.nextDouble();
 
-        Scanner ScannerTwo = new Scanner( System.in );
-        System.out.println("Enter the second number:");
-        double nomberTwo = ScannerTwo.nextDouble();
+            Scanner ScannerTwo = new Scanner( System.in );
+            System.out.println("Enter the second number:");
+            double nomberTwo = ScannerTwo.nextDouble();
 
-        Scanner ScannerThree = new Scanner( System.in );
-        System.out.println("Enter the third number:");
-        double nomberThree = ScannerThree.nextDouble();
+            Scanner ScannerThree = new Scanner( System.in );
+            System.out.println("Enter the third number:");
+            double nomberThree = ScannerThree.nextDouble();
 
-        double result = (nomberOne+nomberTwo+nomberThree)/3;
-        System.out.println("Arithmetic mean = " + result);
+            double result = (nomberOne+nomberTwo+nomberThree)/3;
+            System.out.println("Arithmetic mean = " + result);
 
     }
 }
