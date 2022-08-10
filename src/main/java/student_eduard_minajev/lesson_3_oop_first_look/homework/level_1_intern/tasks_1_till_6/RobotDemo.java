50c1ca52af8aba5c98b5d3f5e54128372c58e11e
EdwardMnv <Billionare225>
2022-08-10 21:34:18 +0300

    Lesson 3 Почти Джеймс Камерон

--- src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/RobotDemo.java
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/RobotDemo.java
@@ -2,8 +2,37 @@ package student_eduard_minajev.lesson_3_oop_first_look.homework.level_1_intern.t
 
 public class RobotDemo {
     public static void main(String[] args){
-        Robot robo = new Robot();
-        robo.sayHello();
+        Robot roby = new Robot("Roby","idle");
+        roby.sayHello();
+        roby.sayYourName();
+        roby.statusReport();
+
+        Robot turbo = new Robot ("Teddy", "idle");
+        turbo.sayHello();
+        turbo.sayYourName();
+        turbo.statusReport();
+
+        Robot hobbit = new Robot ("Frodo","idle");
+        hobbit.sayHello();
+        hobbit.sayYourName();
+        hobbit.statusReport();
+
+        roby.statusChange("Looking at the unknown robot");
+        roby.statusReport();
+        turbo.statusChange("Looking at the unknown robot");
+        turbo.statusReport();
+        hobbit.statusChange("Looking at the uknown robot");
+        hobbit.statusReport();
+
+
+
+        Robot unknow = new Robot ("unknow", "idle");
+        unknow.statusChange("looking with red glowing eyes in the dark");
+        unknow.statusReport();
+        unknow.newNameChange("I'm Terminator T-800 Cyberdyne Systems, Model 1-0-1");
+        unknow.saySarahConnor();
+        unknow.selfIntroduce();
+        unknow.sayComeWithMe();
 
     };
 
