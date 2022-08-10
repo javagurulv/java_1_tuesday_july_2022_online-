5a3937ee7b13ef308728f36fedc79140482e465a
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-10 20:13:12 +0300

    Lesson 2 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_2_variables/level_7_senior/task_29/SumTwoNumbers.java
@@ -0,0 +1,20 @@
+package student_andrei_kholiavskii.lesson_2_variables.level_7_senior.task_29;
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne = 0;
+        int numberTwo = 0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        numberOne = 10;
+        numberTwo = 20;
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+}
