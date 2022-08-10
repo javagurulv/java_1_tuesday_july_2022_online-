e77bc62d4e217486d005ed2208e2c37913b4533d
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-10 20:36:04 +0300

    Lesson 2 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_2_variables/level_7_senior/task_32/Comments.java
@@ -0,0 +1,20 @@
+package student_andrei_kholiavskii.lesson_2_variables.level_7_senior.task_32;
+
+public class Comments {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;// copy-paste from the previous task
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo); /* what is this
+        function used for? When longer comment needed?*/
+
+        double sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+
+}
