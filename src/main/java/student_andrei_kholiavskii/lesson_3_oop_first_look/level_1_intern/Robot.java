fd6377ab2ade7e034d5ad387afbdfe1618e3532d
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-10 22:55:34 +0300

    Lesson 3 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/Robot.java
@@ -0,0 +1,15 @@
+package student_andrei_kholiavskii.lesson_3_oop_first_look.level_1_intern;
+
+class Robot {
+    public Robot() {
+
+    }
+
+    public Robot(String firstRobot) {
+    }
+
+    public void sayHello() {
+System.out.println("Hello!");
+    }
+
+}
---
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/RobotDemo.java
@@ -0,0 +1,8 @@
+package student_andrei_kholiavskii.lesson_3_oop_first_look.level_1_intern;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+       Robot firstRobot = new Robot("firstRobot");
+       firstRobot.sayHello();
+    }
+}
