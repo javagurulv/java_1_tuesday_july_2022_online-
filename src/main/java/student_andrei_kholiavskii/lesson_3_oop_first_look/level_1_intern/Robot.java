af229ad5655ad22e73c3b637796a825409c95ac7
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-11 22:06:46 +0300

    Lesson 3 homework

--- src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/Robot.java
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/Robot.java
@@ -1,15 +1,14 @@
 package student_andrei_kholiavskii.lesson_3_oop_first_look.level_1_intern;
 
 class Robot {
-    public Robot() {
+     String robotName;
 
+
+    public Robot(String robotName) { this.robotName = robotName;}
+
+    public void sayHello() {System.out.println("Hello!");
     }
 
-    public Robot(String firstRobot) {
-    }
-
-    public void sayHello() {
-System.out.println("Hello!");
-    }
+    public void sayYourName() {System.out.println("My name is " + robotName);}
 
 }
--- src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/RobotDemo.java
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_1_intern/RobotDemo.java
@@ -2,7 +2,12 @@ package student_andrei_kholiavskii.lesson_3_oop_first_look.level_1_intern;
 
 public class RobotDemo {
     public static void main(String[] args) {
-       Robot firstRobot = new Robot("firstRobot");
+       Robot firstRobot = new Robot("Robo");
        firstRobot.sayHello();
+       firstRobot.sayYourName();
+
+       Robot secondRobot = new Robot("RoboCop");
+       secondRobot.sayHello();
+       secondRobot.sayYourName();
     }
 }
