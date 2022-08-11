d4d107341d27cd8bd52e6795960eae0bede7298f
Ernest <ernests.aleskevics@gmail.com>
2022-08-11 17:36:07 +0300

    lesson 2 task 18

---
+++ src/main/java/student_ernest_aleskevics/lesson_2/homework/level_5_middle/task_18/SizeMatters.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_2.homework.level_5_middle.task_18;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
