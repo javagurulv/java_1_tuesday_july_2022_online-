404bf96959c7e17f5f1e32a7d0cf2ecfdfcd44de
Ernest <ernests.aleskevics@gmail.com>
2022-08-11 17:40:41 +0300

    lesson 2 task 23

---
+++ src/main/java/student_ernest_aleskevics/lesson_2/homework/level_6_middle/task_23/AttentionPlease.java
@@ -0,0 +1,7 @@
+package student_ernest_aleskevics.lesson_2.homework.level_6_middle.task_23;
+
+public class AttentionPlease {
+    public static void main(String[] args){
+        System.out.println("Code outside the method will not compile!");
+    }
+}
