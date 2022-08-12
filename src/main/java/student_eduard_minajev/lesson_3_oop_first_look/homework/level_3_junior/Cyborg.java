7a11b6d5e947d4f503dabdefd66dfd6086bbc52d
Edwrd <Billionare225>
2022-08-12 22:36:47 +0300

    Lesson 3 Level 3

---
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_3_junior/Cyborg.java
@@ -0,0 +1,24 @@
+package student_eduard_minajev.lesson_3_oop_first_look.homework.level_3_junior;
+
+public class Cyborg {
+
+    String cyborgName;
+    String cyborgStatus;
+
+    Cyborg (String cyborgName, String cyborgStatus) {
+        this.cyborgName=cyborgName;
+        this.cyborgStatus=cyborgStatus;
+    }
+    void nameReport() {System.out.println(this.cyborgName + " says- This unit name is: " + this.cyborgName);}
+    void statusReport() {System.out.println(this.cyborgName + " says - This unit status is: " + this.cyborgStatus);}
+
+    void reName (String newCyborgName) {
+        this.cyborgName=newCyborgName;
+    }
+
+    void setNewStatus (String newCyborgStatus) {
+        this.cyborgStatus=newCyborgStatus;
+    }
+
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_3_junior/CyborgOps.java
@@ -0,0 +1,32 @@
+package student_eduard_minajev.lesson_3_oop_first_look.homework.level_3_junior;
+import java.util.Scanner;
+public class CyborgOps {
+    public static void main(String[] args){
+        Scanner consoleReader = new Scanner(System.in);
+        Cyborg cyborg1 = new Cyborg("NO NAME","IDLE");
+        Cyborg cyborg2 = new Cyborg("NO NAME","IDLE");
+        System.out.println("Please give first cyborg a name!");
+        String reNameCyborg = consoleReader.nextLine();
+        cyborg1.reName(reNameCyborg);
+        cyborg1.nameReport();
+        cyborg1.statusReport();
+        System.out.println("Please give cyborg 1 an order!");
+        String setNewStatus = consoleReader.nextLine();
+        cyborg1.setNewStatus(setNewStatus);
+        cyborg1.statusReport();
+        System.out.println("Please give a name for the second cyborg!");
+        String reNameCyborg2 = consoleReader.nextLine();
+        cyborg2.reName(reNameCyborg2);
+        cyborg2.nameReport();
+        cyborg2.statusReport();
+        System.out.println("Please give cyborg 2 an order!");
+        String setNewStatus2 = consoleReader.nextLine();
+        cyborg2.setNewStatus(setNewStatus2);
+        cyborg2.statusReport();
+        System.out.println("All cyborgs are named and deployed on a mission!");
+
+
+    }
+
+
+}
