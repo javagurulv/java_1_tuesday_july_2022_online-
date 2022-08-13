1584166a5206307b0d3e20bb56ba8e0bf36b9254
Edwrd <Billionare225>
2022-08-13 23:07:02 +0300

    Lesson 3 Level 6. DogDemo включает в себя все действия + имеет интерактивность

---
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_5_middle/Dog.java
@@ -0,0 +1,33 @@
+package student_eduard_minajev.lesson_3_oop_first_look.homework.level_5_middle;
+
+public class Dog {
+    String dogName;
+    int dogAge;
+    String dogHair;
+
+    Dog (String dogName, int dogAge, String dogHair){
+        this.dogName=dogName;
+        this.dogAge=dogAge;
+        this.dogHair=dogHair;
+    }
+    String sayDogName() {return this.dogName;}
+
+    String sayDogHair() {return this.dogHair;}
+
+    int sayDogAge() {return this.dogAge;}
+
+    void changeHair(String newDogHair) {
+        this.dogHair=newDogHair;
+        System.out.println(dogName + " has now " + dogHair + " hair.");
+    }
+
+    void celebrateBirthday(){
+        this.dogAge=dogAge + 1;
+        System.out.println(dogName + " is now " + dogAge + " years old and having an after-party hangover...");
+    }
+
+     void dogTellEverything() {
+        System.out.println(dogName + " is " + dogAge + " years old and has " + dogHair + " hair.");}
+
+
+}
