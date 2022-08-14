3303a0f51730c35b4d87dba0db1a7abeb49f696c
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-14 22:31:10 +0300

    Lesson 3 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_2_intern/task_8/HiYourName.java
@@ -0,0 +1,11 @@
+package student_andrei_kholiavskii.lesson_3_oop_first_look.level_2_intern.task_8;
+
+class HiYourName {
+
+    public static void main(String[] args){
+
+        String userName = "Andrei";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+    }
+}
