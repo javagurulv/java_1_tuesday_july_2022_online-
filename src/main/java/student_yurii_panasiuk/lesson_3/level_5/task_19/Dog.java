e169cd9d3146c406f1a53ecf42c2f6f5a18b13de
Yurii <thoryur@gmail.com>
2022-08-14 05:45:17 +0200

    Lesson 3, level 4

---
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_5/task_19/Dog.java
@@ -0,0 +1,16 @@
+package student_yurii_panasiuk.lesson_3.level_5.task_19;
+
+class Dog {
+
+    String nick = "Dog";
+
+    Dog(String nick) {this.nick = nick;}
+
+    public Dog() {
+
+    }
+
+    void voice ()  {System.out.println(nick + " " + nick + " " + nick);}
+
+
+}
