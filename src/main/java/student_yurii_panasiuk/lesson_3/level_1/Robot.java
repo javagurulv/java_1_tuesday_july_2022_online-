fae60df99e55e177ed8cb56a792324151b615f9b
Yurii <thoryur@gmail.com>
2022-08-14 03:58:01 +0200

    Lesson 3, level 0-2

---
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_1/Robot.java
@@ -0,0 +1,13 @@
+package student_yurii_panasiuk.lesson_3.level_1;
+
+public class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+}
---
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_2/Task_7.java
@@ -0,0 +1,11 @@
+package student_yurii_panasiuk.lesson_3.level_2;
+
+public class Task_7 {
+
+
+    public static void main(String[] args) {
+        String myName = "Yurii";
+        System.out.println(myName);
+
+          }
+}
