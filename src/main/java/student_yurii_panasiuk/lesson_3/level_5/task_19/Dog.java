5f0ae2ef352d00f1a86ef6ae26c5c8bb648a556f
Yurii <thoryur@gmail.com>
2022-08-14 09:20:00 +0200

    Lesson 3, level 5

--- src/main/java/student_yurii_panasiuk/lesson_3/level_5/task_19/Dog.java
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_5/task_19/Dog.java
@@ -1,16 +1,17 @@
 package student_yurii_panasiuk.lesson_3.level_5.task_19;
 
-class Dog {
+class DogTask19 {
 
     String nick = "Dog";
+    int dogAge = 1;
 
-    Dog(String nick) {this.nick = nick;}
+    DogTask19 (String nick, int dogAge) {this.nick = nick; this.dogAge = dogAge;}
 
-    public Dog() {
+    public DogTask19() {
 
     }
 
-    void voice ()  {System.out.println(nick + " " + nick + " " + nick);}
+    void voice ()  {System.out.println (nick + " " + dogAge + " years old");}
 
 
 }
