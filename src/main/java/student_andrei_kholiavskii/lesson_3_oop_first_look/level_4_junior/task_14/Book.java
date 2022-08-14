32cc7b180272a0e0beb575ba1bc4f000d051a393
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-15 00:01:59 +0300

    Lesson 3 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_4_junior/task_14/Book.java
@@ -0,0 +1,15 @@
+package student_andrei_kholiavskii.lesson_3_oop_first_look.level_4_junior.task_14;
+
+public class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
