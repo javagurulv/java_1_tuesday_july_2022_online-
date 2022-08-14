79c7a9fab2bd3d85187ce094bc54650b97137ee9
vsavonins <viktors.savonins@proofit.lv>
2022-08-14 11:43:02 +0300

    code review

--- src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/Robot.java
+++ src/main/java/student_eduard_minajev/lesson_3_oop_first_look/homework/level_1_intern/tasks_1_till_6/Robot.java
@@ -1,5 +1,8 @@
 package student_eduard_minajev.lesson_3_oop_first_look.homework.level_1_intern.tasks_1_till_6;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
  class Robot {
 
      String name;
--- src/main/java/student_ernest_aleskevics/lesson_2/homework/level_6_middle/task_24/NoSleepNoFocus.java
+++ src/main/java/student_ernest_aleskevics/lesson_2/homework/level_6_middle/task_24/NoSleepNoFocus.java
@@ -1,5 +1,8 @@
 package student_ernest_aleskevics.lesson_2.homework.level_6_middle.task_24;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class NoSleepNoFocus {
     public static void main(String[] args) {
         System.out.println("I wanna coffee");
