d5c9c60e464afa47548b89ce59d9ab71fda0f386
Yurii <thoryur@gmail.com>
2022-08-14 05:36:37 +0200

    Lesson 3, level 4

---
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,12 @@
+package student_yurii_panasiuk.lesson_3.level_5.task_18;
+
+public class DogDemo {
+
+    public static void main(String[] args){
+
+        Dog dog1 = new Dog();
+        dog1.voice();
+
+    }
+
+}
