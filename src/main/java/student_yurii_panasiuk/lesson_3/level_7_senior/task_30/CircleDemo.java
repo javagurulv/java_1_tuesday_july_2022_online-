ebabf6575061ec0af01bea404da8a82bc2ed44ab
Yurii <thoryur@gmail.com>
2022-08-14 11:07:30 +0200

    Lesson 3, level 7

---
+++ src/main/java/student_yurii_panasiuk/lesson_3/level_7_senior/task_30/CircleDemo.java
@@ -0,0 +1,10 @@
+package student_yurii_panasiuk.lesson_3.level_7_senior.task_30;
+
+public class CircleDemo {
+
+    public static void main(String[] args){
+
+    Circle newCircle = new Circle(1.5);
+    newCircle.circleArea();
+    }
+}
