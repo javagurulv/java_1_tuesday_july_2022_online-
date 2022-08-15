3c0784333642bd2f715b97e45bb84d822bde6cf0
Ernest <ernests.aleskevics@gmail.com>
2022-08-15 22:29:10 +0300

    lesson 3 task 1-6 ROBOT

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/robot/RobotDemo.java
@@ -0,0 +1,15 @@
+package student_ernest_aleskevics.lesson_3.homework.robot;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robotOne = new Robot( "robotOne");
+        robotOne.sayHello();
+        robotOne.sayYourName();
+
+        Robot robotTwo = new Robot("robotTwo");
+        robotTwo.sayHello();
+        robotTwo.sayYourName();
+
+    }
+}
