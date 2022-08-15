e6ff4fa322f4eacf93a5539b2ebd43a82d156abf
Ernest <ernests.aleskevics@gmail.com>
2022-08-15 22:36:31 +0300

    lesson 3 task 8

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_8/YourName.java
@@ -0,0 +1,10 @@
+package student_ernest_aleskevics.lesson_3.homework.task_8;
+
+public class YourName {
+    public static void main(String[] args) {
+        String userName = "Ernests";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+
+    }
+}
