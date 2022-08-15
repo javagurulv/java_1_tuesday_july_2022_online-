04de6f8294b4719d2ec535764937928b03ae218c
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-15 14:06:21 +0300

    TransactionTypeDemo stub

---
+++ src/main/java/student_levs_blinnikovs/lesson_x/bank_account/part_1_basic/task_1_x/TransactionTypeDemo.java
@@ -0,0 +1,28 @@
+package student_levs_blinnikovs.lesson_x.bank_account.part_1_basic.task_1_x;
+
+/*
+Работа с enum TransactionType.
+
+Создайте класс TransactionTypeDemo и в нём попробуйте выполнить следующие операции:
+
+- объявление переменной типа enum:
+    TransactionType deposit = TransactionType.DEPOSIT;
+    TransactionType withdrawal = TransactionType.WITHDRAWAL;
+    TransactionType transactionType = TransactionType.WITHDRAWAL;
+- присвоение нового заначения переменной типа enum:
+    TransactionType transactionType = TransactionType.WITHDRAWAL;
+    transactionType = TransactionType.DEPOSIT;
+- сравнение переменной типа enum:
+    TransactionType transactionType = TransactionType.WITHDRAWAL;
+    if (transactionType == TransactionType.WITHDRAWAL) {
+        System.out.println("This transaction is withdrawal transaction!");
+    }
+    transactionType = TransactionType.DEPOSIT;
+    if (transactionType == TransactionType.DEPOSIT) {
+        System.out.println("This transaction is deposit transaction!");
+    }
+
+ */
+
+class TransactionTypeDemo {
+}
