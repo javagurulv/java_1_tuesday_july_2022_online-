4842d86379fb935d928791ed8c29aa0c1d6975a9
sobitjon <seshniyozov@list.ru>
2022-08-16 00:18:42 +0500

    lesson 3 homework task 1-6

---
+++ src/main/java/student_sobit_eshniyozov/lesson_3/HelloWorld.java
@@ -0,0 +1,4 @@
+package student_sobit_eshniyozov.lesson_3;
+
+public class HelloWorld {
+}
---
+++ src/main/java/student_sobit_eshniyozov/lesson_3/level_0/ThanksForMativation.java
@@ -0,0 +1,7 @@
+package student_sobit_eshniyozov.lesson_3.level_0;
+
+public class ThanksForMativation {
+    public static void main(String[] args) {
+        System.out.println("Spasibo za mativatsiyu!!!");
+    }
+}
