6e0737abab9d5eb20640bdcadf588c03955b7560
Ernest <ernests.aleskevics@gmail.com>
2022-08-15 23:01:01 +0300

    lesson 3 task 15

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_15/Phone.java
@@ -0,0 +1,9 @@
+package student_ernest_aleskevics.lesson_3.homework.task_15;
+
+class Phone {
+    String model;
+
+        Phone(String newModel) {this.model = newModel;}
+
+        String getModel() {return this.model;}
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_15/PhoneDemo.java
@@ -0,0 +1,9 @@
+package student_ernest_aleskevics.lesson_3.homework.task_15;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+}
