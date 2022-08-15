143513d8071e4bea96cfe42200647b227ae13f8b
olegsradigins <oradigins@gmail.com>
2022-08-15 20:42:32 +0300

    Lesson 3 level 7 Task 30

---
+++ src/main/java/student_olegs_radigins/lesson_3/level7/task_30/CircleDemo.java
@@ -0,0 +1,12 @@
+package student_olegs_radigins.lesson_3.level7.task_30;
+
+class CircleDemo {
+    public static void main(String[] args) {
+        Circle cirkle1 = new Circle(5);
+        cirkle1.calculateArea();
+
+        Circle cirkle2 = new Circle(5.5);
+        cirkle2.calculateArea();
+
+    }
+}
