59b1e5d141bb7c49f328eb3d599fd867b2f7c64c
Ernest <ernests.aleskevics@gmail.com>
2022-08-15 23:05:17 +0300

    lesson 3 task 16

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_16/CarDemo.java
@@ -0,0 +1,9 @@
+package student_ernest_aleskevics.lesson_3.homework.task_16;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+}
