2bc8f3b8e6528e5d876aa0e39143125b916ca3d3
Ivan Mostepanov <neomess13@gmail.com>
2022-08-16 18:40:08 +0300

    Lesson 1 homework 1

--- src/main/java/student_eduard_minajev/lesson_2_variables/homework/level_7/task_28/BankTransaction.java
+++
@@ -1,7 +0,0 @@
-package student_eduard_minajev.lesson_2_variables.homework.level_7.task_28;
-
-import teacher.codereview.CodeReview;
-
-@CodeReview(approved = true)
-public class BankTransaction {
-}
