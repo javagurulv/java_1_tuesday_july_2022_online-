dbc0034940bd6f84ac26f99218e80d3bc8161cba
Ernest <ernests.aleskevics@gmail.com>
2022-08-16 16:55:30 +0300

    lesson 3 task 17

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_17/BankAccount.java
@@ -0,0 +1,26 @@
+package student_ernest_aleskevics.lesson_3.homework.task_17;
+
+class BankAccount {
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerFirstName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
