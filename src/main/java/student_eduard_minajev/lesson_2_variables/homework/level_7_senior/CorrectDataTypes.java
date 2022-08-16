517dfc1f13f7b7a26c156928d61b1e38842da155
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-16 20:59:43 +0300

    Fix Eduards classes after - adding packages and classes back

---
+++ src/main/java/student_eduard_minajev/lesson_2_variables/homework/level_7_senior/CorrectDataTypes.java
@@ -0,0 +1,17 @@
+package student_eduard_minajev.lesson_2_variables.homework.level_7_senior;
+
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
+public class CorrectDataTypes {
+    public static void main(String[] args){
+        int numberOne = 10;
+        double numberTwo = 20.0D;
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        double sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+
+        }
+    }
+
---
+++ src/main/java/student_eduard_minajev/lesson_2_variables/homework/level_7_senior/SumTwoNumbers.java
@@ -0,0 +1,18 @@
+package student_eduard_minajev.lesson_2_variables.homework.level_7_senior;
+
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
+public class SumTwoNumbers {
+    public static void main(String[] args){
+        int numberOne = 10;
+        int numberTwo = 20;
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+        int sum = numberOne + numberTwo;
+        System.out.println("Sum = " + sum);
+
+    }
+
+
+}
