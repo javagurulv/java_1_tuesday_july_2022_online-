df5e0afe56f11ff68caa8955b0905d05383b462a
vsavonins <viktors.savonins@proofit.lv>
2022-08-16 09:20:27 +0300

    fix

--- src/main/java/student_sobit_eshniyozov/lesson_3/level_0/ThanksForMotivation.java
+++ src/main/java/student_sobit_eshniyozov/lesson_3/level_0/ThanksForMotivation.java
@@ -1,6 +1,6 @@
 package student_sobit_eshniyozov.lesson_3.level_0;
 
-public class ThanksForMativation {
+public class ThanksForMotivation {
     public static void main(String[] args) {
         System.out.println("Spasibo za mativatsiyu!!!");
     }
