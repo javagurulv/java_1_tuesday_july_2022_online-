65e9a608de01958f8e09c7fbc1c9ce95ecbfeab7
Ernest <ernests.aleskevics@gmail.com>
2022-08-16 17:50:40 +0300

    lesson 3 task 18

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_18/Dog.java
@@ -0,0 +1,13 @@
+package student_ernest_aleskevics.lesson_3.homework.task_18;
+
+class Dog {
+    String nickName;
+    Dog(String nickName) {
+        this.nickName = nickName;
+    }
+    void voice(){
+        System.out.println("My nickname = " + this.nickName);
+    }
+
+
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_18/DogDemo.java
@@ -0,0 +1,11 @@
+package student_ernest_aleskevics.lesson_3.homework.task_18;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog neo = new Dog ("Neo");
+        neo.voice();
+        neo.voice();
+        neo.voice();
+
+    }
+}
