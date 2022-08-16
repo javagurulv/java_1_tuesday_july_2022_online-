623b468667a8d08890ca2ab2ad0aea41ff5695e4
Ernest <ernests.aleskevics@gmail.com>
2022-08-16 17:58:28 +0300

    lesson 3 task 19

--- src/main/java/student_ernest_aleskevics/lesson_3/homework/task_18/DogDemo.java
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_18/DogDemo.java
@@ -2,9 +2,7 @@ package student_ernest_aleskevics.lesson_3.homework.task_18;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog neo = new Dog ("Neo");
-        neo.voice();
-        neo.voice();
+        Dog neo = new Dog ("Neo", 8);
         neo.voice();
 
     }
