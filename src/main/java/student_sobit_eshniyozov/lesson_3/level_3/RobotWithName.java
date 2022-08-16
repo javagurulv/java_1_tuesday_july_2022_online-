4e58e4381f773fe7d712e4a1680c20cb7adf7ad7
sobitjon <seshniyozov@list.ru>
2022-08-16 19:15:48 +0500

    homework lesson  3

--- src/main/java/student_sobit_eshniyozov/lesson_3/level_3/RobotWithName.java
+++ src/main/java/student_sobit_eshniyozov/lesson_3/level_3/RobotWithName.java
@@ -5,13 +5,10 @@ public class RobotWithName {
     String name;
     public RobotWithName(String nickname){
         this.name=nickname;
-
     }
     public void sayHello(){
         System.out.println(this.name + ": Hello!");
-
     }
-
     public void sayYourName() {
         System.out.println(this.name + ": My name is "+this.name);
 
