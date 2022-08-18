dd140b9c0b751e601411aed5f2806350e5e7ff43
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-18 21:03:58 +0300

    Lesson 3 homework

---
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_5_middle/task_22/DogDemo.java
@@ -0,0 +1,11 @@
+package student_andrei_kholiavskii.lesson_3_oop_first_look.level_5_middle.task_22;
+
+class DogDemo {
+    public static void main(String[] args){
+
+    Dog dog1 = new Dog("Smokie", 7, "yellow");
+    dog1.voice();
+    dog1.changeColour("black");
+    dog1.voice();
+    }
+}
