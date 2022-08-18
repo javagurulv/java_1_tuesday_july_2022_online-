1d768e1a441246a4ddbe9975755c72d71685149d
Andrei Kholiavskii <andrejs.holavskis@gmail.com>
2022-08-18 20:20:11 +0300

    Lesson 3 homework

--- src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_5_middle/task_21/Dog.java
+++ src/main/java/student_andrei_kholiavskii/lesson_3_oop_first_look/level_5_middle/task_21/Dog.java
@@ -2,6 +2,19 @@ package student_andrei_kholiavskii.lesson_3_oop_first_look.level_5_middle.task_2
 
 class Dog {
 
+    String dogName;
+    int dogAge;
+
+    String dogColour;
+
+    Dog(String dogName, int dogAge, String dogColour) {
+
+        this.dogName = dogName;
+        this.dogAge = dogAge;
+        this.dogColour = dogColour;}
+    void voice() {
+
+        System.out.println(dogName + " " + dogAge + " " + dogColour);}
 
 
 }
