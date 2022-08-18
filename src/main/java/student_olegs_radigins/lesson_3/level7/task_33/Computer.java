0f977790c16916a5bf03f31fa8e5c08f45981b56
olegsradigins <oradigins@gmail.com>
2022-08-18 04:52:12 +0300

    Lesson 3 level 7 Task 33

---
+++ src/main/java/student_olegs_radigins/lesson_3/level7/task_33/Computer.java
@@ -0,0 +1,19 @@
+package student_olegs_radigins.lesson_3.level7.task_33;
+
+class Computer {
+    String manufacturer;
+    String model;
+
+    Computer(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    String getManufacturer() {
+        return manufacturer;
+    }
+
+    String getModel() {
+        return model;
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_3/level7/task_33/ComputerDemo.java
@@ -0,0 +1,11 @@
+package student_olegs_radigins.lesson_3.level7.task_33;
+
+class ComputerDemo {
+    public static void main(String[] args) {
+        Computer dell = new Computer("Dell", "XXX-1");
+        String manufacturer = dell.getManufacturer();
+        String model = dell.getModel();
+        System.out.println("Computer manufacturer = " + manufacturer);
+        System.out.println("Computer model = " + model);
+    }
+}
