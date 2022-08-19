3624215efb50769662f8b39ae05df88e317de725
sobitjon <seshniyozov@list.ru>
2022-08-19 21:29:01 +0500

    lesson 3 homework ArchideyaFull

--- src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/Archideya.java
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/Archideya.java
@@ -1,7 +1,7 @@
 package student_sobit_eshniyozov.lesson_3_oop_first_look.level_7;
 
 
-// poka v stadiee razrabotki poka mojno ne proveryat!
+
 
 class Archideya {
     double height;
@@ -19,10 +19,23 @@ class Archideya {
     void parameters() {
         System.out.println("About "+this.variety+":\n" + "Variety is: " + this.variety + " Height is: " + this.height + " Number of leaves: " + this.leaves + " Age of flower: " + this.age);
     }
+
+    Archideya[] allArchideya;
+
+    Archideya(Archideya[] allArchideya){
+        this.allArchideya=allArchideya;
+    }
+
     void halfAYearLater(){
-        System.out.println("Half a year later");
         this.height = this.height+0.2;
         this.age = this.age+0.5;
         this.leaves = this.leaves+2;
         }
+
+    void halfAYearLaterForAll(){
+      for(Archideya archideya: allArchideya){
+          archideya.halfAYearLater();
+      }
+        System.out.println("Half a year later");
+    }
    }
--- src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/ArchideyaDemo.java
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/ArchideyaDemo.java
@@ -1,10 +1,13 @@
 package student_sobit_eshniyozov.lesson_3_oop_first_look.level_7;
 
-// poka v stadiee razrabotki poka mojno ne proveryat!
+
 public class ArchideyaDemo {
     public static void main(String[] args) {
         Archideya flower1 = new Archideya("Schilleriana", 15, 6, 2);
         Archideya flower2 = new Archideya("Vanda",12,4,1.5);
+
+        Archideya[] allArchideya = {flower1, flower2};
+        Archideya test = new Archideya(allArchideya);
 
         System.out.println("\n"+"===========================");
 
@@ -12,9 +15,9 @@ public class ArchideyaDemo {
         System.out.println("\n"+"---------------------------");
         flower2.parameters();
         System.out.println("\n"+"---------------------------");
-        flower1.halfAYearLater(); flower2.halfAYearLater();
-        flower1.halfAYearLater(); flower2.halfAYearLater();
-        flower1.halfAYearLater(); flower2.halfAYearLater();
+        test.halfAYearLaterForAll();
+        test.halfAYearLaterForAll();
+        test.halfAYearLaterForAll();
 
         System.out.println("\n"+"===========================");
         flower1.parameters();
