8e6aa84883aa262fbcd95aabd7a9dccca2678171
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-19 09:15:55 +0300

    Orchid - cleanup

---
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7_levs/OrchidDemo.java
@@ -0,0 +1,34 @@
+package student_sobit_eshniyozov.lesson_3_oop_first_look.level_7_levs;
+
+// poka v stadiee razrabotki poka mojno ne proveryat!
+public class OrchidDemo {
+    public static void main(String[] args) {
+        Orchid flower1 = new Orchid("Schilleriana", 15, 6, 2);
+        Orchid flower2 = new Orchid("Vanda",12,4,1.5);
+
+        System.out.println("\n"+"===========================");
+
+        flower1.printParameters();
+        System.out.println("\n"+"---------------------------");
+
+        flower2.printParameters();
+        System.out.println("\n"+"---------------------------");  // тут добавил отступов для читабельности
+
+        flower1.halfAYearLater();                           // тут были вызовы методов в строчку. Так не пишут. Для лучшей читабельности всё со своей строки
+        flower1.halfAYearLater();
+        flower1.halfAYearLater();
+
+        flower2.halfAYearLater();
+        flower2.halfAYearLater();
+        flower2.halfAYearLater();
+
+        System.out.println("\n"+"===========================");
+        flower1.printParameters();
+
+        System.out.println("\n"+"---------------------------");
+        flower2.printParameters();
+
+        System.out.println("\n"+"---------------------------");
+    }
+}
+
