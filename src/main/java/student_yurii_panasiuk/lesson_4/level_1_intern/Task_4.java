247261939b45e8f5cf728447a743d2ac48de2125
Yurii <thoryur@gmail.com>
2022-08-19 18:51:15 +0200

    Lesson 4, level 1, task 4

---
+++ src/main/java/student_yurii_panasiuk/lesson_4/level_1_intern/Task_4.java
@@ -0,0 +1,17 @@
+package student_yurii_panasiuk.lesson_4.level_1_intern;
+
+import java.util.Scanner;
+
+public class Task_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите чило");
+        Scanner scanner = new Scanner(System.in);
+        double number = scanner.nextDouble();
+
+        if ((number % 2) == 0) { System.out.println("вы ввели четное число"); }
+
+        else { System.out.println("вы ввели нечетное число"); }
+    }
+
+}
--- src/main/java/student_yurii_panasiuk/runaway_student/Teacher.java
+++ src/main/java/student_yurii_panasiuk/runaway_student/Teacher.java
@@ -1,7 +1,7 @@
 package student_yurii_panasiuk.runaway_student;
 
 
-// создаем учителя [Х1, Х2, Y1, Y2, R]
+// создаем учителя [Х1, Х2, Y1, Y2, радиус бассейна]
 
 class Teacher {
 
