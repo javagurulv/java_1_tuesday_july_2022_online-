3603528afee0f0a80c2fc37895572fe1b007a832
sobitjon <seshniyozov@list.ru>
2022-08-19 23:02:50 +0500

    lesson 3 homework Task 30 Circle

---
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/Circle.java
@@ -0,0 +1,18 @@
+package student_sobit_eshniyozov.lesson_3_oop_first_look.level_7;
+import java.lang.Math;
+
+public class Circle {
+
+    double radius;
+    double pi = 3.14;
+    Circle(double radius){
+        this.radius = radius;
+        }
+    void calculateArea(){
+
+        double result=this.pi*Math.pow(this.radius,2);
+        System.out.println("Area of circle is: "+result);
+    }
+
+
+}
---
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/CircleDemo.java
@@ -0,0 +1,10 @@
+package student_sobit_eshniyozov.lesson_3_oop_first_look.level_7;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        Circle circle2 = new Circle(10);
+        circle1.calculateArea();
+        circle2.calculateArea();
+    }
+}
