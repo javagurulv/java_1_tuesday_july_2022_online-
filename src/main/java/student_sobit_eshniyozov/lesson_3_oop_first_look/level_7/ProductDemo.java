a80f6f6b1a10a30f5643c26dcf5c61f359ec8706
sobitjon <seshniyozov@list.ru>
2022-08-20 00:08:26 +0500

    lesson 3 homework Task 31 Product

--- src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/ProductDemo.java
+++ src/main/java/student_sobit_eshniyozov/lesson_3_oop_first_look/level_7/ProductDemo.java
@@ -4,11 +4,11 @@ class ProductDemo {
     public static void main(String[] args) {
         Product product1 = new Product("Potato");
 
-        System.out.println("=================================================================================");
+        System.out.println("=====================================**============================================");
         product1.setRegularPrice(100);
         product1.setDiscount(15);
         product1.actualPrice();
         product1.printInformation();
-        System.out.println("=================================================================================");
+        System.out.println("=====================================**============================================");
     }
 }
