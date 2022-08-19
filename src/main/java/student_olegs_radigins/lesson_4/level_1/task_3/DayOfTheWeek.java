fe5b8f38d811f858af6ef34cce07db216ca154da
olegsradigins <oradigins@gmail.com>
2022-08-19 05:09:25 +0300

    Lesson 4 level 1 Task 3

---
+++ src/main/java/student_olegs_radigins/lesson_4/level_1/task_3/DayOfTheWeek.java
@@ -0,0 +1,31 @@
+package student_olegs_radigins.lesson_4.level_1.task_3;
+
+import java.util.Scanner;
+
+class DayOfTheWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number from 1 to 7: ");
+        int dayOfTheWeek = scanner.nextInt();
+
+        switch (dayOfTheWeek) {
+            case 1: System.out.println("Monday");
+                break;
+            case 2: System.out.println("Tuesday");
+                break;
+            case 3: System.out.println("Wednesday");
+                break;
+            case 4: System.out.println("Thursday");
+                break;
+            case 5: System.out.println("Friday");
+                break;
+            case 6: System.out.println("Saturday");
+                break;
+            case 7: System.out.println("Sunday");
+                break;
+            default: System.out.println("No such day of the week!");
+        }
+
+
+    }
+}
