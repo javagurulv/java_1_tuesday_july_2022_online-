18ff49efc9676387a6b43d1bd862fdc67d102dac
Yurii <thoryur@gmail.com>
2022-08-20 22:39:16 +0200

    Lesson 4, level 5, Task 16

---
+++ src/main/java/student_yurii_panasiuk/lesson_4/level_4_junior/Task_16/Demo.java
@@ -0,0 +1,15 @@
+package student_yurii_panasiuk.lesson_4.level_4_junior.Task_16;
+
+class Demo {
+
+    public static void main(String[] args) {
+
+        LightColorDetector detector = new LightColorDetector();
+
+        detector.detect(385);
+
+    }
+
+
+
+}
---
+++ src/main/java/student_yurii_panasiuk/lesson_4/level_4_junior/Task_16/LightColorDetector.java
@@ -0,0 +1,21 @@
+package student_yurii_panasiuk.lesson_4.level_4_junior.Task_16;
+
+class LightColorDetector {
+
+ void  detect(int wavelength) {
+        //TODO
+        if (wavelength < 380 || wavelength > 750) {System.out.println("Invisible Light");}
+         else if (wavelength >= 380 && wavelength <= 449) {System.out.println("Violet");}
+          else if (wavelength >= 450 && wavelength <= 494) {System.out.println("Blue");}
+           else if (wavelength >= 495 && wavelength <= 569) {System.out.println("Green");}
+            else if (wavelength >= 570 && wavelength <= 589) {System.out.println("Yellow");}
+             else if (wavelength >= 590 && wavelength <= 619) {System.out.println("Orange");}
+              else if (wavelength >= 620 && wavelength <= 750) {System.out.println("Red");} // тут можно убрать лишнее но так нагляднее
+
+        }
+
+}
+
+
+
+
