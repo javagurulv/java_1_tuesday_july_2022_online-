956fba1493ca87d629d765390f66c14deaf06489
Edwrd <Billionare225>
2022-08-20 14:26:28 +0300

    Lesson 4, task seven

---
+++ src/main/java/student_eduard_minajev/lesson_4_if_statement/level_2_intern/Task7.java
@@ -0,0 +1,25 @@
+package student_eduard_minajev.lesson_4_if_statement.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please enter first number!");
+        Scanner numberA = new Scanner(System.in);
+        int numberOne = numberA.nextInt();
+
+        System.out.println("Please enter second number");
+        Scanner numberB = new Scanner(System.in);
+        int numberTwo = numberB.nextInt();
+
+        if (numberOne == numberTwo){
+            System.out.println("Numbers are equal!");
+        }else {
+            System.out.println("Numbers are different!");
+        }
+
+
+    }
+}
