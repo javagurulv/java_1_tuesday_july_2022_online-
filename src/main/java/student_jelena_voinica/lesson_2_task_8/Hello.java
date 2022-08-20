a103a9df3b5a70c3bc6865dcb2e246202034b174
Uguha21 <Agasem2106@icloud.com>
2022-08-20 17:21:14 +0300

    lesson_2_task_8

---
+++ src/main/java/student_jelena_voinica/lesson_2_task_8/Hello.java
@@ -0,0 +1,8 @@
+package student_jelena_voinica.lesson_2_task_8;
+
+public class Hello {
+    public static void main(String[]args){
+        String myName="Jelena";
+        System.out.println("Hello Jelena");
+    }
+}
