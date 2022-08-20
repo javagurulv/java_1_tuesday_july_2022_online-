12c0d0fe8b3f79546e20335dc86000e88fb5b564
olegsradigins <oradigins@gmail.com>
2022-08-20 09:35:18 +0300

    Lesson 4 level 1 Task 4

---
+++ src/main/java/student_olegs_radigins/lesson_4/level_1/task_4/Number.java
@@ -0,0 +1,16 @@
+package student_olegs_radigins.lesson_4.level_1.task_4;
+
+import java.util.Scanner;
+
+class Number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int dayOfTheWeek = scanner.nextInt();
+        if(dayOfTheWeek%2 == 0){
+            System.out.println("You entered an even number.");
+        }else {
+            System.out.println("You enteres an odd number.");
+        }
+    }
+}
