f97aa6c11a9c4aaa568630b5a2b71959e57e7d40
Ivan Mostepanov <neomess13@gmail.com>
2022-08-22 16:02:51 +0300

    lesson_4 Task_1-6

--- src/main/java/student_ivan_mostepanov/lesson_3/TaskNr.java
+++
@@ -1,4 +0,0 @@
-package student_ivan_mostepanov.lesson_3;
-
-public class TaskNr {
-}
---
+++ src/main/java/student_ivan_mostepanov/lesson_3/TaskNr_7_8.java
@@ -0,0 +1,19 @@
+package student_ivan_mostepanov.lesson_3;
+
+class TaskNr_7_8 {
+
+
+
+        public static void main(String[] args){
+
+            String myName = "Ivan";
+
+            System.out.println("My name is " + myName);
+            System.out.println("Hi " + myName + "!");
+
+        }
+
+
+
+
+}
