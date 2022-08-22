74f35045e37c8f2d728a658974eeb93308926ed5
Admin <roman.hytt@gmail.com>
2022-08-22 13:07:03 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/level_2_intern/Task_7.java
@@ -0,0 +1,12 @@
+package student_roman_hytt.level_2_intern;
+
+public class Task_7 {
+public static void main (String [] args){
+  String myName="Roman" ;
+    System.out.println (myName);
+
+}
+
+
+
+}
