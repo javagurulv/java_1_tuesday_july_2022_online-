dc3e39529f063d696371f1d8e373d61fc79619e1
Ivan Mostepanov <neomess13@gmail.com>
2022-08-22 16:18:33 +0300

    lesson_4 Task_1-6

---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_14/Book.java
@@ -0,0 +1,17 @@
+package student_ivan_mostepanov.lesson_3.Task_14;
+
+    class Book {
+
+        String title;
+
+        Book(String bookTitle) {
+            this.title = bookTitle;
+        }
+
+        String getTitle() {
+            return this.title;
+        }
+
+
+
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_14/BookDemo.java
@@ -0,0 +1,12 @@
+package student_ivan_mostepanov.lesson_3.Task_14;
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+
+}
