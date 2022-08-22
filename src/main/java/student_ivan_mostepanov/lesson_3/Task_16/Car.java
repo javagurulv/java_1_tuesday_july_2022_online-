4e2259f730a20742770f0c00c0674d991c01c1db
Ivan Mostepanov <neomess13@gmail.com>
2022-08-22 16:27:31 +0300

    lesson_3 Task_16

---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_16/Car.java
@@ -0,0 +1,18 @@
+package student_ivan_mostepanov.lesson_3.Task_16;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel (){
+        return this.model;
+    }
+
+
+
+
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_16/CarDemo.java
@@ -0,0 +1,13 @@
+package student_ivan_mostepanov.lesson_3.Task_16;
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+
+
+}
