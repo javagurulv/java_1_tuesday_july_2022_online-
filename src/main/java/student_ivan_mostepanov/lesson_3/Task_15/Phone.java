89048c11015d9a3309be2caac266cc15f47f1524
Ivan Mostepanov <neomess13@gmail.com>
2022-08-22 16:23:24 +0300

    lesson_4 Task_15

---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_15/Phone.java
@@ -0,0 +1,18 @@
+package student_ivan_mostepanov.lesson_3.Task_15;
+
+class Phone {
+
+
+    String model;
+
+        Phone (String newModel) {
+            this.model = newModel;
+        }
+
+        String getModel() {
+            return this.model;
+        }
+
+
+
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_15/PhoneDemo.java
@@ -0,0 +1,10 @@
+package student_ivan_mostepanov.lesson_3.Task_15;
+
+class PhoneDemo {
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
