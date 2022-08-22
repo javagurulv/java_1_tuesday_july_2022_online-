b8612d4ca34a28c23358725f38fc82fb6509a988
Admin <roman.hytt@gmail.com>
2022-08-22 12:56:49 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_3/Robot.java
@@ -0,0 +1,12 @@
+package student_roman_hytt.lesson_3;
+
+public class Robot {
+
+public void sayHello () { System.out.println ("Hello!");}
+public void sayYourName() {System.out.println ("My name is Robot");}
+public void sayGoodbye () {System.out.println ("Goodbye!");}
+}
+
+
+
+
---
+++ src/main/java/student_roman_hytt/lesson_3/RobotDemo.java
@@ -0,0 +1,20 @@
+package student_roman_hytt.lesson_3;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1= new Robot ();
+         robot1.sayHello();
+         robot1.sayYourName();
+         robot1.sayGoodbye();
+
+    }
+
+
+
+}
+
+
+
+
