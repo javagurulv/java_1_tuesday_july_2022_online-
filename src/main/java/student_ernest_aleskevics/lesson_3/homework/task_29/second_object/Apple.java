c27ae128d7bc6d3853e8a1d70ccd4872dbde9ce6
Ernest <ernests.aleskevics@gmail.com>
2022-08-22 22:35:31 +0300

    lesson 3 task 29.2

---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_29/second_object/Apple.java
@@ -0,0 +1,18 @@
+package student_ernest_aleskevics.lesson_3.homework.task_29.second_object;
+
+public class Apple {
+    String countryOfOrigin, colour;
+    double pricePerKilogram;
+
+    Apple(String countryOfOrigin, String colour, double pricePerKilogram){
+        this.countryOfOrigin = countryOfOrigin;
+        this.colour = colour;
+        this.pricePerKilogram = pricePerKilogram;
+    }
+
+    void customerTakeMoreThanOneKilogram (int kilograms) {
+        this.pricePerKilogram = pricePerKilogram * kilograms;
+        System.out.println(this.countryOfOrigin + " apple " + kilograms + " kilograms will cost " + this.pricePerKilogram);
+    }
+
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_3/homework/task_29/second_object/AppleDemo.java
@@ -0,0 +1,15 @@
+package student_ernest_aleskevics.lesson_3.homework.task_29.second_object;
+
+public class AppleDemo {
+
+    public static void main(String[] args) {
+
+        Apple spain = new Apple ("Spain", "green", 1.45);
+        spain.customerTakeMoreThanOneKilogram(3);
+
+
+        Apple turkey = new Apple("Turkey", "red", 1.69 );
+        turkey.customerTakeMoreThanOneKilogram(5);
+
+    }
+}
