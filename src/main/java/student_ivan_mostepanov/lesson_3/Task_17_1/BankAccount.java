942e3af7dddba70526f0a4412dc43af3f7d6bb9a
Ivan Mostepanov <neomess13@gmail.com>
2022-08-22 16:50:38 +0300

    lesson_3 Task_17

---
+++ src/main/java/student_ivan_mostepanov/lesson_3/Task_17_1/BankAccount.java
@@ -0,0 +1,28 @@
+package student_ivan_mostepanov.lesson_3.Task_17_1;
+
+    class BankAccount {
+
+        String ownerFirstName;
+        String ownerLastName;
+        int moneyAmount;
+
+        BankAccount(String ownerFirstName,
+                    String ownerLastName,
+                int moneyAmount) {
+            this.ownerFirstName = ownerFirstName;
+            this.ownerLastName = ownerLastName;
+            this.moneyAmount = moneyAmount;
+        }
+
+        String getOwnerFirstName() {
+            return this.ownerFirstName;
+        }
+
+        String getOwnerLastName() {
+            return this.ownerLastName;
+        }
+
+        int getMoneyAmount() {
+            return this.moneyAmount;
+        }
+}
