adf4e8a20481e119edd8fb155811b478f236b338
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-23 20:39:53 +0300

    Lesson 5 - Level 3 - Task 16

---
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_3_junior/task_16/ForLoopDefinition1.java
@@ -0,0 +1,13 @@
+package student_levs_blinnikovs.lesson_5_arrays_for_loop.level_3_junior.task_16;
+
+//Найдите синтаксическую ошибку в программе и исправьте её:
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {              // i не был инициализирован
+            System.out.println(i);
+        }
+    }
+
+}
