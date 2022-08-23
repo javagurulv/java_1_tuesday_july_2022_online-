9f1194d8edd7b8389a8cc3db0d6d2ee4759966bd
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-23 19:19:31 +0300

    Lesson 5 - Task 2 - ArrayDefinition2

---
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_1_intern/task_2/ArrayDefinition2.java
@@ -0,0 +1,13 @@
+package student_levs_blinnikovs.lesson_5_arrays_for_loop.level_1_intern.task_2;
+
+//Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10]; // либо так
+        int[] numbers1 = new int[10]; // либо так (поменял названия вариабла чтобы не было ошибки
+    }
+
+}
+
