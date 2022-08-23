5b8ebdd7c32ca060dae24ad223f6ca13849e6b6a
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-23 20:42:20 +0300

    Lesson 5 - Level 3 - Task 18

---
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_3_junior/task_18/ForLoopDefinition3.java
@@ -0,0 +1,14 @@
+package student_levs_blinnikovs.lesson_5_arrays_for_loop.level_3_junior.task_18;
+
+//Найдите синтаксическую ошибку в программе и исправьте её:
+
+class ForLoopDefinition3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {  // не было разделителя перед инкрементом
+            System.out.println(i);
+        }
+    }
+
+}
+
