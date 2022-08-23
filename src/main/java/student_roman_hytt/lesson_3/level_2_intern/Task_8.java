a6f92f376b57e36a28f790e569daf1082501d075
Admin <roman.hytt@gmail.com>
2022-08-23 13:17:29 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_3/level_2_intern/Task_8.java
@@ -0,0 +1,14 @@
+package student_roman_hytt.lesson_3.level_2_intern;
+
+public class Task_8 {
+
+    public static void main (String [] args) {
+
+        String myName= "Roman";
+        String greeteng= "Hello " + myName + "!";
+
+System.out.println (greeteng);
+
+
+    }
+}
---
+++ src/main/java/student_roman_hytt/lesson_3/level_2_intern/Task_9.java
@@ -0,0 +1,14 @@
+package student_roman_hytt.lesson_3.level_2_intern;
+
+import java.util.Scanner;
+
+public class Task_9 {
+
+    public static void main (String [] args) {
+
+       System.out.println ("what is your name?");
+        Scanner a= new Scanner (System.in);
+        String userName = a.next();
+        System.out.println("Hello "+ userName+ "!");
+    }
+}
