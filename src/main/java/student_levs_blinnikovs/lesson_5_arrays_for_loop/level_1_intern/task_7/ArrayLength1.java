0acf1a34ba606a967ca0758b4cd54036cea2759a
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-23 19:40:08 +0300

    Lesson 5 - Task 8 and Task 7 fixes

--- src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_1_intern/task_7/ArrayLength1.java
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_1_intern/task_7/ArrayLength1.java
@@ -2,7 +2,7 @@ package student_levs_blinnikovs.lesson_5_arrays_for_loop.level_1_intern.task_7;
 
 //Найдите ошибку в программе и исправить её:
 
-class ArrayLength7 {  // тут поменял на 7 но это не ошибка
+class ArrayLength1 {  // тут поменял на 7 но это не ошибка
 
     public static void main(String[] args) {
         int[] numbers = new int[2];    // тут поменял ссылку на массив целых чисел
