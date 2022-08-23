1b3756eb5e565ede705f7e41ee2dc97cacda4e4d
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-23 21:59:25 +0300

    Lesson 5 - Level 5 - Task 31 - ArrayUtil

---
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_5_middle/task_31/ArrayUtil.java
@@ -0,0 +1,15 @@
+package student_levs_blinnikovs.lesson_5_arrays_for_loop.level_5_middle.task_31;
+
+/*
+Создайте класс ArrayUtil.
+Напишите в этом классе метод для создания
+массива целых чисел указанной длины.
+ */
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+}
