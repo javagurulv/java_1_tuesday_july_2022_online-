056f61b49bd0617642442557f0654d3a9fbd408c
Admin <roman.hytt@gmail.com>
2022-08-24 14:40:14 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_3/level_4/PhoneDemo.java
@@ -0,0 +1,12 @@
+package student_roman_hytt.lesson_3.level_4;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+
+}
