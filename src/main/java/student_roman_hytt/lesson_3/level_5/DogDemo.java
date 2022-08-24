ca81c7ee9b7cef5b2948b1300bcb06890ed51a17
Admin <roman.hytt@gmail.com>
2022-08-24 15:54:02 +0300

    Lesson_2

---
+++ src/main/java/student_roman_hytt/lesson_3/level_5/DogDemo.java
@@ -0,0 +1,16 @@
+package student_roman_hytt.lesson_3.level_5;
+
+public class DogDemo {
+    public static void main (String [] args) {
+        Dog x= new Dog("Lucky");
+        x.voice();
+        
+
+
+
+
+    }
+
+
+
+}
