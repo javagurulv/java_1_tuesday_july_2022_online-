da2d6b0044f765632ae29b09f6ebbef9ae48a20b
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-26 09:31:45 +0300

    Random var name fixes

--- src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_2_intern/task_11/ArrayUserReader.java
+++ src/main/java/student_levs_blinnikovs/lesson_5_arrays_for_loop/level_2_intern/task_11/ArrayUserReader.java
@@ -15,21 +15,21 @@ import teacher.codereview.CodeReview;
 @CodeReview(approved = true)
 class ArrayUserReader {
     public static void main(String[] args) {
-        int[] numArray = new int[3];
+        int[] numbers = new int[3];
 
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("Enter 3 array element numbers below.");
 
-        for (int i = 0; i < numArray.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Enter " + (i + 1) + ". array element: ");
-            numArray[i] = userInput.nextInt();
+            numbers[i] = userInput.nextInt();
         }
 
         System.out.println("Here is your array: ");
 
-        for (int i = 0; i < numArray.length; i++) {
-            System.out.println(numArray[i]);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
 
 
