9a9e36094dbc4046dea3f73ff962e2ad167b6674
olegsradigin <oradigins@gmail.com>
2022-08-26 05:33:03 +0300

    Lesson 4, level 4, task 11

---
+++ src/main/java/student_olegs_radigins/lesson_4/level_4/task_11/Calculator.java
@@ -0,0 +1,23 @@
+package student_olegs_radigins.lesson_4.level_4.task_11;
+
+import java.awt.desktop.ScreenSleepEvent;
+
+class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+    public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    public int divide (int firstNumber, int secondNumber) {
+        if( secondNumber != 0){
+            return firstNumber / secondNumber;
+        }
+        System.out.print("Can't divide by ");
+        return secondNumber;
+    }
+}
+
