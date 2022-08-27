2afe1bb8737452a818ff70b93910510a28d1694c
Edwrd <Billionare225>
2022-08-27 23:07:06 +0300

    Lesson 5 Level 4

---
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_4_junior/Task30.java
@@ -0,0 +1,40 @@
+package student_eduard_minajev.lesson_5_arrays_for_loop.homework.level_4_junior;
+
+import java.util.Random;
+
+public class Task30 {
+
+    public static void main(String[] args){
+
+        int sizeOfArray = generateArrayLength();
+        int [] newArray = fillArrayRandomNumbers(sizeOfArray);
+        printOddNumbers(newArray);
+
+
+    }
+    public static int generateArrayLength () {
+        Random randomLength = new Random();
+        int arrayLength = randomLength.nextInt(10);
+        return arrayLength;
+    }
+
+    public static int [] fillArrayRandomNumbers (int number){
+        int [] randomArray = new int[number];
+        Random random = new Random();
+        for (int i = 0; i < randomArray.length; i++){
+            randomArray[i] = random.nextInt(1000);
+            System.out.println(randomArray[i]);
+        } return randomArray;
+    }
+
+    public static void printOddNumbers (int [] number){
+        for (int i = 0; i< number.length; i++ ){
+            if (number[i] % 2 !=0 ) {
+                System.out.println(number[i]);
+            }
+        }
+    }
+
+
+
+}
