48adb9346a3b526fb30e06a11ebe076c283d5c97
Yurii <thoryur@gmail.com>
2022-08-27 12:08:13 +0200

    lessson 5, level 3 intern,tasks 16-24

--- src/main/java/student_yurii_panasiuk/lesson_5/level_3_junior/ForLoopDefinition8.java
+++ src/main/java/student_yurii_panasiuk/lesson_5/level_3_junior/ForLoopDefinition8.java
@@ -1,10 +1,11 @@
 package student_yurii_panasiuk.lesson_5.level_3_junior;
 
-class ForLoopDefinition5_1 {
+class ForLoopDefinition8 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 101; i++) {
             System.out.println(i);
+        }
     }
-}
+
 }
