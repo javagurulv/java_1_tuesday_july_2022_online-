e6eb18e04c9499bde5f4af97e92ea386dbabbd12
Edwrd <Billionare225>
2022-08-27 17:21:33 +0300

    Lesson 5 Level 4

--- src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/leve_4_junior/Task25.java
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/leve_4_junior/Task25.java
@@ -4,11 +4,26 @@
 - заполните массив числами полученными от пользователя;
 - распечатайте на консоль все элементы массива. */
 
-
 package student_eduard_minajev.lesson_5_arrays_for_loop.homework.leve_4_junior;
+
+import java.util.Scanner;
 
 public class Task25 {
 
+    public static void main (String[] args){
 
+        System.out.println("Please enter the length of an array. For your convenience, don't go above 10! ");
+        Scanner scanner = new Scanner(System.in);
+        int userArrayLength = scanner.nextInt();
+        int [] userArray = new int[userArrayLength];
+
+        System.out.println("Now fill this array with your numbers.");
+        Scanner userNumber = new Scanner(System.in);
+        for (int i = 0; i< userArray.length; i++) {
+            userArray[i] = userNumber.nextInt() ;
+            System.out.println(userArray[i]);
+        }
+
+    }
 
 }
---
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/leve_4_junior/Task25V2.java
@@ -0,0 +1,36 @@
+/* Напишите программу, в которой:
+- запросите у пользователя с консоли длину массива;
+- создайте массив указанной пользователем длины;
+- заполните массив числами полученными от пользователя;
+- распечатайте на консоль все элементы массива. */
+
+package student_eduard_minajev.lesson_5_arrays_for_loop.homework.leve_4_junior;
+
+import java.util.Scanner;
+
+public class Task25V2 {
+
+    public static void main(String[] args){
+        int arrayLength = requestArrayLength();
+        int [] newArray = createUserArray(arrayLength);
+    }
+
+    public static int requestArrayLength (){
+        System.out.println("Please enter array length");
+        Scanner userArrayLength = new Scanner(System.in);
+        int arrayLength = userArrayLength.nextInt();
+        return arrayLength;
+    }
+
+    public static int[] createUserArray (int number){
+        int [] userArray = new int[number];
+        System.out.println("Please fill in the array");
+        Scanner userNumbers = new Scanner(System.in);
+        for (int i = 0; i < userArray.length; i++){
+            userArray[i] = userNumbers.nextInt();
+            System.out.println(userArray[i]);
+        } return userArray;
+
+    }
+
+}
