15e5c3fff16af6964372581686a11041dbc1665a
Ernest <ernests.aleskevics@gmail.com>
2022-08-28 15:23:52 +0300

    level 4 task 7

---
+++ src/main/java/student_ernest_aleskevics/lesson_4/level_2/task_7/EqualsOrDifferent.java
@@ -0,0 +1,23 @@
+package student_ernest_aleskevics.lesson_4.level_2.task_7;
+
+import java.util.Scanner;
+
+public class EqualsOrDifferent {
+
+    public static void main(String[] args) {
+
+        Scanner inPut = new Scanner(System.in);
+
+        System.out.println("Please enter the fist number: ");
+        int numberOne = inPut.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numberTwo = inPut.nextInt();
+
+        if (numberOne == numberTwo) {
+            System.out.println("Numbers are equals");
+        } else {
+            System.out.println("Numbers are different");
+        }
+    }
+}
