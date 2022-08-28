79ae73819881835ddca33a121c69fb3072dbb43a
Ernest <ernests.aleskevics@gmail.com>
2022-08-28 15:35:58 +0300

    level 4 task 8

---
+++ src/main/java/student_ernest_aleskevics/lesson_4/level_3/task_8/ThreeNumbers.java
@@ -0,0 +1,30 @@
+package student_ernest_aleskevics.lesson_4.level_3.task_8;
+
+import java.util.Scanner;
+
+public class ThreeNumbers {
+    public static void main(String[] args) {
+
+        Scanner inPut = new Scanner(System.in);
+
+        System.out.println("Please enter the fist number: ");
+        int numberOne = inPut.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numberTwo = inPut.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numberThree = inPut.nextInt();
+
+        if (numberOne == numberTwo && numberOne == numberThree && numberTwo == numberThree){
+            System.out.println("All numbers are equal");
+        } else if (numberOne != numberTwo && numberOne != numberThree && numberTwo != numberThree){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+
+    }
+
+
+}
