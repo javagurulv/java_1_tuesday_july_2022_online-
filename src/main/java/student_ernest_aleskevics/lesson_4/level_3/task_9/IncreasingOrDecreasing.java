94a9da007c185da962b95f0ea21728b6c13ad076
Ernest <ernests.aleskevics@gmail.com>
2022-08-28 15:41:39 +0300

    level 4 task 9

---
+++ src/main/java/student_ernest_aleskevics/lesson_4/level_3/task_9/IncreasingOrDecreasing.java
@@ -0,0 +1,28 @@
+package student_ernest_aleskevics.lesson_4.level_3.task_9;
+
+import java.util.Scanner;
+
+public class IncreasingOrDecreasing {
+
+    public static void main(String[] args) {
+
+        Scanner inPut = new Scanner(System.in);
+
+        System.out.println("Please enter the fist number: ");
+        int numberOne = inPut.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numberTwo = inPut.nextInt();
+
+        System.out.println("Please enter the second number: ");
+        int numberThree = inPut.nextInt();
+
+        if (numberOne > numberTwo && numberOne > numberThree && numberTwo > numberThree){
+            System.out.println("Decreasing");
+        } else if (numberThree > numberTwo && numberThree> numberOne && numberTwo > numberOne){
+            System.out.println("Increasing");
+        } else {
+            System.out.println("Neither increasing or decreasing order");
+        }
+    }
+}
