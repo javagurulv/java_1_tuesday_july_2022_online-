2c3c2c7011297977ae63cb0a5e5ee2fa0bdbdfcd
Edwrd <Billionare225>
2022-08-28 20:49:48 +0300

    Lesson 5 Level 6

---
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_5_middle/ArrayUtil.java
@@ -0,0 +1,72 @@
+/*Создайте класс ArrayUtil.
+Напишите в этом классе метод для создания
+массива целых чисел указанной длины.
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        // Напишите реализацию !!!
+    }
+
+} */
+
+package student_eduard_minajev.lesson_5_arrays_for_loop.homework.level_5_middle;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayUtil {
+
+
+    public  int[] createArray(int arrayLength){
+        int[] newArray = new  int [arrayLength];
+        for (int i = 0; i< newArray.length; i++){
+            newArray[i]= newArray[i] + i;
+            System.out.println(newArray[i]);
+        }
+        return newArray;
+    }
+
+    public int [] fillArrayWithRandomNumbers (int number){
+        int [] randomArray = new int[number];
+        Random random = new Random();
+        for (int i = 0; i < randomArray.length; i++){
+            randomArray[i] = random.nextInt(1000);
+             /*System.out.println(randomArray[i]);*/
+        } return randomArray;
+    }
+
+    /* Task 34  */
+
+    public void printArrayToConsole(int[] array){
+        for (int i= 0; i< array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    /* Task 35 */
+
+    public int findMaxNumber(int[] array) {
+        int maxValueOfArray = 0;
+        for (int i = 0; i < array.length;){
+            if (array[i] > maxValueOfArray){
+                maxValueOfArray = array[i];
+            }System.out.println(maxValueOfArray);
+        }return maxValueOfArray;
+    }
+
+    /* Task 37 */
+
+    public int findMinNumber(int[] array) {
+        int minValueOfArray = array[0];
+        for (int i = 0; i < array.length;){
+            if(array[i] < minValueOfArray){
+                minValueOfArray = array[i];
+            }System.out.println(minValueOfArray);
+        }return minValueOfArray;
+    }
+
+
+
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_5_middle/ArrayUtilTest.java
@@ -0,0 +1,39 @@
+package student_eduard_minajev.lesson_5_arrays_for_loop.homework.level_5_middle;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args){
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+        /* test.shouldFindMaxNumber();
+        * test.shouldFindMinNumber();*/
+
+
+
+    }
+
+    public void shouldCreateArray() {
+        int requiredLength = 10;
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.createArray(requiredLength);
+    }
+
+    /* Task 36 */
+    /* Тут пока не понятно, не знаю с чем связать findMaxNumber
+    public void shouldFindMaxNumber() {
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.findMaxNumber();
+
+    } */
+
+    /* Task 38  Здесь та же самая проблема, к сожалению */
+    /* public void shouldFindMinNumber() {
+
+    } */
+
+
+
+
+}
+
+
