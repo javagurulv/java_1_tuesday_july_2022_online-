18edb790af89ad41d4a5017c13128f6432a68ef6
Ernest <ernests.aleskevics@gmail.com>
2022-08-28 22:20:28 +0300

    level 4 task 17

---
+++ src/main/java/student_ernest_aleskevics/lesson_4/level_6/Stock.java
@@ -0,0 +1,26 @@
+package student_ernest_aleskevics.lesson_4.level_6;
+
+public class Stock {
+
+    String companyName;
+    int currentPrice, minPrice, maxPrice;
+
+    Stock (String companyName, int currentPrice){
+        this.companyName = companyName;
+        this.currentPrice = currentPrice;
+        this.maxPrice = currentPrice;
+        this.minPrice = currentPrice;
+    }
+
+    void updatePrice(int newPrice) {
+        currentPrice = newPrice;
+        if (maxPrice < currentPrice){
+            maxPrice = currentPrice;
+        } else if (minPrice > currentPrice){
+            minPrice = currentPrice;
+        }
+    }
+    void getPriceInformation(){
+        System.out.println("Company: " + companyName + ". Current price: " + currentPrice + ". Min price: " + minPrice + ". Max price: " + maxPrice);
+    }
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_4/level_6/StockDemo.java
@@ -0,0 +1,14 @@
+package student_ernest_aleskevics.lesson_4.level_6;
+
+public class StockDemo {
+
+    public static void main(String[] args) {
+
+        Stock huawei = new Stock("Huawei", 10);
+        huawei.updatePrice(15);
+        huawei.updatePrice(5);
+        huawei.updatePrice(7);
+        huawei.getPriceInformation();
+
+    }
+}
