c224aa350388b77357112b0901edddef4160dd73
Ivan Mostepanov <neomess13@gmail.com>
2022-08-29 21:02:08 +0300

    lesson_2_level_3

---
+++ src/main/java/student_ivan_mostepanov/lesson_2/level_3_junior/Task_11.java
@@ -0,0 +1,32 @@
+package student_ivan_mostepanov.lesson_2.level_3_junior;
+
+import java.util.Scanner;
+
+class Task_11 {
+
+    public static void main (String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        double numberOne = scanner.nextInt();
+        System.out.println("Enter Second number");
+        double numberTwo = scanner.nextInt();
+        System.out.println("Enter Third number");
+        double numberThree = scanner.nextInt();
+
+
+    double averageNumber = (numberOne + numberTwo + numberThree) / 3;
+
+    System.out.println("Average number of "+ numberOne +" ; " + numberTwo + " ; " + numberThree + " = " + averageNumber);
+
+
+
+
+    }
+
+
+
+
+
+}
