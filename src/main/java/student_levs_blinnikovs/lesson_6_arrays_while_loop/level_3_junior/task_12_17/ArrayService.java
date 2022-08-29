f7fb90e150bbc2759c31739b962f1f008f963e3c
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-29 11:29:46 +0300

    ArrayService - Task 13

---
+++ src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_3_junior/task_12_17/ArrayService.java
@@ -0,0 +1,25 @@
+package student_levs_blinnikovs.lesson_6_arrays_while_loop.level_3_junior.task_12_17;
+
+class ArrayService {
+
+    public boolean contains(int[] array, int number) {
+        for (int element : array) {
+            if (element == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int findOccurrences(int[] array, int number) {
+        int occurredCount = 0;
+        for (int element : array) {
+            if (element == number) {
+                occurredCount++;
+            }
+        }
+        return occurredCount;
+    }
+
+
+}
--- src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_3_junior/task_12_17/ArrayServiceTest.java
+++ src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_3_junior/task_12_17/ArrayServiceTest.java
@@ -1,4 +1,4 @@
-package student_levs_blinnikovs.lesson_6_arrays_while_loop.level_3_junior.task_12;
+package student_levs_blinnikovs.lesson_6_arrays_while_loop.level_3_junior.task_12_17;
 
 public class ArrayServiceTest {
 
@@ -28,9 +28,29 @@ public class ArrayServiceTest {
         checkTestResult(realResult == expectedResult, "Doesn't contain test");
     }
 
+    private void shouldOccurTwoTimes() {
+        int[] testArray = {1,2,3,3};
+        int numberToOccur = 3;
+        int expectedOccurrences = 2;
+        ArrayService arrayService = new ArrayService();
+        int realOccurrences = arrayService.findOccurrences(testArray, numberToOccur);
+        checkTestResult((realOccurrences == expectedOccurrences), "Occurs twice test");
+    }
+
+    private void shouldNotOccur() {
+        int[] testArray = {1,2,3,3};
+        int numberToOccur = 4;
+        int expectedOccurrences = 0;
+        ArrayService arrayService = new ArrayService();
+        int realOccurrences = arrayService.findOccurrences(testArray, numberToOccur);
+        checkTestResult((realOccurrences == expectedOccurrences), "Doesn't occur test");
+    }
+
     public static void main(String[] args) {
         ArrayServiceTest test = new ArrayServiceTest();
         test.shouldContain();
         test.shouldNotContain();
+        test.shouldOccurTwoTimes();
+        test.shouldNotOccur();
     }
 }
