af76e715c4bdf3985c97c2c84b1e3f4b083fe010
Ivan Mostepanov <neomess13@gmail.com>
2022-08-29 21:16:49 +0300

    lesson_2_level_5

--- src/main/java/student_ivan_mostepanov/lesson_2/level_5_middle/Task_15.java
+++ src/main/java/student_ivan_mostepanov/lesson_2/level_5_middle/Task_15.java
@@ -3,9 +3,9 @@ package student_ivan_mostepanov.lesson_2.level_5_middle;
 class Task_15 {
 
     public static void main (String[]args){
-        System.out.println("Writing. ");
+        System.out.println("Writing... ");
         System.out.println("Now writing new line. ");
-        System.out.print("Writing. ");
+        System.out.print("Writing... ");
         System.out.print("Now writing same line. ");
 
 
--- src/main/java/student_ivan_mostepanov/lesson_2/level_5_middle/Task_18.java
+++ src/main/java/student_ivan_mostepanov/lesson_2/level_5_middle/Task_18.java
@@ -5,7 +5,7 @@ class Task_18 {
         public static void main(String[] args) {
             System.out.println("Sorry, I have an errors.");
             System.out.println("Please fix me!");
-            System.out.println("DONE!");
+            System.out.println("DONE");
         }
 
 
