9a0d7b9e576a3ded8bc2ce26bca1e118bd2a0146
Ernest <ernests.aleskevics@gmail.com>
2022-08-29 12:34:48 +0300

    level 5 task 10

---
+++ src/main/java/student_ernest_aleskevics/lesson_5/level_2/task_10/TenNumbers.java
@@ -0,0 +1,23 @@
+package student_ernest_aleskevics.lesson_5.level_2.task_10;
+
+public class TenNumbers {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        numbers[5] = 6;
+        numbers[6] = 7;
+        numbers[7] = 8;
+        numbers[8] = 9;
+        numbers[9] = 10;
+
+        for (int i = 0; i < numbers.length; i++ ) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
