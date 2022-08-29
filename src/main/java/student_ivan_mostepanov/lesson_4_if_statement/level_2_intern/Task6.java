e7035d156b82e643982fc9c7319d417182bc93a0
Ivan Mostepanov <neomess13@gmail.com>
2022-08-30 01:01:20 +0300

    lesson_4_task_6

---
+++ src/main/java/student_ivan_mostepanov/lesson_4_if_statement/level_2_intern/Task6.java
@@ -0,0 +1,29 @@
+package student_ivan_mostepanov.lesson_4_if_statement.level_2_intern;
+
+import java.util.Scanner;
+
+class Task6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number ");
+        int firstNumber = sc.nextInt();
+        System.out.println("Enter second number ");
+        int secondNumber = sc.nextInt();
+
+
+        if (firstNumber < secondNumber) {
+            System.out.println("Number " + firstNumber + " is smaller");
+        }
+        else if (firstNumber > secondNumber) {
+            System.out.println("Number " + secondNumber + " is smaller ");
+        }
+        else {
+            System.out.println("Numbers are the same");
+        }
+
+    }
+
+
+
+}
