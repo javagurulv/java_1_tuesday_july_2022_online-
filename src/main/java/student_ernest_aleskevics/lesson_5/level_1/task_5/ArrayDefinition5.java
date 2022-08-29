729320cae2670002f59fe811dc26ba021b88f79e
Ernest <ernests.aleskevics@gmail.com>
2022-08-29 11:43:23 +0300

    level 5 task 5

---
+++ src/main/java/student_ernest_aleskevics/lesson_5/level_1/task_5/ArrayDefinition5.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_5.level_1.task_5;
+
+public class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+}
+
