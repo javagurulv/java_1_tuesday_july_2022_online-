f72dd071698b556dc2803a7b5fd1e970bfbae3e8
Ernest <ernests.aleskevics@gmail.com>
2022-08-29 11:36:57 +0300

    level 5 task 2

---
+++ src/main/java/student_ernest_aleskevics/lesson_5/level_1/task_2/ArrayDefinition2.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_5.level_1.task_2;
+
+public class ArrayDefinition2 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+}
+
