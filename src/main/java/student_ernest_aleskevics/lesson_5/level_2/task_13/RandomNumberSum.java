390f7f5e0e766806fb2c1b7d9f40588b319d482e
Ernest <ernests.aleskevics@gmail.com>
2022-08-29 16:36:22 +0300

    level 5 task 13

---
+++ src/main/java/student_ernest_aleskevics/lesson_5/level_2/task_13/RandomNumberSum.java
@@ -0,0 +1,40 @@
+package student_ernest_aleskevics.lesson_5.level_2.task_13;
+
+import java.util.Random;
+
+public class RandomNumberSum {
+
+    public static void main(String[] args) {
+        int [] randomNumbers = generateRandomNumbers(10);
+        printNumbersToConsole(randomNumbers);
+        int sumRandomNumbers = calculateRandomNumbers(randomNumbers);
+        System.out.println(sumRandomNumbers);
+    }
+
+    private static int[] generateRandomNumbers(int numberCount) {
+        int[] randomNumbers = new int[numberCount];
+        Random random = new Random();
+        for (int i = 0; i < randomNumbers.length; i++) {
+            int newRandomNumber = random.nextInt(10);
+            randomNumbers[i] = newRandomNumber;
+        }
+        return randomNumbers;
+    }
+    private static int calculateRandomNumbers (int[] randomNumbers) {
+        int sumRandomNumbers = 0;
+        for (int i = 0; i < randomNumbers.length; i++) {
+            sumRandomNumbers = sumRandomNumbers + randomNumbers[i];
+        }
+        return sumRandomNumbers;
+    }
+    private static void printNumbersToConsole (int[] randomNumbers) {
+        for (int i = 0; i < randomNumbers.length; i++) {
+            System.out.println(randomNumbers[i]);
+        }
+    }
+}
+
+
+
+
+
