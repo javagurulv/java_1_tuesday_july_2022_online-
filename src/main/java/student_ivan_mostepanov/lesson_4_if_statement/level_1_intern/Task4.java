c04dc108c3721f1002805d45f6cee6513e17709f
Ivan Mostepanov <neomess13@gmail.com>
2022-08-30 00:45:33 +0300

    lesson_4_task_1

---
+++ src/main/java/student_ivan_mostepanov/lesson_4_if_statement/level_1_intern/Task4.java
@@ -0,0 +1,27 @@
+package student_ivan_mostepanov.lesson_4_if_statement.level_1_intern;
+
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int number = sc.nextInt();
+
+        int divider = 2;
+        int remainder = number % divider;
+
+        if (remainder == 0) {
+            System.out.println("Number is Even!");
+        }
+        else  {
+                System.out.println("Number is Odd!");
+            }
+
+        }
+
+
+
+
+}
