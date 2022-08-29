047f767e0610b9dd5359555adfc58870d5b1eb75
Ivan Mostepanov <neomess13@gmail.com>
2022-08-29 22:10:57 +0300

    lesson_2_level_7

---
+++ src/main/java/student_ivan_mostepanov/lesson_2/level_7_senior/task_29/Task29.java
@@ -0,0 +1,22 @@
+package student_ivan_mostepanov.lesson_2.level_7_senior.task_29;
+
+class Task29 {
+
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+
+}
