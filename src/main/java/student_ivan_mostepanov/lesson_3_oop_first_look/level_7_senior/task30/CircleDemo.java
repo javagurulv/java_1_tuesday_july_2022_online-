4ed1b0b495525211eaee91bbcb3bcd445d91c0ce
Ivan Mostepanov <neomess13@gmail.com>
2022-08-29 23:02:27 +0300

    lesson_2_level_7

---
+++ src/main/java/student_ivan_mostepanov/lesson_3_oop_first_look/level_7_senior/task30/CircleDemo.java
@@ -0,0 +1,12 @@
+package student_ivan_mostepanov.lesson_3_oop_first_look.level_7_senior.task30;
+
+class CircleDemo {
+
+    public static void main(String[]args){
+        Circle circle1 = new Circle(7.5);
+        circle1.calculateArea();
+
+
+    }
+
+}
