e6d1d04dbb963b343cc0c783ddff3c535fe87e09
Ernest <ernests.aleskevics@gmail.com>
2022-08-29 12:14:14 +0300

    level 5 task 9

---
+++ src/main/java/student_ernest_aleskevics/lesson_5/level_1/task_9/ArrayLength3.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_5.level_1.task_9;
+
+public class ArrayLength3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+}
