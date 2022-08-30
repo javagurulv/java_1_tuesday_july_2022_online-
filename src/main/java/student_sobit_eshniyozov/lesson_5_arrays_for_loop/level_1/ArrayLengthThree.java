194794bee429334271222f79f9372d1de890549e
sobitjon <seshniyozov@list.ru>
2022-08-30 15:27:31 +0500

    homework lesson  5 level 2

--- src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_1/ArrayLengthThree.java
+++ src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_1/ArrayLengthThree.java
@@ -1,4 +1,4 @@
-package student_sobit_eshniyozov.lesson_5.level_1;
+package student_sobit_eshniyozov.lesson_5_arrays_for_loop.level_1;
 
 public class ArrayLengthThree {
     public static void main(String[] args) {
