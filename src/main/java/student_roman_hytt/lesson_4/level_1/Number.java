077d1f32a7ba2d3e96720891c35c904cb87482b2
Admin <roman.hytt@gmail.com>
2022-08-30 18:58:10 +0300

    1

---
+++ src/main/java/student_roman_hytt/lesson_4/level_1/Number.java
@@ -0,0 +1,24 @@
+package student_roman_hytt.lesson_4.level_1;
+
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        System.out.println("Please write you number!");
+
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        if ( firstNumber > -1 ){
+            System.out.println( firstNumber +" Number is positive ");}
+
+        else System.out.println(firstNumber+ " Number is negative");
+
+        if ( firstNumber ==0){
+            System.out.println( " Your number iz zerro ");}
+    }
+
+
+}
+
+
+
---
+++ src/main/java/student_roman_hytt/lesson_4/level_2/Num.java
@@ -0,0 +1,4 @@
+package student_roman_hytt.lesson_4.level_2;
+
+public class Num {
+}
