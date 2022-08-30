5f079b5318ebdab9a0a163086517596fef5fd50c
lblinnikovs <levs.blinnikovs@gmail.com>
2022-08-30 21:45:22 +0300

    Task 18 - Indefinate loops

---
+++ src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_4_junior/task_18_x/ForInfiniteLoop.java
@@ -0,0 +1,10 @@
+package student_levs_blinnikovs.lesson_6_arrays_while_loop.level_4_junior.task_18_x;
+
+class ForInfiniteLoop {
+    public static void main(String[] args) {
+        System.out.println("Понеслась!");
+        for (int i = 0; i >= 0; i++) {
+            System.err.println(i);
+        }
+    }
+}
