fc2027efa7dfb83ef88aa343c4b75f0b09b6eb27
Ivan Mostepanov <neomess13@gmail.com>
2022-08-31 20:04:07 +0300

    fix

---
+++ src/main/java/student_ivan_mostepanov/lesson_5_arrays_for_loop/level_1_intern/ArrayDefinition5.java
@@ -0,0 +1,12 @@
+package student_ivan_mostepanov.lesson_5_arrays_for_loop.level_1_intern;
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int [] {1, 2, 3,4, };
+    }
+
+
+
+
+}
---
+++ src/main/java/student_ivan_mostepanov/lesson_5_arrays_for_loop/level_1_intern/ArrayDefinition6.java
@@ -0,0 +1,10 @@
+package student_ivan_mostepanov.lesson_5_arrays_for_loop.level_1_intern;
+
+class ArrayDefinition6 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int []{1, 2, 3,};
+    }
+
+
+}
