c354e58d4bf0cbf56b7cd92e9f74240706f8c447
sobitjon <seshniyozov@list.ru>
2022-09-02 17:02:19 +0500

    lesson 5 homework level 5 without test

---
+++ src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_5/ArrayUntil.java
@@ -0,0 +1,34 @@
+package student_sobit_eshniyozov.lesson_5_arrays_for_loop.level_5;
+
+import java.util.Random;
+
+class ArrayUntil {
+
+    public static void main(String[] args) {
+        ArrayUntil arrayUntilOne = new ArrayUntil();
+        arrayUntilOne.createArray(6);
+        arrayUntilOne.createRandomArray(6);
+    }
+    public int[] createArray(int arrayLength) {
+
+        int[] group = new int[arrayLength];
+
+        for (int i = 0; i<group.length; i++){
+            System.out.println(group[i]);
+        }
+        return group;
+    }
+    public int[] createRandomArray(int arrayLength) {
+
+        int[] group = new int[arrayLength];
+
+        for (int i = 0; i<group.length; i++){
+            Random random = new Random();
+            group[i]=random.nextInt(150);
+            System.out.println(group[i]);
+        }
+        return group;
+    }
+
+
+}
