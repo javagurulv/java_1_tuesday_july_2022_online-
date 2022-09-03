120eafa607c27d771e1daea5673b00a32ac8b1bd
Ernest <ernests.aleskevics@gmail.com>
2022-09-03 16:36:11 +0300

    lesson 6 task 4

---
+++ src/main/java/student_ernest_aleskevics/lesson_6/task_4/WhileDefinition4.java
@@ -0,0 +1,12 @@
+package student_ernest_aleskevics.lesson_6.task_4;
+
+public class WhileDefinition4 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
