5f7a14a06f4e34bc6aefed4aee85c27c22deafd6
Admin <roman.hytt@gmail.com>
2022-09-04 17:14:02 +0300

    14

--- src/main/java/student_roman_hytt/lesson_4/level_1/task_3/Week.java
+++ src/main/java/student_roman_hytt/lesson_4/level_1/task_3/Week.java
@@ -10,12 +10,23 @@ public class Week {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please write your number");
-        int firstNumber = scanner.nextInt();
-
-        if (firstNumber == 1) {
-            System.out.println("Monday");
-        } else if (firstNumber == 2) {
-            System.out.println("Tuesday");
+        int i = scanner.nextInt();
+        switch (i) {
+            case 1:
+                System.out.println("Monday");break;
+            case 2:
+                System.out.println("Tuesday");break;
+            case 3:
+            System.out.println("Wednesday");break;
+            case 4:
+            System.out.println("Thursday");break;
+            case 5:
+             System.out.println("Friday");break;
+            case 6:
+             System.out.println("Saturday");break;
+            case 7:
+            System.out.println("Sunday");break;
+            default: System.out.println("Something is wrong");
         }
     }
 }
