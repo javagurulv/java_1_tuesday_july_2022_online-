6f5594b2b06b09003596fc77b6e8e6a6dd5542b6
olegsradigin <oradigins@gmail.com>
2022-09-05 19:50:07 +0300

    Lesson 6, level 1, tasks 1-5

---
+++ src/main/java/student_olegs_radigins/lesson_6/level_1/task_3/WhileDefinition3.java
@@ -0,0 +1,11 @@
+package student_olegs_radigins.lesson_6.level_1.task_3;
+
+class WhileDefinition3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_6/level_1/task_5/WhileDefinition5.java
@@ -0,0 +1,11 @@
+package student_olegs_radigins.lesson_6.level_1.task_5;
+
+class WhileDefinition5 {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
