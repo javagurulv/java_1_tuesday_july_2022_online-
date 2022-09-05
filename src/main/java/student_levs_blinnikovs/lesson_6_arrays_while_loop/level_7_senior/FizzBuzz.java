cc00b2e16a71157d60fa1c930215366f8444febc
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-05 10:28:46 +0300

    FizzBuzz fix and runner

--- src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_7_senior/FizzBuzz.java
+++ src/main/java/student_levs_blinnikovs/lesson_6_arrays_while_loop/level_7_senior/FizzBuzz.java
@@ -12,14 +12,21 @@ package student_levs_blinnikovs.lesson_6_arrays_while_loop.level_7_senior;
 
 class FizzBuzz {
 
-    private int initialNumber;
-
     public String detect(int number) {
-        this.initialNumber = number;
-        if ((initialNumber % 3 == 0) && (initialNumber % 5 == 0)) return "FizzBuzz";
-        else if (initialNumber % 3 == 0) return "Fizz";
-        else if (initialNumber % 5 == 0) return "Buzz";
-        else return "" + initialNumber;
+        if ((number % 3 == 0) && (number % 5 == 0)) return "FizzBuzz";
+        else if (number % 3 == 0) return "Fizz";
+        else if (number % 5 == 0) return "Buzz";
+        else return "" + number;
+    }
+
+    public static void main(String[] args) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        System.out.println(fizzBuzz.detect(15));
+        System.out.println(fizzBuzz.detect(3));
+        System.out.println(fizzBuzz.detect(5));
+
     }
 
 }
