d1753d5b661566a10d2ffc9ec91cb7d6f15f4105
Uguha21 <Agasem2106@icloud.com>
2022-09-06 09:41:54 +0300

    lesson_3_task_7

---
+++ src/main/java/student_jelena_voinica/lesson_3/Task_7.java
@@ -0,0 +1,8 @@
+package student_jelena_voinica.lesson_3;
+
+public class Task_7 {
+    public static void main(String[]args){
+        String myName = "Jelena";
+        System.out.println(myName);
+    }
+}
