986972f157f43d4b0848665e9882a1ef9f6b954a
Edwrd <Billionare225>
2022-09-06 21:31:27 +0300

    Lesson 5 Level 5 task 31-33 redone

--- src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_5_middle/ArrayUtil.java
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_5_middle/ArrayUtil.java
@@ -15,42 +15,40 @@ package student_eduard_minajev.lesson_5_arrays_for_loop.homework.level_5_middle;
 import java.util.Random;
 import java.util.Scanner;
 
-import teacher.codereview.CodeReview;
-import teacher.codereview.CodeReviewComment;
 
-@CodeReview(approved = false)
-@CodeReviewComment(teacher = "Лишний импорт Scanner")
 public class ArrayUtil {
 
-    /* Task 31 - done */
-	@CodeReviewComment(teacher = "Зачем тут цикл?")
-    public  int[] createArray(int arrayLength){
-        int[] newArray = new  int [arrayLength];
-        for (int i = 0; i< newArray.length; i++){
-            /* System.out.println(newArray[i]); */
-        } return newArray;
+    /* Task 31 - redone */
+    public int generateArrayLength () {
+        System.out.println("Pleas enter array length");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    public  int[] createArray(int arrayLength ) {
+        return new int[arrayLength];
+
     }
 
-    /* Task 33 - done */
-	@CodeReviewComment(teacher = "Этот метод делает две вещи создаёт массив и заполняет его случайными числами."
-			+ "Подбери долее правильное название методу.")
-    public int [] fillArrayWithRandomNumbers (int number){
-        int [] randomArray = new int[number];
+    /* Task 33 - redone */
+
+    public void fillArrayWithRandomNumbers(int[] array){
         Random random = new Random();
-        for (int i = 0; i < randomArray.length; i++){
-            randomArray[i] = random.nextInt(100);
-        } return randomArray;
+        for (int i = 0; i < array.length; i++){
+            array[i] = random.nextInt(100);
+        }
     }
 
     /* Task 34  */
     public void printArrayToConsole(int[] array){
-        System.out.println(array);
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array);
+        }
     }
 
     /* Task 35 */
-	@CodeReviewComment(teacher = "Тут ошибка в логике!")
+
     public int findMaxNumber(int[] array) {
-        int maxValueOfArray = 0;
+        int maxValueOfArray = array [0];
         for (int i = 0; i < array.length; i++){
             if (array[i] > maxValueOfArray){
                 maxValueOfArray = array[i];
@@ -68,8 +66,5 @@ public class ArrayUtil {
             }
         }return minValueOfArray;
     }
-
-
-
 
 }
