5e375627d8371cc18789453e9fd48b3484f35308
Ivan Mostepanov <neomess13@gmail.com>
2022-09-06 22:02:45 +0300

    lesson_6

---
+++ src/main/java/student_ivan_mostepanov/lesson_5_arrays_for_loop/level_2_intern/Task12.java
@@ -0,0 +1,27 @@
+package student_ivan_mostepanov.lesson_5_arrays_for_loop.level_2_intern;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task12 {
+
+
+
+    public static void main(String[] args){
+
+        Random randomGenerator = new Random();
+        int[] numbers = new int[10];
+        for (int j = 0; j < numbers.length; j++) {
+            numbers[j] = randomGenerator.nextInt(100);
+            System.out.println(numbers[j]);
+        }
+
+        }
+
+    }
+
+
+
+
+
+
