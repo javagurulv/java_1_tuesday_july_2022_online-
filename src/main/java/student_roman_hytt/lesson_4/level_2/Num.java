d01c0cecfa7496c51ff91c2c998b72261fcc03b8
Admin <roman.hytt@gmail.com>
2022-09-06 20:18:18 +0300

    v

--- src/main/java/student_roman_hytt/lesson_4/level_2/Num.java
+++ src/main/java/student_roman_hytt/lesson_4/level_2/Num.java
@@ -1,4 +1,11 @@
 package student_roman_hytt.lesson_4.level_2;
 
 public class Num {
+int num1;
+int num2;
+
+int maxOfTwo (int num1,int num2){if (num1>num2) return num1; else return num2;
+}
+
+
 }
---
+++ src/main/java/student_roman_hytt/lesson_4/level_2/NumTest.java
@@ -0,0 +1,29 @@
+package student_roman_hytt.lesson_4.level_2;
+
+import student_roman_hytt.lesson_4.level_4.Calculator;
+
+public class NumTest {
+    public static void main(String[] args) {
+        NumTest numTest = new NumTest();
+
+        numTest.maxOfTwo ();}
+
+    private void maxOfTwo() {
+        int firstNumber = 1;
+        int secondNumber = 2;
+        int result = 2;
+
+        Num num = new Num();
+
+        int realResult = num.maxOfTwo(firstNumber,secondNumber);
+            if (realResult == result) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+
+
+        }
+
+    }
+
