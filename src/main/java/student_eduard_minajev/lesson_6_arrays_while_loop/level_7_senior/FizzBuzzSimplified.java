6f205dc33bde1dcc27c76ab9af5433cb7644ab2e
Edwrd <Billionare225>
2022-09-07 22:53:49 +0300

    Lesson 6 Level 7

---
+++ src/main/java/student_eduard_minajev/lesson_6_arrays_while_loop/level_7_senior/FizzBuzzSimplified.java
@@ -0,0 +1,14 @@
+package student_eduard_minajev.lesson_6_arrays_while_loop.level_7_senior;
+
+public class FizzBuzzSimplified {
+
+    public String detect(int number) {
+
+        if ((number % 3 == 0) && (number % 5 == 0)) return "FizzBuzz";
+        else if (number % 3 == 0) return "Fizz";
+        else if (number % 5 == 0) return "Buzz";
+        else return "" + number;
+
+    }
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_6_arrays_while_loop/level_7_senior/FizzBuzzTest.java
@@ -0,0 +1,46 @@
+package student_eduard_minajev.lesson_6_arrays_while_loop.level_7_senior;
+
+public class FizzBuzzTest {
+
+    public static void main(String[] args) {
+    FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
+    fizzBuzzTest.shouldReturnFizzBuzz();
+    fizzBuzzTest.shouldReturnFizz();
+    fizzBuzzTest.shouldReturnBuzz();
+    fizzBuzzTest.shouldReturnNumber();
+    }
+
+    private void checkTestResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " = OK!");
+        } else {
+            System.out.println(testName + " = FAIL!");
+        }
+    }
+
+    public void shouldReturnFizzBuzz (){
+        FizzBuzzSimplified test = new FizzBuzzSimplified();
+        String fizzBuzz = test.detect(15);
+        checkTestResult("FizzBuzz".equals(fizzBuzz),"shouldReturnFizzBuzz");
+    }
+
+    public void shouldReturnFizz () {
+        FizzBuzzSimplified test = new FizzBuzzSimplified();
+        String fizzBuzz = test.detect(3);
+        checkTestResult("Fizz".equals(fizzBuzz),"shouldReturnFizz");
+    }
+
+    public void shouldReturnBuzz () {
+        FizzBuzzSimplified test = new FizzBuzzSimplified();
+        String fizzBuzz = test.detect(5);
+        checkTestResult("Buzz".equals(fizzBuzz),"shouldReturnBuzz");
+    }
+
+    public void shouldReturnNumber () {
+        FizzBuzzSimplified test = new FizzBuzzSimplified();
+        String fizzBuzz = test.detect(17);
+        checkTestResult("17".equals(fizzBuzz),"shouldReturnNumber");
+    }
+
+
+}
