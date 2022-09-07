8312e2862cc17bca008f76bd1ac8d980ad97f340
Edwrd <Billionare225>
2022-09-07 21:01:31 +0300

    Lesson 6 Level 1 re-done

--- src/main/java/student_eduard_minajev/lesson_6_arrays_while_loop/level_1_intern/NumberUtils.java
+++ src/main/java/student_eduard_minajev/lesson_6_arrays_while_loop/level_1_intern/NumberUtils.java
@@ -15,18 +15,15 @@ class NumberUtils {
 
 package student_eduard_minajev.lesson_6_arrays_while_loop.level_1_intern;
 
-// Буду честен, гуглил вопрос :) Пишут, что нужно тернарный оператор исспользовать.
 
-import teacher.codereview.CodeReview;
-import teacher.codereview.CodeReviewComment;
 
-@CodeReview(approved = false)
+
 public class NumberUtils {
 
-// метод записан
-	@CodeReviewComment(teacher = "смотри src/main/java/teacher/lesson_6_arrays_while_loop/codereview/CleanCodeWarning_1.txt")
-	@CodeReviewComment(teacher = "смотри src/main/java/teacher/lesson_6_arrays_while_loop/codereview/CleanCodeWarning_2.txt")
-    public boolean isEven(int number) {return (number % 2 == 0) ? true : false;}
+
+    public boolean isEven(int number) {
+		return number % 2 == 0;
+	}
 
 }
 
