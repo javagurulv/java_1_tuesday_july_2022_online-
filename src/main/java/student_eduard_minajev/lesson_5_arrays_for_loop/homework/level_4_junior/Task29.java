9f0ba1abc8656953c77c1dfb77e4bf721f44b52d
Edwrd <Billionare225>
2022-09-07 20:54:35 +0300

    Lesson 5 Level 4 re-done

--- src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_4_junior/Task29.java
+++ src/main/java/student_eduard_minajev/lesson_5_arrays_for_loop/homework/level_4_junior/Task29.java
@@ -2,41 +2,42 @@ package student_eduard_minajev.lesson_5_arrays_for_loop.homework.level_4_junior;
 
 import java.util.Random;
 
-import teacher.codereview.CodeReview;
-import teacher.codereview.CodeReviewComment;
 
-@CodeReview(approved = false)
+
 public class Task29 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int sizeOfArray = generateArrayLength();
-        int [] newArray = fillArrayRandomNumbers(sizeOfArray);
+        int[] newArray = createNewArray(sizeOfArray);
+        fillArrayWithRandomNumbers(newArray);
         printEvenNumbers(newArray);
 
+
     }
 
-	@CodeReviewComment(teacher = "смотри src/main/java/teacher/lesson_5_arrays_for_loop/codereview/CleanCodeWarning_6.txt")
-	public static int generateArrayLength () {
+    public static int generateArrayLength() {
         Random randomLength = new Random();
-        int arrayLength = randomLength.nextInt(10);
-        return arrayLength;
+        return randomLength.nextInt(10);
+
     }
 
-	@CodeReviewComment(teacher = "смотри src/main/java/teacher/lesson_5_arrays_for_loop/codereview/CleanCodeWarning_7.txt")
-    public static int [] fillArrayRandomNumbers (int number){
-        int [] randomArray = new int[number];
+    public static int[] createNewArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    public static int[] fillArrayWithRandomNumbers(int[] array) {
         Random random = new Random();
-        for (int i = 0; i < randomArray.length; i++){
-            randomArray[i] = random.nextInt(1000);
-            System.out.println(randomArray[i]);
-        } return randomArray;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        return array;
     }
 
-    public static void printEvenNumbers (int [] number){
-        for (int i = 0; i< number.length; i++ ){
-            if (number[i] % 2 == 0 ) {
-                System.out.println("Even numbers are " + number[i]);
+    public static void printEvenNumbers (int [] array){
+        for (int i = 0; i< array.length; i++ ){
+            if (array[i] % 2 == 0 ) {
+                System.out.println("Even numbers are " + array[i]);
             }
         }
     }
