7673737cac08e98df74ab0822d5adb4f44cbedc7
vsavonins <viktors.savonins@proofit.lv>
2022-09-07 08:49:40 +0300

    LESSON 6 code review

--- src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_1/ArrayDefinitionThree.java
+++ src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_1/ArrayDefinitionThree.java
@@ -1,5 +1,8 @@
 package student_sobit_eshniyozov.lesson_5_arrays_for_loop.level_1;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 public class ArrayDefinitionThree {
     public static void main(String[] args) {
         int[] numbers = new int[10];
