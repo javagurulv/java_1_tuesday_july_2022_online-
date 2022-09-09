5ddda88d948e9966bba79aac01b5b1496cb5ed60
Edwrd <Billionare225>
2022-09-09 23:10:43 +0300

    Lesson 7 Level 1

---
+++ src/main/java/student_eduard_minajev/lesson_6_arrays_while_loop/level_5_middle/TicTacToeTest.java
@@ -0,0 +1,14 @@
+package student_eduard_minajev.lesson_6_arrays_while_loop.level_5_middle;
+
+public class TicTacToeTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void createPlayField (){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.createField();
+    }
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_7_classes_and_objectives/Coin.java
@@ -0,0 +1,37 @@
+package student_eduard_minajev.lesson_7_classes_and_objectives;
+
+public class Coin {
+
+    private int denomination;
+    private String title;
+
+    public Coin(int denomination, String title) {
+        this.denomination = denomination;
+        this.title = title;
+    }
+
+    public int getDenomination() {
+        return denomination;
+    }
+
+    public String getTitle () {
+        return title;
+    }
+
+
+    public int setDenomination (int newDenomination){
+        if (newDenomination == 1 || newDenomination == 2 || newDenomination == 5 || newDenomination == 10 || newDenomination == 20 || newDenomination == 50 ) {
+            this.denomination = newDenomination;
+        }
+        return this.denomination;
+    }
+
+    public String setTitle (String newTitle){
+        if (denomination == 1 || denomination == 2 && newTitle == "Euro" ){
+
+        } else if (denomination == 1 || denomination == 2 || denomination == 5 || denomination == 10 || denomination == 20 || denomination == 50 && newTitle == "Cent") {
+        }
+        this.title = newTitle;
+        return newTitle;
+    }
+}
