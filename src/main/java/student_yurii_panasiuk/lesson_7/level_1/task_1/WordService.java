a5647a4cfdcb3ac8207962deeedc67db5c6beb70
Yurii <thoryur@gmail.com>
2022-09-09 06:18:01 +0200

    lessson 7, level 1, task 1

---
+++ src/main/java/student_yurii_panasiuk/lesson_7/level_1/task_1/WordService.java
@@ -0,0 +1,34 @@
+package student_yurii_panasiuk.lesson_7.level_1.task_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*     Дана строка с текстом.
+        Написать метод, который находит слово
+        в тексте встречающееся больше всего раз.
+
+        Если несколько слов в тексте встречаются одинаково
+        максимальное число раз, то возвращаем, то слово,
+        которое встречается в тексте первым.
+
+  */
+class WordService {
+
+     public static void main(String[] args) {
+
+   // public String findMostFrequentWord(String text) {
+
+        String  text = "type text here"; // Дана строка с текстом.
+
+         List<Character> chars = new ArrayList<>();
+         for (char ch : text.toCharArray()) {
+             chars.add(ch);
+         }
+         return chars;
+         // System.out.println(chars);
+
+
+
+    }
+
+}
