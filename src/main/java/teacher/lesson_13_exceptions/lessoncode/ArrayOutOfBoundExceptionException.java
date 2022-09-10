a293381ddcdf1de3b0da83b9bf97f9e4d6bb0cd6
vsavonins <viktors.savonins@proofit.lv>
2022-09-10 10:11:46 +0300

    Lessons 11 - 17

---
+++ src/main/java/teacher/lesson_13_exceptions/lessoncode/ArrayOutOfBoundExceptionException.java
@@ -0,0 +1,10 @@
+package teacher.lesson_13_exceptions.lessoncode;
+
+public class ArrayOutOfBoundExceptionException {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2};
+		System.out.println(arr[10]);
+	}
+
+}
