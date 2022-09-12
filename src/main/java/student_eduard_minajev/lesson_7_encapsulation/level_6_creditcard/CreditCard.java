91739fdc67fd10b68c79295968b82e81d9611bd3
Edwrd <Billionare225>
2022-09-12 22:42:35 +0300

    Lesson 7 Credit Card

--- src/main/java/student_eduard_minajev/lesson_7_encapsulation/level_6_creditcard/CreditCard.java
+++ src/main/java/student_eduard_minajev/lesson_7_encapsulation/level_6_creditcard/CreditCard.java
@@ -40,7 +40,7 @@ import java.util.Scanner;
 
 public class CreditCard {
 
-     private long cardNumber;
+    private long cardNumber;
 
     private int pinCode;
 
@@ -50,7 +50,7 @@ public class CreditCard {
 
     private double creditDebt;
 
-    CreditCard (long cardNumber, int pinCode, double accountBalance, double creditLimit, double creditDebt){
+    CreditCard(long cardNumber, int pinCode, double accountBalance, double creditLimit, double creditDebt) {
         this.cardNumber = cardNumber;
         this.pinCode = pinCode;
         this.accountBalance = accountBalance;
@@ -78,20 +78,86 @@ public class CreditCard {
         return creditDebt;
     }
 
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
+    public void setAccountBalance(double newAccountBalance) {
+        this.accountBalance = newAccountBalance;
+    }
+    public void setCreditLimit (double newCreditLimit){
+        this.creditLimit = newCreditLimit;
+    }
+    public void setCreditDebt (double newCreditDebt) {
+        this.creditDebt = newCreditDebt;
     }
 
-
-    public double deposit (double depositSum){
+    // Создаем отдельный метод для проверки Пин-кода. Это должен быть Булеан. Если код совпадает - тру.
+    public boolean checkPin () {
         System.out.println("Please enter your card's PIN code!");
         Scanner scanner = new Scanner(System.in);
         int userPin = scanner.nextInt();
-        getPinCode();
-        if (userPin == getPinCode()){
-            System.out.println("Please enter desired amount of money to be put on a deposit!");
-            double newDeposit = scanner.nextDouble();
-            setAccountBalance(newDeposit);
-        } return accountBalance;
+        int cardPin = getPinCode();
+        if (userPin == cardPin) { return true;
+        } else {
+            return false;
+        }
+
     }
+    // Отдельный метод для погашения задолжности по кредиту. Сумма автоматически должна вычитываться из суммы на депозите.
+    public double payOffDebt (){
+        getCreditDebt();
+        setAccountBalance(accountBalance - getCreditDebt());
+        return accountBalance;
+    }
+    // Отдельный метод для определения суммы взноса
+    public double depositValue () {
+        System.out.println("Please enter desired deposit value!");
+        Scanner scanner = new Scanner(System.in);
+        double depositSum = scanner.nextDouble();
+        return depositSum;
+
+    }
+    // непосредственно сам метод взноса
+    public double deposit (){
+        if (checkPin() == true){
+            double value = depositValue(); // убрать нельзя, иначе во время работы программа будет запрашивать сумму взноса два раза.
+            double newDeposit = getAccountBalance() + value;
+            setAccountBalance(newDeposit);
+            payOffDebt(); // Загрузка метода погашения долга по кредиту. Если долга нет, будет вычтен 0.
+            System.out.println("Your current Balance is " + getAccountBalance());
+        } return accountBalance;
+
+    }
+
+    public void printBalanceToConsole (){
+        System.out.println("Your account balance is: " + getAccountBalance() + " Your credit debit is: " + getCreditDebt());
+    }
+
+    public double applyCreditLimit (double newCreditLimit) {
+        if (newCreditLimit > 0) {
+            setCreditLimit(newCreditLimit);
+        }
+        return creditLimit;
+    }
+
+    public double withdrawValue () {
+        System.out.println("Please enter desired withdraw value!");
+        Scanner scanner = new Scanner(System.in);
+        double withdrawValue = scanner.nextDouble();
+        return withdrawValue;
+    }
+
+    public double withdraw () {
+        if (checkPin() == true){
+            double value = withdrawValue();
+            double newDeposit = getAccountBalance() - value;
+            if (newDeposit >= 0){
+                setAccountBalance(newDeposit);
+                return accountBalance;
+            } else if (newDeposit >= getCreditLimit()) {
+                setCreditDebt(newDeposit);
+                return creditDebt;
+            }else {
+            System.out.println("Your account does not enough funds, and you have exceeded your credit limit.");
+            }
+        } return getAccountBalance();
+    }
+
 }
