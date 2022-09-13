6c963ba941e57ace3084506f0c6b18b8370def5c
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-13 10:42:26 +0300

    WordService method reordering based on access modifiers and logic hiding into private methods

--- src/main/java/student_levs_blinnikovs/lesson_7_encapsulation/level_1_4/task_1_8/WordService.java
+++ src/main/java/student_levs_blinnikovs/lesson_7_encapsulation/level_1_4/task_1_8/WordService.java
@@ -2,11 +2,29 @@ package student_levs_blinnikovs.lesson_7_encapsulation.level_1_4.task_1_8;
 
 class WordService {
 
-    String[] splitToComparableWords(String text) {
+    public String findMostFrequentWord(String text) {
+        String[] words = splitToComparableWords(text);
+        return findMostFrequentWord(words);
+    }
+
+    private String findMostFrequentWord(String[] words) {
+        int maxRepetitionCount = 0;
+        String mostFrequentWord = "";
+        for (String word : words) {
+            int currentWordRepetitionCount = countWordRepetitions(words, word);
+            if (currentWordRepetitionCount > maxRepetitionCount) {
+                maxRepetitionCount = currentWordRepetitionCount;
+                mostFrequentWord = word;
+            }
+        }
+        return mostFrequentWord;
+    }
+
+    private String[] splitToComparableWords(String text) {
         return text.toLowerCase().split("\\W+");
     }
 
-    int countWordRepetitions(String[] words, String wordToCheck) {
+    private int countWordRepetitions(String[] words, String wordToCheck) {
         int wordRepetitionCount = 0;
         for (String word : words) {
             if (word.equals(wordToCheck)) {
@@ -14,20 +32,6 @@ class WordService {
             }
         }
         return wordRepetitionCount;
-    }
-
-    public String findMostFrequentWord(String text) {
-        String[] words = splitToComparableWords(text);
-        int maxRepetitionCount = 0;
-        String mostOftenWord = "";
-        for (String word : words) {
-            int currentWordRepetitionCount = countWordRepetitions(words, word);
-            if (currentWordRepetitionCount > maxRepetitionCount) {
-                maxRepetitionCount = currentWordRepetitionCount;
-                mostOftenWord = word;
-            }
-        }
-        return mostOftenWord;
     }
 
 }
