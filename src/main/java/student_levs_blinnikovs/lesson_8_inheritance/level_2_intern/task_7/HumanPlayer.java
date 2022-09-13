ff25ee9f999d68e4cbb6098a927aaa2e571fd74f
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-13 19:51:37 +0300

    Player HumanPlayer

---
+++ src/main/java/student_levs_blinnikovs/lesson_8_inheritance/level_2_intern/task_7/HumanPlayer.java
@@ -0,0 +1,9 @@
+package student_levs_blinnikovs.lesson_8_inheritance.level_2_intern.task_7;
+
+class HumanPlayer extends Player {
+
+    HumanPlayer(String name) {
+        super(name);
+    }
+
+}
---
+++ src/main/java/student_levs_blinnikovs/lesson_8_inheritance/level_2_intern/task_7/Player.java
@@ -0,0 +1,11 @@
+package student_levs_blinnikovs.lesson_8_inheritance.level_2_intern.task_7;
+
+class Player {
+
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+
+}
