799fe10055730b89b6ca16f6358d4fe024f9928b
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-13 16:22:14 +0300

    DefaultConstructor demo

---
+++ src/main/java/student_levs_blinnikovs/lesson_8_inheritance/level_1_intern/task_1/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package student_levs_blinnikovs.lesson_8_inheritance.level_1_intern.task_1;
+
+class DefaultConstructor {
+
+    int intExample;
+    double doubleExample;
+
+    DefaultConstructor() {
+
+    }
+
+}
---
+++ src/main/java/student_levs_blinnikovs/lesson_8_inheritance/level_1_intern/task_1/DefaultConstructorDemo.java
@@ -0,0 +1,12 @@
+package student_levs_blinnikovs.lesson_8_inheritance.level_1_intern.task_1;
+
+class DefaultConstructorDemo {
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+
+        System.out.println(defaultConstructor.intExample == 0);
+        System.out.println(defaultConstructor.doubleExample == 0.0);
+
+    }
+}
