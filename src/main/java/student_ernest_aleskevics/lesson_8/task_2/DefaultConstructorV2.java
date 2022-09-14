4e3553de710451fd47f1f858ddfbaddf75105c55
Ernest <ernests.aleskevics@gmail.com>
2022-09-14 17:52:13 +0300

    lesson 8 task 2

---
+++ src/main/java/student_ernest_aleskevics/lesson_8/task_2/DefaultConstructorV2.java
@@ -0,0 +1,7 @@
+package student_ernest_aleskevics.lesson_8.task_2;
+
+public class DefaultConstructorV2 {
+
+    int age;
+
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_8/task_2/DefaultConstructorV2Demo.java
@@ -0,0 +1,14 @@
+package student_ernest_aleskevics.lesson_8.task_2;
+
+public class DefaultConstructorV2Demo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructorV2 constructorCheck = new DefaultConstructorV2();
+        constructorCheck.age = 1;
+        System.out.println(constructorCheck.age);
+        }
+
+
+    }
+
