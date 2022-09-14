b0b664c843737bb4bbe82e84495b4eb5847cf4dc
Ernest <ernests.aleskevics@gmail.com>
2022-09-14 19:22:40 +0300

    lesson 8 task 7

---
+++ src/main/java/student_ernest_aleskevics/lesson_8/level_2/task_7/HumanPlayer.java
@@ -0,0 +1,9 @@
+package student_ernest_aleskevics.lesson_8.level_2.task_7;
+
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(String name) {
+    super(name);
+    }
+
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_8/level_2/task_7/Player.java
@@ -0,0 +1,9 @@
+package student_ernest_aleskevics.lesson_8.level_2.task_7;
+
+public class Player {
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+}
