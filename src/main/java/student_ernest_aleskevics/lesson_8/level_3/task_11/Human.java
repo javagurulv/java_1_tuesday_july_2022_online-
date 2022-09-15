8eec82c626cd6dac2c5880f61eb19268a3a6d5e0
Ernest <ernests.aleskevics@gmail.com>
2022-09-15 18:09:10 +0300

    lesson 8 task 11

---
+++ src/main/java/student_ernest_aleskevics/lesson_8/level_3/task_11/Human.java
@@ -0,0 +1,4 @@
+package student_ernest_aleskevics.lesson_8.level_3.task_11;
+
+public class Human {
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_8/level_3/task_11/LoanSystem.java
@@ -0,0 +1,6 @@
+package student_ernest_aleskevics.lesson_8.level_3.task_11;
+
+abstract class LoanSystem {
+
+    abstract boolean canReceiveLoan(Human human);
+}
