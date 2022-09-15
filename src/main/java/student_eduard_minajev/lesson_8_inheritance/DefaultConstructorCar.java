84bc29ec3ce98fcd6fa145e00f08b0a81fcb2975
Edwrd <Billionare225>
2022-09-15 21:31:42 +0300

    Lesson 8 Default Constructor

---
+++ src/main/java/student_eduard_minajev/lesson_8_inheritance/DefaultConstructorCar.java
@@ -0,0 +1,13 @@
+package student_eduard_minajev.lesson_8_inheritance;
+
+public class DefaultConstructorCar {
+
+    String brandName;
+
+    String bodyType;
+
+    double millage;
+
+
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_8_inheritance/DefaultConstructorCarDemo.java
@@ -0,0 +1,13 @@
+package student_eduard_minajev.lesson_8_inheritance;
+
+public class DefaultConstructorCarDemo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructorCar car = new DefaultConstructorCar();
+
+        System.out.println(car.bodyType);
+        System.out.println(car.brandName);
+        System.out.println(car.millage);
+    }
+}
