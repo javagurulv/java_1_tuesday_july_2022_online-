e78b43f1030f47755b3a4c0260324b76bbd662da
Uguha21 <Agasem2106@icloud.com>
2022-09-15 16:18:37 +0300

    lesson_4_Task_1

---
+++ src/main/java/student_jelena_voinica/lesson_4/Task_1.java
@@ -0,0 +1,17 @@
+package student_jelena_voinica_lesson_4;
+
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input number");
+        int input = in.nextInt();
+        if(input>0){
+            System.out.println("Positive");
+        }
+        else if (input<0){
+        }
+        System.out.println("Negative");
+    }
+}
