025d0ab44b4a7cc07ac697a3fb43df9057d3fc47
olegsradigin <oradigins@gmail.com>
2022-09-16 05:03:24 +0300

    Lesson 6, level 2, task 7

---
+++ src/main/java/student_olegs_radigins/lesson_6/level_2/task_7/DayOfTheWeekDetector.java
@@ -0,0 +1,27 @@
+package student_olegs_radigins.lesson_6.level_2.task_7;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetector {
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+    public String findDayOfTheWeek(int dayNumber) {
+        String dayOfTheWeek;
+        switch (dayNumber) {
+            case 1 -> dayOfTheWeek = "Monday";
+            case 2 -> dayOfTheWeek = "Tuesday";
+            case 3 -> dayOfTheWeek = "Wednesday";
+            case 4 -> dayOfTheWeek = "Thursday";
+            case 5 -> dayOfTheWeek = "Friday";
+            case 6 -> dayOfTheWeek = "Saturday";
+            case 7 -> dayOfTheWeek = "Sunday";
+            default -> dayOfTheWeek = "Not correct day number";
+        }
+        return dayOfTheWeek;
+
+
+    }
+}
