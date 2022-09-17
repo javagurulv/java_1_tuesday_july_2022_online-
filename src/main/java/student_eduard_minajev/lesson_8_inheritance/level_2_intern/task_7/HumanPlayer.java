9840e044ce259a04faac97ddd7e9c3ef508d863d
Edwrd <Billionare225>
2022-09-17 23:33:24 +0300

    Lesson 8

---
+++ src/main/java/student_eduard_minajev/lesson_8_inheritance/level_2_intern/task_7/HumanPlayer.java
@@ -0,0 +1,10 @@
+package student_eduard_minajev.lesson_8_inheritance.level_2_intern.task_7;
+
+public class HumanPlayer extends Player {
+
+        HumanPlayer(String name) {
+            super(name);
+
+        }
+
+}
---
+++ src/main/java/student_eduard_minajev/lesson_8_inheritance/level_2_intern/task_7/Player.java
@@ -0,0 +1,11 @@
+package student_eduard_minajev.lesson_8_inheritance.level_2_intern.task_7;
+
+public class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+}
