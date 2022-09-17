d46c8775ea95dc6a1bb6e7988c1494838e779f65
Edwrd <Billionare225>
2022-09-17 23:34:09 +0300

    Lesson 8

--- src/main/java/student_eduard_minajev/lesson_8_inheritance/level_2_intern/task_6/Porsche911Demo.java
+++ src/main/java/student_eduard_minajev/lesson_8_inheritance/level_2_intern/task_6/Porsche911Demo.java
@@ -1,4 +1,4 @@
-package student_eduard_minajev.lesson_8_inheritance.level_2_intern.Task_6;
+package student_eduard_minajev.lesson_8_inheritance.level_2_intern.task_6;
 
 public class Porsche911Demo {
 
