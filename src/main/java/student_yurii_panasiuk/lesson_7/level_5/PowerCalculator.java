3bd4c3eab5e38ee8efabc1e8c6c37c043ca8306b
Yurii <thoryur@gmail.com>
2022-09-18 12:29:19 +0200

    lessson 7, level 5, Task 9

--- src/main/java/student_yurii_panasiuk/lesson_7/level_5/PowerCalculator.java
+++ src/main/java/student_yurii_panasiuk/lesson_7/level_5/PowerCalculator.java
@@ -2,16 +2,26 @@ package student_yurii_panasiuk.lesson_7.level_5;
 
 class PowerCalculator {
 
-
-/*
-    double exponentiation (double number, int power){
-       int power = this.power;
-        for (i=0, i<this.power )
-
+    double exponentiation(double number, double power) {
+        double result = 1.0;
+        for (int i = 0; i < power; i++) {
+            result = result * number;
+        }
+        return result;
     }
-
- */
-
+    boolean verificationPower(double power) {
+        if (power % 1 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    void verificationFailedMessage () {
+            System.out.println("power must be an integer greater than 0");
+    }
 }
+
+
+
 
 
---
+++ src/main/java/student_yurii_panasiuk/lesson_7/level_5/PowerCalculatorTest.java
@@ -0,0 +1,37 @@
+package student_yurii_panasiuk.lesson_7.level_5;
+import java.util.List;
+
+ class PowerCalculatorTest {
+
+    public static void main(String[] args) {
+        PowerCalculator calculator = new PowerCalculator();
+
+        if (!calculator.verificationPower(0)){
+            calculator.verificationFailedMessage();
+        }
+
+        powerCalculatorTest();
+
+    }
+
+        static void powerCalculatorTest () {
+            PowerCalculator calculator = new PowerCalculator();
+
+            List<Double> listOfNubers = List.of      (1.0, 2.0, 3.0);
+            List<Double> listOfPowers = List.of      (1.0, 2.0, 3.0);
+            List<Double> listOfAnswers = List.of     (1.0, 4.0, 27.0);
+
+            System.out.print("powerCalculatorTest - ");
+            int correctAnswerCounter = 0;
+
+            for (int i = 0; i < listOfNubers.size(); i++ )
+
+            if ((calculator.exponentiation(listOfNubers.get(i), listOfPowers.get(i))) ==
+                (listOfAnswers.get(i))){
+                correctAnswerCounter++;
+                }
+            if (correctAnswerCounter == listOfNubers.size() ){ System.out.println(" OK");}
+             else {System.out.println(" FAIL");}
+        }
+    }
+
