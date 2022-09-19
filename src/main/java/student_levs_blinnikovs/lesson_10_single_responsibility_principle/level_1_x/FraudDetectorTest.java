8fba082ed6274b91df913a1c8cb1373b1a036d79
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-19 22:04:56 +0300

    FraudDetector & FraudDetectorTest

--- src/main/java/student_levs_blinnikovs/lesson_10_single_responsibility_principle/level_1_x/FraudDetectorTest.java
+++ src/main/java/student_levs_blinnikovs/lesson_10_single_responsibility_principle/level_1_x/FraudDetectorTest.java
@@ -4,19 +4,55 @@ class FraudDetectorTest {
 
     void shouldBeFraudTransactionForTraderPokemonTest() {
         FraudDetector detector = new FraudDetector();
-        Trader fraudster = new Trader("Pokemon", "Saransk");
+        Trader fraudster = new Trader("Pokemon", "Saransk", "Russia");
         Transaction fraudTransaction = new Transaction(fraudster, 100);
         boolean expectedFraudTransaction = true;
         boolean realFraudTransaction = detector.isFraud(fraudTransaction);
         testResultPrinter(expectedFraudTransaction == realFraudTransaction);
     }
 
-    void shouldNotBeFraudTransactionForOtherTraderTest() {
+    void shouldNotBeFraudTransactionTest() {
         FraudDetector detector = new FraudDetector();
-        Trader legal = new Trader("Nomekop", "Warsaw");
-        Transaction transaction = new Transaction(legal, 100);
+        Trader legalTrader = new Trader("Nomekop", "Warsaw", "Poland");
+        Transaction transaction = new Transaction(legalTrader, 100);
         boolean expectedFraudTransaction = false;
         boolean realFraudTransaction = detector.isFraud(transaction);
+        testResultPrinter(expectedFraudTransaction == realFraudTransaction);
+    }
+
+    void shouldBeFraudTransactionAboveMaxAmountTest() {
+        FraudDetector detector = new FraudDetector();
+        Trader legalTrader = new Trader("Nomekop", "Warsaw", "Poland");
+        Transaction highTransaction = new Transaction(legalTrader, 1000001);
+        boolean expectedFraudTransaction = true;
+        boolean realFraudTransaction = detector.isFraud(highTransaction);
+        testResultPrinter(expectedFraudTransaction == realFraudTransaction);
+    }
+
+    void shouldBeFraudTransactionFromSydneyTest() {
+        FraudDetector detector = new FraudDetector();
+        Trader fromIllegalCity = new Trader("Aussie", "Sydney", "Australia");
+        Transaction transaction = new Transaction(fromIllegalCity, 100);
+        boolean expectedFraudTransaction = true;
+        boolean realFraudTransaction = detector.isFraud(transaction);
+        testResultPrinter(expectedFraudTransaction == realFraudTransaction);
+    }
+
+    void shouldBeFraudTransactionFromJamaicaTest() {
+        FraudDetector detector = new FraudDetector();
+        Trader fromIllegalCountry = new Trader("Bob Marley", "Kingston", "Jamaica");
+        Transaction transaction = new Transaction(fromIllegalCountry, 100);
+        boolean expectedFraudTransaction = true;
+        boolean realFraudTransaction = detector.isFraud(transaction);
+        testResultPrinter(expectedFraudTransaction == realFraudTransaction);
+    }
+
+    void shouldBeFraudTransactionMoreThanAmountFromGermanyTest() {
+        FraudDetector detector = new FraudDetector();
+        Trader fromIllegalCountry = new Trader("Angela Merkel", "Berlin", "Germany");
+        Transaction tooHighTransaction = new Transaction(fromIllegalCountry, 2000);
+        boolean expectedFraudTransaction = true;
+        boolean realFraudTransaction = detector.isFraud(tooHighTransaction);
         testResultPrinter(expectedFraudTransaction == realFraudTransaction);
     }
 
@@ -31,7 +67,11 @@ class FraudDetectorTest {
     public static void main(String[] args) {
         FraudDetectorTest test = new FraudDetectorTest();
         test.shouldBeFraudTransactionForTraderPokemonTest();
-        test.shouldNotBeFraudTransactionForOtherTraderTest();
+        test.shouldNotBeFraudTransactionTest();
+        test.shouldBeFraudTransactionAboveMaxAmountTest();
+        test.shouldBeFraudTransactionFromSydneyTest();
+        test.shouldBeFraudTransactionFromJamaicaTest();
+        test.shouldBeFraudTransactionMoreThanAmountFromGermanyTest();
     }
 
 }
