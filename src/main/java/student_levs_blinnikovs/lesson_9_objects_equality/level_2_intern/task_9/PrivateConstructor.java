c27b7ce4ff4ec5b671b4d4665d1a0cfd5db0213e
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-19 16:44:16 +0300

    PrivateConstructor

---
+++ src/main/java/student_levs_blinnikovs/lesson_9_objects_equality/level_2_intern/task_9/PrivateConstructor.java
@@ -0,0 +1,9 @@
+package student_levs_blinnikovs.lesson_9_objects_equality.level_2_intern.task_9;
+
+class PrivateConstructor {
+
+    private PrivateConstructor() {
+
+    }
+
+}
---
+++ src/main/java/student_levs_blinnikovs/lesson_9_objects_equality/level_2_intern/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,7 @@
+package student_levs_blinnikovs.lesson_9_objects_equality.level_2_intern.task_9;
+
+class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        //PrivateConstructor privateConstructor = new PrivateConstructor();
+    }
+}
