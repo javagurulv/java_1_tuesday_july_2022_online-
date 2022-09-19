f20f4f7217b41dcdeab0b2a36684afeb6ed2e8f1
Uguha21 <Agasem2106@icloud.com>
2022-09-19 19:40:41 +0300

    lesson_4_Task_4

---
+++ src/main/java/student_jelena_voinica/lesson_4/Task_4.java
@@ -0,0 +1,21 @@
+package student_jelena_voinica.lesson_4;
+
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        System.out.println("Input number");
+        Scanner scanner = new Scanner(System.in);
+
+        double num = scanner.nextDouble();
+
+        if ((num % 2 ) == 0){
+
+            System.out.println("even");}
+
+        else{
+            System.out.println("odd");}
+
+
+        }
+    }
