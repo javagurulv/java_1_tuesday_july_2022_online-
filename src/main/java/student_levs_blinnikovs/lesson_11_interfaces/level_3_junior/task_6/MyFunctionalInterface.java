9d30aab0a5b61ec4eaf2f53224c4369a09b215d7
lblinnikovs <levs.blinnikovs@gmail.com>
2022-09-20 18:24:36 +0300

    Interface training

--- src/main/java/student_levs_blinnikovs/lesson_11_interfaces/level_3_junior/task_6/MyFunctionalInterface.java
+++ src/main/java/student_levs_blinnikovs/lesson_11_interfaces/level_3_junior/task_6/MyFunctionalInterface.java
@@ -1,7 +1,8 @@
 package student_levs_blinnikovs.lesson_11_interfaces.level_3_junior.task_6;
 
 /**
- * Функциональный интерфейс это интерфейс у которого один метод
+ * Функциональный интерфейс это интерфейс у которого есть только один абстрактный метод (но при
+ * этом могут быть статические, приватные и дэфолтные.
  * хорошая практика их обозначать аннотацией @FunctionalInterface
  */
 
@@ -9,5 +10,17 @@ package student_levs_blinnikovs.lesson_11_interfaces.level_3_junior.task_6;
 interface MyFunctionalInterface {
 
     void doStuff();
+
+    static void doSomething() {
+
+    }
+
+    private void doYetAnotherSomething() {
+
+    }
+
+    default void someMethod() {
+
+    }
 
 }
