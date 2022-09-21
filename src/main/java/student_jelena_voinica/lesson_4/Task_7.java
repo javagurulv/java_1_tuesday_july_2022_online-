c2576ccfa975e88993930774e0278b5cf43b6099
Uguha21 <Agasem2106@icloud.com>
2022-09-21 10:47:31 +0300

    lesson_4_Task_7

---
+++ src/main/java/student_jelena_voinica/lesson_4/Task_7.java
@@ -0,0 +1,20 @@
+package student_jelena_voinica.lesson_4;
+
+import java.util.Scanner;
+
+public class Task_7 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input two number");
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+
+        if (i==j){
+            System.out.println("Number are equals:");
+        }
+        else
+            if(i!=j){
+                System.out.println("Number are different:");
+            }
+    }
+}
