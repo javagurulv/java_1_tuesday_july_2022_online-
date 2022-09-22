c4317e2b6bf5437487f9f237ef0a144a8b537f97
Uguha21 <Agasem2106@icloud.com>
2022-09-22 14:21:46 +0300

    lesson_4_Task_8

---
+++ src/main/java/student_jelena_voinica/lesson_4/Task_8.java
@@ -0,0 +1,31 @@
+package student_jelena_voinica.lesson_4;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[]args){
+        System.out.println("Input there number:");
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if(num1==num2 && num2==num3 )
+        {
+            System.out.println("Number are equals:");
+        }
+        else if (num1!=num2 && num2!=num3 && num1!=num3){
+            System.out.println("Number are different:");}
+
+        else{
+            System.out.println("Neither all are equals or different:");
+
+                }
+    }
+        }
+
+
+
+
+
