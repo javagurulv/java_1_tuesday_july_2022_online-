b8ee5b8b962aafb9fe032a281d03fa87b13cac95
Ivan <neomess13@gmail.com>
2022-09-22 16:00:15 +0300

    lesson_5_task_25

---
+++ src/main/java/student_ivan_mostepanov/lesson_5_arrays_for_loop/lesson_4_junior/Task25.java
@@ -0,0 +1,35 @@
+package student_ivan_mostepanov.lesson_5_arrays_for_loop.lesson_4_junior;
+
+import java.util.Scanner;
+
+
+
+class Task25 {
+
+    public static void main(String[] args){
+        int[] array = createArray();
+        fillArrayWithUserNumbers(array);
+        printArrayToConsole(array);
+    }
+    private static int[] createArray (){
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
+        return new int[arrayLength];
+    }
+
+    private static void fillArrayWithUserNumbers(int[] array){
+        Scanner scanner = new Scanner(System.in);
+                for (int i = 0; i < array.length; i++){
+            array[i] = scanner.nextInt();
+        }
+    }
+    private static void printArrayToConsole(int[] array){
+        for (int j : array) {
+            System.out.println(j);
+        }
+    }
+
+
+
+
+}
