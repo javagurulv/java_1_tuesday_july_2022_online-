afdebc5ad5d2569a0c48dead9fa698f637942a61
Uguha21 <Agasem2106@icloud.com>
2022-09-22 14:43:44 +0300

    lesson_4_Task_11

---
+++ src/main/java/student_jelena_voinica/lesson_4/Task_11.java
@@ -0,0 +1,18 @@
+package student_jelena_voinica.lesson_4;
+
+public class Task_11 {
+     int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+     int subtraction(int firstNumber,int secondNumber){
+         return firstNumber - secondNumber;
+     }
+     double multiplication(double firstNumber,double secondNumber){
+         return firstNumber * secondNumber;
+     }
+     double division(double firstNumber,double secondNumber){
+         return firstNumber / secondNumber;
+
+     }
+
+}
