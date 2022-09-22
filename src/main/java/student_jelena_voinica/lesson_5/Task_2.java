51767c8a299380da25b7e83a61a19510e18734f9
Uguha21 <Agasem2106@icloud.com>
2022-09-22 20:02:14 +0300

    lesson_5_Task_2

---
+++ src/main/java/student_jelena_voinica/lesson_5/Task_2.java
@@ -0,0 +1,12 @@
+package student_jelena_voinica.lesson_5;
+
+public class Task_2 {
+    class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[10];
+    }
+
+}
+
+}
