a3aecd9778affd6ca1925dc60b9ab7fc6919191f
olegsradigin <oradigins@gmail.com>
2022-09-23 19:17:57 +0300

    Lesson 8, level 2, Tasks 7

---
+++ src/main/java/student_olegs_radigins/lesson_8/level_2/task_7/HumanPlayer.java
@@ -0,0 +1,7 @@
+package student_olegs_radigins.lesson_8.level_2.task_7;
+
+class HumanPlayer extends Player {
+    HumanPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_8/level_2/task_7/Player.java
@@ -0,0 +1,9 @@
+package student_olegs_radigins.lesson_8.level_2.task_7;
+
+class Player {
+    private String name;
+
+    Player(String name) {
+        this.name = name;
+    }
+}
