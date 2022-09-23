ca1096d9f413d17ded57625ded1bc297259bf38d
sobitjon <seshniyozov@list.ru>
2022-09-23 16:21:06 +0500

    homework lesson  5 level 6

---
+++ src/main/java/student_sobit_eshniyozov/lesson_5_arrays_for_loop/level_6/TicTacToe.java
@@ -0,0 +1,21 @@
+package student_sobit_eshniyozov.lesson_5_arrays_for_loop.level_6;
+
+//        Поле передаётся в этот метод как первый параметр int[][] field.
+//        Знак игрока (0 или 1) передаётся вторым параметром: int playerToCheck.
+//
+//        Метод должен вернуть:
+//        - true если в одной из горизонталей есть три подряд клетки
+//        занятые знаком указанного игрока (int playerToCheck);
+//        - false иначе.
+
+class TicTacToe {
+
+//    int [][] field;
+//    int playerToCheck;
+//
+//
+//    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck){
+//
+    }
+
+
---
+++ src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_1/HelloWorld.java
@@ -0,0 +1,4 @@
+package student_sobit_eshniyozov.lesson_6_arrays_while_loop.level_1;
+
+public class HelloWorld {
+}
