8fd11ea3358fa429b96d82f22402b0a38ef5176d
olegsradigin <oradigins@gmail.com>
2022-09-23 18:52:52 +0300

    Lesson 8, level 1, Tasks 3

---
+++ src/main/java/student_olegs_radigins/lesson_8/level_1/task_3/DefaultConstructorV3Demo.java
@@ -0,0 +1,11 @@
+package student_olegs_radigins.lesson_8.level_1.task_3;
+
+class DefaultConstructorV3Demo {
+    public static void main(String[] args) {
+        DefaultConstructorV3 defaultConstructorV3 = new DefaultConstructorV3();
+        System.out.println(defaultConstructorV3.getFullName());
+        System.out.println(defaultConstructorV3.getAge());
+        System.out.println(defaultConstructorV3.isFemale());
+        System.out.println(defaultConstructorV3.isMale());
+    }
+}
