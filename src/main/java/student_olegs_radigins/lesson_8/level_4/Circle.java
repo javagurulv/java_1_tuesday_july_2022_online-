f7f0fc67a1b6aeb0a15954af083c99903a9c978b
olegsradigin <oradigins@gmail.com>
2022-09-24 04:40:53 +0300

    Lesson 8, level 4, Tasks 14,15

---
+++ src/main/java/student_olegs_radigins/lesson_8/level_4/Circle.java
@@ -0,0 +1,18 @@
+package student_olegs_radigins.lesson_8.level_4;
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        return Math.PI * radius *radius;
+    }
+
+    double calculatePerimeter() {
+        return 2 * radius *Math.PI ;
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_8/level_4/Shape.java
@@ -0,0 +1,12 @@
+package student_olegs_radigins.lesson_8.level_4;
+
+abstract class Shape {
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+    abstract double calculatePerimeter();
+}
