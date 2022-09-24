0d859f5956b9de0aa87d02d837d511ddfb4b0a85
Yurii <thoryur@gmail.com>
2022-09-24 10:41:34 +0200

    lessson 7, level 6, Task 10

---
+++ src/main/java/student_yurii_panasiuk/lesson_7/level_6/task_10/Wallet.java
@@ -0,0 +1,18 @@
+package student_yurii_panasiuk.lesson_7.level_6.task_10;
+
+ class Wallet {
+
+    double monetAmmount;
+
+    public void setMonetAmmount(double monetAmmount) {
+        this.monetAmmount = monetAmmount;
+    }
+
+    public double getMonetAmmount() {
+        return monetAmmount;
+    }
+
+    public Wallet(double monetAmmount) {
+        this.monetAmmount = monetAmmount;
+    }
+}
