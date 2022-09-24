d769c55aed6c736df6bf213088d0a00759cf3b26
Uguha21 <Agasem2106@icloud.com>
2022-09-24 17:12:01 +0300

    lesson_5_Task_9

---
+++ src/main/java/student_jelena_voinica/lesson_5/Task_9.java
@@ -0,0 +1,13 @@
+package student_jelena_voinica.lesson_5;
+
+public class Task_9 {
+    class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[2];
+        System.out.println(numbers[-2]);
+    }
+
+}
+
+}
