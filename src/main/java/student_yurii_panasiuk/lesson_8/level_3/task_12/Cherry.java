6cb67e9e2a885e5076d20765172c8741900d458c
Yurii <thoryur@gmail.com>
2022-09-25 09:22:50 +0200

    lessson 8, level 3, Task 12

--- src/main/java/student_yurii_panasiuk/lesson_8/level_3/task_12/Cherry.java
+++ src/main/java/student_yurii_panasiuk/lesson_8/level_3/task_12/Cherry.java
@@ -6,6 +6,7 @@ package student_yurii_panasiuk.lesson_8.level_3.task_12;
          super(treeHeight);
      }
 
+     @Override
      void setTreeHeight (int treeHeight){
          this.treeHeight = treeHeight;
      }
