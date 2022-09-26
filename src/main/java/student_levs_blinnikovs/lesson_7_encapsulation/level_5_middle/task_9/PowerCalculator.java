26f74957ffa88a0ef220c6997594c2dacf42e547
vsavonins <viktors.savonins@proofit.lv>
2022-09-26 09:43:17 +0300

    Code review Lesson 7

--- src/main/java/student_levs_blinnikovs/lesson_7_encapsulation/level_5_middle/task_9/PowerCalculator.java
+++ src/main/java/student_levs_blinnikovs/lesson_7_encapsulation/level_5_middle/task_9/PowerCalculator.java
@@ -1,5 +1,10 @@
 package student_levs_blinnikovs.lesson_7_encapsulation.level_5_middle.task_9;
 
+import teacher.codereview.CodeReview;
+import teacher.codereview.CodeReviewComment;
+
+@CodeReview(approved = false)
+@CodeReviewComment(teacher = "Смотри src/main/java/teacher/lesson_7_encapsulation/codereview/CleanCodeWarning_3.txt")
 class PowerCalculator {
 
     int power(int base, int power) {
