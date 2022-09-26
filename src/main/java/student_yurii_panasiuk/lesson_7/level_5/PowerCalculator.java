62d10eff89d1a0344efd9dd611d130e970367fc4
vsavonins <viktors.savonins@proofit.lv>
2022-09-26 09:42:13 +0300

    Code review Lesson 7

--- src/main/java/student_yurii_panasiuk/lesson_7/level_5/PowerCalculator.java
+++ src/main/java/student_yurii_panasiuk/lesson_7/level_5/PowerCalculator.java
@@ -1,5 +1,10 @@
 package student_yurii_panasiuk.lesson_7.level_5;
 
+import teacher.codereview.CodeReview;
+import teacher.codereview.CodeReviewComment;
+
+@CodeReview(approved = false)
+@CodeReviewComment(teacher = "Смотри src/main/java/teacher/lesson_7_encapsulation/codereview/CleanCodeWarning_3.txt")
 class PowerCalculator {
 
     double exponentiation(double number, double power) {
