7dfba9d5960e6c565fa9f57db28a51ab5dcb098d
olegsradigin <oradigins@gmail.com>
2022-09-27 21:24:21 +0300

    Lesson 9, level 2, Task 10

---
+++ src/main/java/student_olegs_radigins/lesson_9/level_2/task_10/ConstructorChaining.java
@@ -0,0 +1,15 @@
+package student_olegs_radigins.lesson_9.level_2.task_10;
+
+class ConstructorChaining {
+    private String constructorName;
+    private int parameterCount;
+
+    private ConstructorChaining(String constructorName) {
+        this.constructorName = constructorName;
+    }
+
+    public ConstructorChaining(String constructorName, int parameterCount) {
+        this(constructorName);
+        this.parameterCount = parameterCount;
+    }
+}
