1a5adcf2b63c8ea230e8299faf0c9cd5d32fbfed
olegsradigin <oradigins@gmail.com>
2022-09-27 21:08:46 +0300

    Lesson 9, level 2, Task 9

---
+++ src/main/java/student_olegs_radigins/lesson_9/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,7 @@
+package student_olegs_radigins.lesson_9.level_2.task_9;
+
+class PrivateConstructor {
+    private PrivateConstructor() {
+    }
+
+}
---
+++ src/main/java/student_olegs_radigins/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,8 @@
+package student_olegs_radigins.lesson_9.level_2.task_9;
+
+public class PrivateConstructorDemo {
+    public static void main(String[] args) {
+        //PrivateConstructor privateConstructor = new PrivateConstructor();
+
+    }
+}
