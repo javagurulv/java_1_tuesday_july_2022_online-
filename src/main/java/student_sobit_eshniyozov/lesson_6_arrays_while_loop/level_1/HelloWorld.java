7b5bfe3d0b99464ba8f9a9a73a71407799930bbd
vsavonins <viktors.savonins@proofit.lv>
2022-09-27 10:05:25 +0300

    Code review Lesson 6

--- src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_1/HelloWorld.java
+++
@@ -1,4 +0,0 @@
-package student_sobit_eshniyozov.lesson_6_arrays_while_loop.level_1;
-
-public class HelloWorld {
-}
--- src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_1/Task_6/NumberUtils.java
+++ src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_1/Task_6/NumberUtils.java
@@ -4,6 +4,11 @@ package student_sobit_eshniyozov.lesson_6_arrays_while_loop.level_1.Task_6;
 //          Напишите тесты для проверки метода isEven(int number).
 //          Упростите работу метода, код внутри может быть написан в одну строку.
 
+import teacher.codereview.CodeReview;
+import teacher.codereview.CodeReviewComment;
+
+@CodeReview(approved = true)
+@CodeReviewComment(teacher = "Можно и проще return number % 2 == 0;")
 class NumberUtils {
 
     public boolean isEven(int number) {
