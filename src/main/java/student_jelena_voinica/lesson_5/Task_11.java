38de70cf02a2b598f803a0d48dcadd2248c8a01f
Uguha21 <Agasem2106@icloud.com>
2022-09-28 20:33:05 +0300

    lesson_5_Task_11

---
+++ src/main/java/student_jelena_voinica/lesson_5/Task_11.java
@@ -0,0 +1,21 @@
+package student_jelena_voinica.lesson_5;
+
+import java.util.Scanner;
+
+public class Task_11 {
+    public static void main(String[]args){
+        System.out.println("Input number");
+        Scanner scanner = new Scanner(System.in);
+
+        int[]nums = new int[10];
+
+        for(int i=0;i<10;i++){
+            nums[i] = i;
+
+        }
+        for (int i=0;i<10;i++){
+            System.out.println("String [" + i +"]");
+        }
+    }
+
+}
