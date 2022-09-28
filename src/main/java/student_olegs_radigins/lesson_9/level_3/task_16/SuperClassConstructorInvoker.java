f8351a585d03a43e73457c37b451d6ad8238c072
olegsradigin <oradigins@gmail.com>
2022-09-28 19:26:27 +0300

    Lesson 9, level 3, Task 16

---
+++ src/main/java/student_olegs_radigins/lesson_9/level_3/task_16/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_olegs_radigins.lesson_9.level_3.task_16;
+
+public class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
