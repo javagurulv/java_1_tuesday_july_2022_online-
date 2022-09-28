d7c013d0d035730d720dacd7958577ca36b88755
Uguha21 <Agasem2106@icloud.com>
2022-09-28 09:45:13 +0300

    lesson_5_Task_10

---
+++ src/main/java/student_jelena_voinica/lesson_5/Task_10.java
@@ -0,0 +1,13 @@
+package student_jelena_voinica.lesson_5;
+
+public class Task_10 {
+    public static void main (String[]args){
+        int[]num = {0,1,2,3,4,5,6,7,8,9,};
+
+        for(int i=0;i<num.length;i++)
+
+        System.out.println(num[i]+"");
+
+    }
+}
+
