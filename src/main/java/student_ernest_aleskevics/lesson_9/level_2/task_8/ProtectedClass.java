294b992407fa3384b5bb8c4eebcdab5c31fc77df
Ernest <ernests.aleskevics@gmail.com>
2022-09-29 17:50:56 +0300

    lesson 9 task 8

---
+++ src/main/java/student_ernest_aleskevics/lesson_9/level_2/task_8/ProtectedClass.java
@@ -0,0 +1,10 @@
+package student_ernest_aleskevics.lesson_9.level_2.task_8;
+
+class ProtectedClass {
+
+    private String title;
+    private String author;
+
+    /* Protected дает подклассу возможность использовать вспомогательный метод или переменную,
+    предотвращая неродственный класс от попыток использовать их. */
+}
