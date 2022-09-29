177c68fcb3863e6062a873c60568cf356c14d886
Ernest <ernests.aleskevics@gmail.com>
2022-09-29 17:57:33 +0300

    lesson 9 task 9

---
+++ src/main/java/student_ernest_aleskevics/lesson_9/level_2/task_9/PrivateConstructor.java
@@ -0,0 +1,7 @@
+package student_ernest_aleskevics.lesson_9.level_2.task_9;
+
+public class PrivateConstructor {
+
+    private PrivateConstructor(){
+    }
+}
---
+++ src/main/java/student_ernest_aleskevics/lesson_9/level_2/task_9/PrivateConstructorDemo.java
@@ -0,0 +1,8 @@
+package student_ernest_aleskevics.lesson_9.level_2.task_9;
+
+public class PrivateConstructorDemo {
+
+    public static void main(String[] args) {
+       // PrivateConstructor newPrivateConstructor = new PrivateConstructor(); - можно использовать protected, public или по умолчанию
+    }
+}
