1c680ede85ec0321394739ddb5bbb3756868595c
Yurii <thoryur@gmail.com>
2022-10-01 12:39:35 +0200

    lessson 9, level 1, Task 11

---
+++ src/main/java/student_yurii_panasiuk/lesson_9/level_2/task_11/Descendant.java
@@ -0,0 +1,8 @@
+package student_yurii_panasiuk.lesson_9.level_2.task_11;
+
+ class Descendant extends MainClass {
+
+     Descendant(int quantity, String name) {
+         super(quantity, name);
+     }
+ }
---
+++ src/main/java/student_yurii_panasiuk/lesson_9/level_2/task_11/MainClass.java
@@ -0,0 +1,11 @@
+package student_yurii_panasiuk.lesson_9.level_2.task_11;
+
+ class MainClass {
+     protected int quantity;
+     protected String name;
+
+     protected MainClass(int quantity, String name) {
+         this.quantity = quantity;
+         this.name = name;
+     }
+ }
