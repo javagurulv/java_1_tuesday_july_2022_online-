20fb8cc4ba7a1d7a712c6e82655e6dc35c52ddef
Uguha21 <Agasem2106@icloud.com>
2022-10-03 19:35:02 +0300

    lesson_5_Task_18

---
+++ src/main/java/student_jelena_voinica/lesson_5/Task_18.java
@@ -0,0 +1,9 @@
+package student_jelena_voinica.lesson_5;
+
+public class Task_18
+{ public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+    }
+}
+}
