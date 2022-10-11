673cdd0ce4f6c305c4aa68a079f2bc4d9290b75f
sobitjon <seshniyozov@list.ru>
2022-10-11 12:15:08 +0500

    homework lesson  6 level 3.

--- src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_3/ArrayService.java
+++ src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_3/ArrayService.java
@@ -1,7 +1,6 @@
 package student_sobit_eshniyozov.lesson_6_arrays_while_loop.level_3;
 
-public class ArrayService {
-
+class ArrayService {
 
    public boolean contains(int[] arr, int numberToSearch){
     int i = 0;
@@ -13,4 +12,46 @@ public class ArrayService {
         }
         return false;
     }
-}
+        public int countOccurrences(int[] arr, int numberToSearch){
+        int i = 0;
+        int occurrenceNumber = 0;
+            while (i < arr.length) {
+                if (arr[i] == numberToSearch) {
+                    occurrenceNumber++;
+                }
+                i++;
+            }
+            return occurrenceNumber;
+        }
+
+    public boolean replaceFirstNumber(int[] arr, int numberToReplace, int newNumber){
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] == numberToReplace){
+                arr[i] = newNumber;
+            return true;
+            }
+           i++;
+        }
+        return false;
+   }
+    public int replaceAllNumbers(int[] arr, int numberToReplace, int newNumber){
+       int replaceNumbers = 0;
+       int i = 0;
+       while (i < arr.length) {
+            if (arr[i] == numberToReplace){
+                arr[i] = newNumber;
+                replaceNumbers++;
+            }
+            i++;
+        }
+        return replaceNumbers;
+    }
+
+
+
+
+
+ }
+
+
--- src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_3/ArrayServiceTest.java
+++ src/main/java/student_sobit_eshniyozov/lesson_6_arrays_while_loop/level_3/ArrayServiceTest.java
@@ -1,12 +1,13 @@
 package student_sobit_eshniyozov.lesson_6_arrays_while_loop.level_3;
-
-public class ArrayServiceTest {
-
+class ArrayServiceTest {
 
     public static void main(String[] args) {
 
         ArrayServiceTest test = new ArrayServiceTest();
         test.shouldContain();
+        test.shouldCountOccurrences();
+        test.shouldReplaceFirstNumber();
+        test.shouldReplaceAllNumbers();
 
 
     }
@@ -19,7 +20,6 @@ public class ArrayServiceTest {
         }
     }
 
-
     private void shouldContain() {
         int[] testArray = {1,2,3};
         int numberToSearch = 3;
@@ -27,5 +27,32 @@ public class ArrayServiceTest {
         ArrayService arrayService = new ArrayService();
         boolean realResult = arrayService.contains(testArray, numberToSearch);
         printResult(expectedResult==realResult, "Test should Contain");
+    }
+
+    private void shouldCountOccurrences() {
+        int[] testArray = {1,2,3,3};
+        int numberToSearch = 3;
+        int expectedResult = 2;
+        ArrayService arrayService = new ArrayService();
+        int realResult = arrayService.countOccurrences(testArray, numberToSearch);
+        printResult((expectedResult==realResult), "Test should Count Occur");
+    }
+    private void shouldReplaceFirstNumber() {
+        int[] testArray = {1,2,3,};
+        int numberToReplace = 1;
+        int newNumber = 5;
+        boolean expectedReplace = true;
+        ArrayService arrayService = new ArrayService();
+        boolean realReplace = arrayService.replaceFirstNumber(testArray, numberToReplace, newNumber);
+        printResult((expectedReplace==realReplace), "Test should replace number");
+    }
+    private void shouldReplaceAllNumbers() {
+        int[] testArray = {1,3,3,};
+        int numberToReplace = 3;
+        int newNumber = 5;
+        int expectedReplace = 2;
+        ArrayService arrayService = new ArrayService();
+        int realReplace = arrayService.replaceAllNumbers(testArray, numberToReplace, newNumber);
+        printResult((expectedReplace==realReplace), "Test should replace all numbers");
     }
 }
