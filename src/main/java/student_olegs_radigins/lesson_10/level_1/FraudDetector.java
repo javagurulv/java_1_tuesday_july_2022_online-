06cfe69d5ae26711d8c2e4cdb91dce3999720c03
olegsradigin <oradigins@gmail.com>
2022-10-16 14:50:47 +0300

    Lesson 10, level 1, Task 1-6

---
+++ src/main/java/student_olegs_radigins/lesson_10/level_1/FraudDetector.java
@@ -0,0 +1,7 @@
+package student_olegs_radigins.lesson_10.level_1;
+
+class FraudDetector {
+    boolean isFraud(Transaction t) {
+        return true;
+    }
+}
---
+++ src/main/java/student_olegs_radigins/lesson_10/level_1/Trader.java
@@ -0,0 +1,19 @@
+package student_olegs_radigins.lesson_10.level_1;
+
+class Trader {
+    private String fullName;
+    private String city;
+
+    public Trader(String fullName, String city) {
+        this.fullName = fullName;
+        this.city = city;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
