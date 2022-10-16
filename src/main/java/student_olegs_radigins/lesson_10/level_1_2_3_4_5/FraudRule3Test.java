3ee90ecb25d1a06622b72be9b7a3d2242673c5fa
olegsradigin <oradigins@gmail.com>
2022-10-16 18:14:09 +0300

    Lesson 10, level 5, Task 17-23

--- src/main/java/student_olegs_radigins/lesson_10/level_1_2_3_4_5/FraudRule3Test.java
+++ src/main/java/student_olegs_radigins/lesson_10/level_1_2_3_4_5/FraudRule3Test.java
@@ -1,21 +1,21 @@
 package student_olegs_radigins.lesson_10.level_1_2_3_4_5;
 
-class FraudRule1Test {
+class FraudRule3Test {
     void positiveTest(){
-        Trader trader = new Trader("Pokemon", "Riga", "Latvia");
+        Trader trader = new Trader("NePokemon", "Сидней", "Latvia");
         Transaction transaction = new Transaction(trader, 1);
-        FraudRule1 fraudRule1 = new FraudRule1("Pokemon");
-        if (fraudRule1.isFraud(transaction)){
+        FraudRule3 fraudRule3 = new FraudRule3("Fraud city");
+        if (fraudRule3.isFraud(transaction)){
             System.out.println("Positive test ok");
         }else {
             System.out.println("Positive test fail");
         }
     }
     void negativeTest(){
-        Trader trader = new Trader("Pokemone", "Riga", "Latvia");
+        Trader trader = new Trader("NePokemon", "Riga", "Latvia");
         Transaction transaction = new Transaction(trader, 1);
-        FraudRule1 fraudRule1 = new FraudRule1("Pokemon");
-        if (!fraudRule1.isFraud(transaction)){
+        FraudRule3 fraudRule3 = new FraudRule3("Fraud city");
+        if (!fraudRule3.isFraud(transaction)){
             System.out.println("Negative test ok");
         }else {
             System.out.println("Negative test fail");
@@ -23,8 +23,8 @@ class FraudRule1Test {
     }
 
     public static void main(String[] args) {
-        FraudRule1Test fraudRule1Test = new FraudRule1Test();
-        fraudRule1Test.positiveTest();
-        fraudRule1Test.negativeTest();
+        FraudRule3Test fraudRule3Test = new FraudRule3Test();
+        fraudRule3Test.positiveTest();
+        fraudRule3Test.negativeTest();
     }
 }
--- src/main/java/student_olegs_radigins/lesson_10/level_1_2_3_4_5/FraudRule4Test.java
+++ src/main/java/student_olegs_radigins/lesson_10/level_1_2_3_4_5/FraudRule4Test.java
@@ -1,21 +1,21 @@
 package student_olegs_radigins.lesson_10.level_1_2_3_4_5;
 
-class FraudRule1Test {
+public class FraudRule4Test {
     void positiveTest(){
-        Trader trader = new Trader("Pokemon", "Riga", "Latvia");
+        Trader trader = new Trader("NePokemon", "Сидней", "Ямайка");
         Transaction transaction = new Transaction(trader, 1);
-        FraudRule1 fraudRule1 = new FraudRule1("Pokemon");
-        if (fraudRule1.isFraud(transaction)){
+        FraudRule4 fraudRule4 = new FraudRule4("Fraud country");
+        if (fraudRule4.isFraud(transaction)){
             System.out.println("Positive test ok");
         }else {
             System.out.println("Positive test fail");
         }
     }
     void negativeTest(){
-        Trader trader = new Trader("Pokemone", "Riga", "Latvia");
+        Trader trader = new Trader("NePokemon", "Riga", "Latvia");
         Transaction transaction = new Transaction(trader, 1);
-        FraudRule1 fraudRule1 = new FraudRule1("Pokemon");
-        if (!fraudRule1.isFraud(transaction)){
+        FraudRule4 fraudRule4 = new FraudRule4("Fraud country");
+        if (!fraudRule4.isFraud(transaction)){
             System.out.println("Negative test ok");
         }else {
             System.out.println("Negative test fail");
@@ -23,8 +23,8 @@ class FraudRule1Test {
     }
 
     public static void main(String[] args) {
-        FraudRule1Test fraudRule1Test = new FraudRule1Test();
-        fraudRule1Test.positiveTest();
-        fraudRule1Test.negativeTest();
+        FraudRule4Test fraudRule4Test = new FraudRule4Test();
+        fraudRule4Test.positiveTest();
+        fraudRule4Test.negativeTest();
     }
 }
