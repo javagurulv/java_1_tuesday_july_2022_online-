7b3c1e549d3d604ca0daeca0cc1c480ced96b90b
Yurii <thoryur@gmail.com>
2022-10-16 19:40:23 +0200

    lessson 11, Task 6

--- src/main/java/student_yurii_panasiuk/lesson_11/level_2_3/task_4_5_6/DayOfTheWeekDetectorVersion1.java
+++ src/main/java/student_yurii_panasiuk/lesson_11/level_2_3/task_4_5_6/DayOfTheWeekDetectorVersion1.java
@@ -1,4 +1,4 @@
-package student_yurii_panasiuk.lesson_11.level_2.task_4_5_6;
+package student_yurii_panasiuk.lesson_11.level_2_3.task_4_5_6;
 
  class DayOfTheWeekDetectorVersion1 implements DayOfTheWeekDetector{
 
--- src/main/java/student_yurii_panasiuk/lesson_11/level_2_3/task_4_5_6/Test.java
+++ src/main/java/student_yurii_panasiuk/lesson_11/level_2_3/task_4_5_6/Test.java
@@ -1,4 +1,4 @@
-package student_yurii_panasiuk.lesson_11.level_2.task_4_5_6;
+package student_yurii_panasiuk.lesson_11.level_2_3.task_4_5_6;
 
 import java.util.List;
 import java.util.function.Consumer;
