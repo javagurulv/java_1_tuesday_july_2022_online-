7e077e373e2f030a7e185e71b7e24663fc640d67
olegsradigin <oradigins@gmail.com>
2022-10-17 04:12:33 +0300

    Lesson 11, level 1, Task 2

---
+++ src/main/java/student_olegs_radigins/lesson_11/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,14 @@
+package student_olegs_radigins.lesson_11.level_1.task_2;
+
+interface ArrayUtil {
+   int[] createArray(int arrayLength);
+
+   void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+}
