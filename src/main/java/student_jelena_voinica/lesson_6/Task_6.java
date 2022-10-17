6325b63e8855a99220b3ee58dae61dac0975fbd0
Uguha21 <Agasem2106@icloud.com>
2022-10-17 17:18:29 +0300

    lesson_6_Task_6

---
+++ src/main/java/student_jelena_voinica/lesson_6/Task_6.java
@@ -0,0 +1,10 @@
+package student_jelena_voinica.lesson_6;
+
+public class Task_6 {
+    class NumberUtils {
+
+        public boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+    }
+}
