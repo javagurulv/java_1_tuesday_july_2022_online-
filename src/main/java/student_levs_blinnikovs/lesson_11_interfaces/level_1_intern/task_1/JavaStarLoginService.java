4c8f1e96d1e5d9cc3b4413ae80027ee66f278b7f
vsavonins <viktors.savonins@proofit.lv>
2022-10-18 09:10:13 +0300

    code review

--- src/main/java/student_levs_blinnikovs/lesson_11_interfaces/level_1_intern/task_1/JavaStarLoginService.java
+++ src/main/java/student_levs_blinnikovs/lesson_11_interfaces/level_1_intern/task_1/JavaStarLoginService.java
@@ -1,5 +1,8 @@
 package student_levs_blinnikovs.lesson_11_interfaces.level_1_intern.task_1;
 
+import teacher.codereview.CodeReview;
+
+@CodeReview(approved = true)
 interface JavaStarLoginService {
 
     boolean canLogin(String login, String password);
