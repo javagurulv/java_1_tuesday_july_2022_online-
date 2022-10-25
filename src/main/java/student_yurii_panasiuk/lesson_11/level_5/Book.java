f3849363ef8322b3477601f2ad0788fd76fc3ed3
Yurii <thoryur@gmail.com>
2022-10-25 13:19:58 +0200

    lessson 11,  corrections

---
+++ src/main/java/student_yurii_panasiuk/lesson_11/level_5/Book.java
@@ -0,0 +1,6 @@
+package student_yurii_panasiuk.lesson_11.level_5;
+
+ class Book {
+     private String bookAutor;
+     private String bookTitle;
+}
---
+++ src/main/java/student_yurii_panasiuk/lesson_11/level_5/BookReader.java
@@ -0,0 +1,12 @@
+package student_yurii_panasiuk.lesson_11.level_5;
+
+public interface BookReader {
+/*
+    Book newBook = null;
+
+    default boolean bookAdd(Book newBook) {
+        return true;
+    };
+
+ */
+}
