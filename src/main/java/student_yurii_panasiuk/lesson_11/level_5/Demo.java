f3cfa65c1ac61ba62fd968ca91a462544a1471a9
Yurii <thoryur@gmail.com>
2022-10-25 21:24:30 +0200

    lessson 11, Task 15 +

--- src/main/java/student_yurii_panasiuk/lesson_11/level_5/Demo.java
+++ src/main/java/student_yurii_panasiuk/lesson_11/level_5/Demo.java
@@ -46,7 +46,7 @@ public class Demo {
         System.out.println(bookReaderDemo.bookAdd(book1,library));
         System.out.println(bookReaderDemo.bookAdd(book4,library));
         System.out.println(bookReaderDemo.bookAdd(book5,library));
-        System.out.println("\\n");
+        System.out.println("\n");
         System.out.println(bookReaderDemo.bookDel(book1,library));
         System.out.println(bookReaderDemo.bookDel(book1,library));
         System.out.println(bookReaderDemo.bookDel(book4,library));
