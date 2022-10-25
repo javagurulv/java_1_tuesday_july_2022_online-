f18cd971549d265cfa6a05172fbb7cd19f5c2a33
Uguha21 <Agasem2106@icloud.com>
2022-10-25 19:37:27 +0300

    lesson_6_ForInfiniteLoop

---
+++ src/main/java/student_jelena_voinica/lesson_6/ForInfiniteLoop.java
@@ -0,0 +1,11 @@
+package student_jelena_voinica.lesson_6;
+
+public class ForInfiniteLoop {
+    public static void main(String[]args) {
+        int i = 0;
+        for (; ; ) {
+            i++;
+            System.out.println(i);
+        }
+    }
+    }
