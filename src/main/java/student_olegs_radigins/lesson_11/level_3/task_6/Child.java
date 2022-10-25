f9a66ab9c4ade03e7fdde4e0226f5dc0c0b7b4aa
olegsradigin <oradigins@gmail.com>
2022-10-25 19:11:36 +0300

    Lesson 11, level 3, Task 7

---
+++ src/main/java/student_olegs_radigins/lesson_11/level_3/task_6/Child.java
@@ -0,0 +1,6 @@
+package student_olegs_radigins.lesson_11.level_3.task_6;
+
+interface Child {
+    void talk();
+}
+// Функциональный интерфейс в Java это интерфейс с одним методом.
---
+++ src/main/java/student_olegs_radigins/lesson_11/level_3/task_7/Product.java
@@ -0,0 +1,13 @@
+package student_olegs_radigins.lesson_11.level_3.task_7;
+
+class Product {
+    private String title;
+
+    public Product(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}
