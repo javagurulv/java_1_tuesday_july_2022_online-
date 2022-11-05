629290e8633f7ce2be9293d58b8323ba123e1cbb
olegsradigin <oradigins@gmail.com>
2022-11-05 10:32:59 +0200

    Lesson 11, level 3, Task 8

--- src/main/java/student_olegs_radigins/lesson_11/level_3/task_7/ProductDatabase.java
+++
@@ -1,9 +0,0 @@
-package student_olegs_radigins.lesson_11.level_3.task_7;
-
-interface ProductDatabase {
-    void save(Product product);
-
-    String findByTitle(String productTitle);
-}
-
-// Нет, это не функциональный интерфейс.Два метода
