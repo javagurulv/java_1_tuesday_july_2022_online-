94c49d96f153ecda5d23c2c0b506c4f93a8fe761
olegsradigin <oradigins@gmail.com>
2022-11-23 06:31:31 +0200

    Lesson 12, level 1, Task 5

---
+++ src/main/java/student_olegs_radigins/lesson_12/level_1/task_5/Book.java
@@ -0,0 +1,34 @@
+package student_olegs_radigins.lesson_12.level_1.task_5;
+
+import java.util.Objects;
+
+class Book {
+    private String title;
+    private String author;
+
+    Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return Objects.equals(getTitle(), book.getTitle()) && Objects.equals(getAuthor(), book.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getAuthor());
+    }
+}
