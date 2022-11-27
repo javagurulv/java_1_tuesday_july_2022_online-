8e721b0e972d2214b13936e223243e8384fac49e
pomogblin <thoryur@gmaI.com>
2022-11-27 20:24:47 +0200

    Lesson 12, level 3, task 17

---
+++ src/main/java/student_yurii_panasiuk/lesson_12/level_2_3/TitleSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_yurii_panasiuk.lesson_12.level_2_3;
+
+public class TitleSearchCriteria implements SearchCriteria {
+
+        private String titleToSearch;
+
+        public TitleSearchCriteria(String titleToSearch) {
+            this.titleToSearch = titleToSearch;
+        }
+
+        public boolean match(Book book) {
+            return book.getTitle().equals(titleToSearch);
+        }
+
+}
