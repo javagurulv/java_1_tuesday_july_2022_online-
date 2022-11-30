6d4cdb03f35b7196f16cca5aaaa5dd924c6775c1
lblinnikovs <levs.blinnikovs@gmail.com>
2022-11-30 09:30:58 +0200

    Exceptions - Level 2 Intern - Task 8 - UserCredentials

---
+++ src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_2_intern/task_7/UserCredentials.java
@@ -0,0 +1,12 @@
+package student_levs_blinnikovs.lesson_13_exceptions.level_2_intern.task_7;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+}
