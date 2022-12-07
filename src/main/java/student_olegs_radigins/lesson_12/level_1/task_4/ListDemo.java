6aa2cca5a633b5e8d7750547ba4a0c7789406180
olegsradigin <oradigins@gmail.com>
2022-12-07 19:12:49 +0200

    Lesson 12, level 1, Task 4

---
+++ src/main/java/student_olegs_radigins/lesson_12/level_1/task_4/ListDemo.java
@@ -0,0 +1,19 @@
+package student_olegs_radigins.lesson_12.level_1.task_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> demo = new ArrayList<>();
+        demo.add(1);
+        demo.add(1);
+        demo.add(1);
+        demo.add(1);
+        demo.add(1);
+        demo.add(1);
+        System.out.println(demo);
+
+    }
+}
+// Да. Может.
