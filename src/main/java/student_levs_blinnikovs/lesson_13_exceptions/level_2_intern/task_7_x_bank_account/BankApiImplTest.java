50438d1cea877fd20e1018a14ce48fa6d0f1c4ff
lblinnikovs <levs.blinnikovs@gmail.com>
2022-12-20 06:59:01 +0200

    Exceptions - Level 2 Intern - Task 14 - Tests exception handling.

--- src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_2_intern/task_7_x_bank_account/BankApiImplTest.java
+++ src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_2_intern/task_7_x_bank_account/BankApiImplTest.java
@@ -1,17 +1,15 @@
 package student_levs_blinnikovs.lesson_13_exceptions.level_2_intern.task_7_x_bank_account;
 
 import java.util.List;
-import java.util.Optional;
-import static student_levs_blinnikovs.personal.test_utils.TestUtil.printTestResult;
 
 class BankApiImplTest {
-    public static void main(String[] args) throws AccessDeniedException {
+    public static void main(String[] args) {
         BankApiImplTest test = new BankApiImplTest();
-        test.shouldFindById();
-        test.shouldNotFindNonExistingById();
+        test.shouldNotThrowExceptionWithProperRoles();
+        test.shouldThrowExceptionWithoutProperRoles();
     }
 
-    void shouldFindById() throws AccessDeniedException {
+    void shouldNotThrowExceptionWithProperRoles() {
         BankClient client1 = new BankClient("1", "Levs");
 
         List<Role> roles = List.of(Role.CAN_SEARCH_CLIENTS);
@@ -20,25 +18,32 @@ class BankApiImplTest {
 
         UserCredentials userCredentials = new UserCredentials(roles);
         BankApiImpl api = new BankApiImpl(bankClients);
-        Optional<BankClient> expectedBankClient = Optional.of(client1);
-        Optional<BankClient> realBankClient = api.findByUid(userCredentials, "1");
 
-        printTestResult(realBankClient.equals(expectedBankClient), "Should find by id");
+        try {
+            api.findByUid(userCredentials, "1");
+            System.out.println("TEST OK - Shouldn't throw AccessDeniedException and didn't.");
+        } catch (AccessDeniedException e) {
+            System.out.println("TEST NOT OK - Should not throw AccessDeniedException but did.");
+        }
     }
 
-    void shouldNotFindNonExistingById() throws AccessDeniedException {
+    void shouldThrowExceptionWithoutProperRoles() {
         BankClient client1 = new BankClient("1", "Levs");
 
-        List<Role> roles = List.of(Role.CAN_SEARCH_CLIENTS);
+        List<Role> roles = List.of();
 
         List<BankClient> bankClients = List.of(client1);
 
         UserCredentials userCredentials = new UserCredentials(roles);
         BankApiImpl api = new BankApiImpl(bankClients);
-        Optional<BankClient> expectedBankClient = Optional.of(client1);
-        Optional<BankClient> realBankClient = api.findByUid(userCredentials, "2");
 
-        printTestResult(!realBankClient.equals(expectedBankClient), "Should not find non existing by id");
+        try {
+            api.findByUid(userCredentials, "1");
+            System.out.println("TEST NOT OK - Didn't throw AccessDeniedException.");
+        } catch (AccessDeniedException e) {
+            System.out.println("TEST OK - Should not find non existing by id and throw AccessDeniedException.");
+        }
+
     }
 
 }
