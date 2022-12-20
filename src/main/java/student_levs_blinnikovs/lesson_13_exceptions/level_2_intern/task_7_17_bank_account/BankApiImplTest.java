0ad81ee276d46aa4ec64cceb025871901905b6af
lblinnikovs <levs.blinnikovs@gmail.com>
2022-12-20 14:57:13 +0200

    Returned smoke tests

--- src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_2_intern/task_7_17_bank_account/BankApiImplTest.java
+++ src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_2_intern/task_7_17_bank_account/BankApiImplTest.java
@@ -1,12 +1,47 @@
 package student_levs_blinnikovs.lesson_13_exceptions.level_2_intern.task_7_17_bank_account;
 
 import java.util.List;
+import java.util.Optional;
+
+import static student_levs_blinnikovs.personal.test_utils.TestUtil.printTestResult;
 
 class BankApiImplTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AccessDeniedException {
         BankApiImplTest test = new BankApiImplTest();
+        test.shouldFindByIdSmoke();
+        test.shouldNotFindNonExistingByIdSmoke();
         test.shouldNotThrowExceptionWithProperRoles();
         test.shouldThrowExceptionWithoutProperRoles();
+    }
+
+    void shouldFindByIdSmoke() throws AccessDeniedException {
+        BankClient client1 = new BankClient("1", "Levs");
+
+        List<Role> roles = List.of(Role.CAN_SEARCH_CLIENTS);
+
+        List<BankClient> bankClients = List.of(client1);
+
+        UserCredentials userCredentials = new UserCredentials(roles);
+        BankApiImpl api = new BankApiImpl(bankClients);
+        Optional<BankClient> expectedBankClient = Optional.of(client1);
+        Optional<BankClient> realBankClient = api.findByUid(userCredentials, "1");
+
+        printTestResult(realBankClient.equals(expectedBankClient), "Should find by id");
+    }
+
+    void shouldNotFindNonExistingByIdSmoke() throws AccessDeniedException {
+        BankClient client1 = new BankClient("1", "Levs");
+
+        List<Role> roles = List.of(Role.CAN_SEARCH_CLIENTS);
+
+        List<BankClient> bankClients = List.of(client1);
+
+        UserCredentials userCredentials = new UserCredentials(roles);
+        BankApiImpl api = new BankApiImpl(bankClients);
+        Optional<BankClient> expectedBankClient = Optional.of(client1);
+        Optional<BankClient> realBankClient = api.findByUid(userCredentials, "2");
+
+        printTestResult(!realBankClient.equals(expectedBankClient), "Should not find non existing by id");
     }
 
     void shouldNotThrowExceptionWithProperRoles() {
