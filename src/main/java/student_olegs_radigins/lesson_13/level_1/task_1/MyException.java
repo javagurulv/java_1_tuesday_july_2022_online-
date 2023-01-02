c25ff2d4a73c88cdb7d153c886275681f9d915b8
olegsradigins <oradigins@gmail.com>
2023-01-02 18:56:53 +0200

    Lesson 13, level 1, task 1-3

---
+++ src/main/java/student_olegs_radigins/lesson_13/level_1/task_1/MyException.java
@@ -0,0 +1,4 @@
+package student_olegs_radigins.lesson_13.level_1.task_1;
+
+class MyException extends Throwable {
+}
---
+++ src/main/java/student_olegs_radigins/lesson_13/level_1/task_2/MyException.java
@@ -0,0 +1,4 @@
+package student_olegs_radigins.lesson_13.level_1.task_2;
+
+class MyException extends Throwable {
+}
