e000e5a3ecf32f628979e5065440d4d519453769
olegsradigin <oradigins@gmail.com>
2023-01-15 20:18:12 +0300

    Lesson 14, level 2 , task 10

---
+++ src/main/java/student_olegs_radigins/lesson_14/level_3/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,7 @@
+package student_olegs_radigins.lesson_14.level_3;
+
+class GameOfLifeNextGenerationCalculator {
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null;
+    }
+}
