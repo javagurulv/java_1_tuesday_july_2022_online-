f5b23c0e9bf7d2aac9dc33f0137e5b602d81f326
olegsradigin <oradigins@gmail.com>
2023-01-15 10:02:32 +0200

    Lesson 14, level 1, Task 5-8

--- src/main/java/student_olegs_radigins/lesson_14/level_1/TaxCalculatorImpl.java
+++ src/main/java/student_olegs_radigins/lesson_14/level_1/TaxCalculatorImpl.java
@@ -7,7 +7,11 @@ public class TaxCalculatorImpl implements TaxCalculator{
     public BigDecimal calculateTax(BigDecimal income) {
         if(income.compareTo(new BigDecimal(20000)) <= 0){
             return income.multiply(new BigDecimal(0.25));
-        }
-        return null;
+        } else {
+            var incomeBiggerThen20000 = income.subtract(new BigDecimal("20000"));
+            var taxAfter20000 = incomeBiggerThen20000.multiply(new BigDecimal("0.40"));
+            var taxBefore20000 = new BigDecimal("20000").multiply(new BigDecimal("0.25"));
+            return taxBefore20000.add(taxAfter20000);
+    }
     }
 }
--- src/test/java/student_olegs_radigins/lesson_14/level_1/TaxCalculatorImplTest.java
+++ src/test/java/student_olegs_radigins/lesson_14/level_1/TaxCalculatorImplTest.java
@@ -29,6 +29,12 @@ public class TaxCalculatorImplTest {
         var taxToPay = taxCalculator.calculateTax(income);
         assertEquals(taxToPay, new BigDecimal("5000.00"));
     }
+    @Test
+    public void shouldPay40PercentTaxWhenIncomeBiggerThen20000() {
+        var income = new BigDecimal(30000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("9000.00"));
+    }
 
 
 }
