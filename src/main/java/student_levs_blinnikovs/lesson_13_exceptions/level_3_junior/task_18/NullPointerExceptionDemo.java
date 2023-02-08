a2f17bbda59e72e879388c7b92c9ef56a309c309
lblinnikovs <levs.blinnikovs@gmail.com>
2023-02-08 17:38:17 +0100

    Exceptions - Task 18 - NullPointerExceptionDemo

--- src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_3_junior/task_18/NullPointerExceptionDemo.java
+++ src/main/java/student_levs_blinnikovs/lesson_13_exceptions/level_3_junior/task_18/NullPointerExceptionDemo.java
@@ -2,6 +2,7 @@ package student_levs_blinnikovs.lesson_13_exceptions.level_3_junior.task_18;
 
 class NullPointerExceptionDemo {
     public static void main(String[] args) {
-
+        String nullString = null;
+        nullString.length(); // NullPointerException happens when you try to access variable which is null
     }
 }
